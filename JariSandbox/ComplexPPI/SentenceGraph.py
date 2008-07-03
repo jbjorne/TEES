f24422929427424ae9abaa11e21837bf1edec59a
jari <jari>
2008-07-03 13:24:10 +0000

    Some preliminary work. Reading and drawing the graphs works now. Files will be deleted and moved around later.

---
+++ JariSandbox/ComplexPPI/SentenceGraph.py
@@ -0,0 +1,48 @@
+import networkx as NX
+import Range
+
+class SentenceGraph:
+    def __init__(self, tokenElements, dependencyElements):
+        self.tokens = tokenElements
+        self.dependencies = dependencyElements
+        self.dependencyGraph = NX.XDiGraph()
+        self.interactions = None
+        self.entities = None
+        self.interactionGraph = None
+        
+        self.tokensById = {}
+        for token in self.tokens:
+            self.tokensById[token.attrib["id"]] = token
+            self.dependencyGraph.add_node(token)
+        #self.dependenciesById = {}
+        for dependency in self.dependencies:
+            self.dependencyGraph.add_edge(self.tokensById[dependency.attrib["t1"]],\
+                                          self.tokensById[dependency.attrib["t2"]],\
+                                          dependency)
+            #self.dependenciesById[dependency.attrib["id"]] = dependency
+    
+    def mapInteractions(self, entityElements, interactionElements):
+        self.interactions = interactionElements
+        self.entities = entityElements
+        self.interactionGraph = NX.XDiGraph()
+        
+        for token in self.tokens:
+            self.interactionGraph.add_node(token)
+        
+        self.entitiesById = {}
+        for entity in self.entities:
+            self.entitiesById[entity.attrib["id"]] = entity
+        for interaction in self.interactions:
+            token1 = self.mapEntity(self.entitiesById[interaction.attrib["e1"]])
+            token2 = self.mapEntity(self.entitiesById[interaction.attrib["e2"]])
+            self.interactionGraph.add_edge(token1, token2, interaction)
+    
+    def mapEntity(self, entityElement):
+        headOffset = Range.charOffsetToSingleTuple(entityElement.attrib["headOffset"])
+        headTokens = []
+        for token in self.tokens:
+            tokenOffset = Range.charOffsetToSingleTuple(token.attrib["charOffset"])
+            if Range.overlap(headOffset,tokenOffset):
+                headTokens.append(token)
+        assert(len(headTokens)==1) # Terrible hack, but should work for now
+        return headTokens[0]
---
+++ JariSandbox/ComplexPPI/SimpleTest.py
@@ -0,0 +1,24 @@
+import sys
+import cElementTree as ET
+from InteractionXML.CorpusElements import CorpusElements
+from SentenceGraph import *
+import GraphToSVG
+
+if __name__=="__main__":
+    defaultInteractionFilename = "Data/BioInferForComplexPPI.xml"
+    
+    print >> sys.stderr, "Loading corpus file", defaultInteractionFilename
+    corpusTree = ET.parse(defaultInteractionFilename)
+    corpusRoot = corpusTree.getroot()
+    corpusElements = CorpusElements(corpusRoot, "split_gs", "split_gs")
+    sentence = corpusElements.sentencesById["BioInfer.d6.s6"]
+    print >> sys.stderr, "Building graph"
+    graph = SentenceGraph(sentence.tokens, sentence.dependencies)
+    print >> sys.stderr, "Mapping interactions"
+    graph.mapInteractions(sentence.entities, sentence.interactions)
+    print >> sys.stderr, "Drawing"
+    svgTokensById, svgTokens = GraphToSVG.tokensToSVG(graph.tokens)
+    svgDepEdges = GraphToSVG.edgesToSVG(svgTokensById, graph.dependencyGraph)
+    svgIntEdges = GraphToSVG.edgesToSVG(svgTokensById, graph.interactionGraph)
+    GraphToSVG.writeSVG(svgTokens, svgDepEdges, "Data/depGraph.svg")
+    GraphToSVG.writeSVG(svgTokens, svgIntEdges, "Data/intGraph.svg")
