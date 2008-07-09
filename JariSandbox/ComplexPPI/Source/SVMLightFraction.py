4feef33965dd804ad4660322b7b94a63cb010afe
jari <jari>
2008-07-09 13:18:33 +0000

    Added options for modules.

--- JariSandbox/ComplexPPI/Source/SVMLightFraction.py
+++ JariSandbox/ComplexPPI/Source/SVMLightFraction.py
@@ -1,23 +1,33 @@
 import Core.ExampleUtils as Example
 import sys, os
 import cElementTree as ET
-from ExampleBuilders.SimpleDependencyExampleBuilder import SimpleDependencyExampleBuilder
+#from ExampleBuilders.SimpleDependencyExampleBuilder import SimpleDependencyExampleBuilder
 from InteractionXML.CorpusElements import CorpusElements
 from Core.SentenceGraph import *
-from Classifiers.SVMLightClassifier import *
+#from Classifiers.SVMLightClassifier import SVMLightClassifier as Classifier
 from Core.Evaluation import Evaluation
+from optparse import OptionParser
 
 if __name__=="__main__":
-    defaultInteractionFilename = "/usr/share/biotext/ComplexPPI/BioInferForComplexPPI.xml"
+    defaultAnalysisFilename = "/usr/share/biotext/ComplexPPI/BioInferForComplexPPI.xml"
+    optparser = OptionParser(usage="%prog [options]\nCreate an html visualization for a corpus.")
+    optparser.add_option("-i", "--input", default=defaultAnalysisFilename, dest="input", help="Corpus", metavar="FILE")
+    optparser.add_option("-c", "--classifier", default="SVMLightClassifier", dest="classifier", help="Corpus", metavar="FILE")
+    optparser.add_option("-e", "--exampleBuilder", default="SimpleDependencyExampleBuilder", dest="exampleBuilder", help="Corpus", metavar="FILE")
+    (options, args) = optparser.parse_args()
+
+    print >> sys.stderr, "Importing modules"
+    exec "from ExampleBuilders." + options.exampleBuilder + " import " + options.exampleBuilder + " as ExampleBuilder"
+    exec "from Classifiers." + options.classifier + " import " + options.classifier + " as Classifier"
     
-    print >> sys.stderr, "Loading corpus file", defaultInteractionFilename
-    corpusTree = ET.parse(defaultInteractionFilename)
+    print >> sys.stderr, "Loading corpus file", options.input
+    corpusTree = ET.parse(options.input)
     corpusRoot = corpusTree.getroot()
     corpusElements = CorpusElements(corpusRoot, "split_gs", "split_gs")
     
     # Build examples
     examples = []
-    exampleBuilder = SimpleDependencyExampleBuilder()
+    exampleBuilder = ExampleBuilder()
     for sentence in corpusElements.sentences:
         print >> sys.stderr, "\rProcessing sentence", sentence.sentence.attrib["id"], "          ",
         #print >> sys.stderr, "Building graph"
@@ -32,7 +42,7 @@ if __name__=="__main__":
     corpusDivision = Example.makeCorpusDivision(corpusElements)
     exampleSets = Example.divideExamples(examples, corpusDivision)
 
-    classifier = SVMLightClassifier()
+    classifier = Classifier()
     
     # Optimize
     print >> sys.stderr, "Optimizing c-parameter"
