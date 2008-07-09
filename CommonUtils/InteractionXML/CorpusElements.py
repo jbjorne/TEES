36fe63da3f1e913539f04d95909895e7bf552f95
jari <jari>
2008-07-09 12:50:46 +0000

    Added a simple list of sentences to the class.

--- CommonUtils/InteractionXML/CorpusElements.py
+++ CommonUtils/InteractionXML/CorpusElements.py
@@ -6,9 +6,11 @@ class CorpusElements:
         self.documents = rootElement.findall("document")
         self.documentsById = {}
         self.sentencesById = {}
+        self.sentences = []
         for documentElement in self.documents:
             self.documentsById[documentElement.attrib["id"]] = documentElement
             sentenceElements = documentElement.findall("sentence")
             for sentenceElement in sentenceElements:
                 sentenceObj = SentenceElements(sentenceElement, parse, tokenization)
                 self.sentencesById[sentenceElement.attrib["id"]] = sentenceObj
+                self.sentences.append(sentenceObj)
