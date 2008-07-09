32cd5869073375563794bb016cd29728c3b766f4
jari <jari>
2008-07-09 12:47:00 +0000

    Moved to ComplexPPI/Source and subdirectories.

--- JariSandbox/ComplexPPI/ExampleBuilder.py
+++
@@ -1,8 +0,0 @@
-from FeatureSet import FeatureSet
-
-class ExampleBuilder:
-    def __init__(self):
-        self.featureSet = FeatureSet()
-
-    def buildExamples(self, sentenceGraph, examplesByDocument):
-        pass
