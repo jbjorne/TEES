ebd98a587b908ac9882309e7c04d150284b778e8
jari <jari>
2008-09-23 13:10:14 +0000

    Added (currently disabled) code for downsampling negative instances.

--- JariSandbox/ComplexPPI/Source/Classifiers/SVMMultiClassClassifier.py
+++ JariSandbox/ComplexPPI/Source/Classifiers/SVMMultiClassClassifier.py
@@ -6,6 +6,7 @@ import Core.ExampleUtils as Example
 import combine
 from Core.Evaluation import Evaluation
 from Core.Classifier import Classifier
+import Core.Split as Split
 from Evaluators.MultiClassEvaluator import MultiClassEvaluator
 
 binDir = "/usr/share/biotext/ComplexPPI/SVMMultiClass"
@@ -13,9 +14,9 @@ binDir = "/usr/share/biotext/ComplexPPI/SVMMultiClass"
 defaultOptimizationParameters = {"c":[0.0001,0.001,0.01,0.1,1,10,100]}
 
 class SVMMultiClassClassifier(Classifier):
-    def __init__(self, workDir=None):
+    def __init__(self, workDir=None, negRatio=None):
         #global tempDir
-        
+        self.negRatio = None
         self._makeTempDir(workDir)        
     
     def __del__(self):
@@ -25,7 +26,9 @@ class SVMMultiClassClassifier(Classifier):
             shutil.rmtree(self.tempDir)
     
     def train(self, examples, parameters=None):
-        examples = self.filterTrainingSet(examples)      
+        examples = self.filterTrainingSet(examples)
+        if self.negRatio != None:
+            examples = self.downSampleNegatives(examples, self.negRatio)
         Example.writeExamples(examples, self.tempDir+"/train.dat")
         args = [binDir+"/svm_multiclass_learn"]
         if parameters != None:
@@ -54,6 +57,26 @@ class SVMMultiClassClassifier(Classifier):
         for k,v in parameters.iteritems():
             args.append("-"+k)
             args.append(str(v))
+
+    def downSampleNegatives(self, examples, ratio):
+        positives = []
+        negatives = []
+        for example in examples:
+            if example[1] == 1:
+                negatives.append(example)
+            else:
+                positives.append(example)
+        
+        targetNumNegatives = ratio * len(positives)
+        if targetNumNegatives > len(negatives):
+            targetNumNegatives = len(negatives)
+        sample = Split.getSample(len(negatives), targetNumNegatives / float(len(negatives)) )
+        examples = positives
+        for i in range(len(sample)):
+            if sample[i] == 0:
+                examples.append(negatives[i])
+        return examples
+
     
 #    def optimize(self, trainExamples, classifyExamples, parameters=defaultOptimizationParameters, evaluationClass=Evaluation, evaluationArgs=None):
 #        return Classifier.optimize(self, trainExamples, classifyExamples, parameters, MultiClassEvaluator, None)
