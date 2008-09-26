3fc07f8b478dedc61519cb72f421712677350792
jari <jari>
2008-09-26 15:15:45 +0000

    Entities outside interaction graph do not add tokens to the graph.

--- JariSandbox/ComplexPPI/Source/Core/SentenceGraph.py
+++ JariSandbox/ComplexPPI/Source/Core/SentenceGraph.py
@@ -65,9 +65,17 @@ class SentenceGraph:
         
         self.entitiesById = {}
         entityHeadTokenByEntity = {}
-        for entity in self.entities:
-            self.entitiesById[entity.attrib["id"]] = entity
-            entityHeadTokenByEntity[entity] = self.mapEntity(entity, verbose)
+        for entity in self.entities[:]:
+            entityIsPartOfInteractionGraph = False
+            for interaction in self.interactions:
+                if interaction.attrib["e1"] == entity.attrib["id"] or interaction.attrib["e2"] == entity.attrib["id"]:
+                    entityIsPartOfInteractionGraph = True
+                    break
+            if entityIsPartOfInteractionGraph:
+                self.entitiesById[entity.attrib["id"]] = entity
+                entityHeadTokenByEntity[entity] = self.mapEntity(entity, verbose)
+            else:
+                self.entities.remove(entity)
         for interaction in self.interactions:
             token1 = entityHeadTokenByEntity[self.entitiesById[interaction.attrib["e1"]]]
             token2 = entityHeadTokenByEntity[self.entitiesById[interaction.attrib["e2"]]]
