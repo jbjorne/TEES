88986dc5d249512d101fa2e78d21f79e8c2c24b2
jari <jari>
2008-09-30 19:01:42 +0000

    Updated to accept constructor parameters.

--- JariSandbox/ComplexPPI/Source/Classifiers/AllTrueClassifier.py
+++ JariSandbox/ComplexPPI/Source/Classifiers/AllTrueClassifier.py
@@ -1,6 +1,15 @@
 from Core.Classifier import Classifier
 
 class AllTrueClassifier(Classifier):
+    def __init__(self, workDir=None):
+        self._makeTempDir(workDir)        
+    
+    def __del__(self):
+        self.debugFile.close()
+        if self._workDir == None and os.path.exists(self.tempDir):
+            print >> sys.stderr, "Removing temporary classifier work directory", self.tempDir
+            shutil.rmtree(self.tempDir)
+
     def train(self, examples, parameters=None):        
         pass
     
