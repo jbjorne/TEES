6c87d9ac324cca850f678312300bb71c3cceaadb
jari <jari>
2008-11-04 16:28:38 +0000

    Allow different step sizes.

--- JariSandbox/ComplexPPI/Source/Utils/ProgressCounter.py
+++ JariSandbox/ComplexPPI/Source/Utils/ProgressCounter.py
@@ -1,13 +1,14 @@
 import sys
 
 class ProgressCounter:
-    def __init__(self, total, id="UNKNOWN"):
+    def __init__(self, total, id="UNKNOWN", step=5.0):
         self.total = float(total)
         self.current = 0
         self.progress = 0.0
         self.prevProgress = -99.0
         self.id = id
         self.prevUpdateString = "None"
+        self.step = step
     
     def __del__(self):
         # If this counter didn't finish, show the info about the last update
@@ -20,7 +21,7 @@ class ProgressCounter:
         self.current += amount
         self.progress = self.current / self.total * 100.0
         self.prevUpdateString = string + "%.2f" % self.progress + " %"
-        if self.progress >= 100.0 or self.progress - self.prevProgress >= 5.0:
+        if self.progress >= 100.0 or self.progress - self.prevProgress >= self.step:
             print >> sys.stderr, "\r" + self.prevUpdateString,
             self.prevProgress = self.progress
         if self.progress >= 100.0:
