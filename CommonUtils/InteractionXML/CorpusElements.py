80c261693bd021ce63e8e6e3e63bd686131d6a9c
jari <jari>
2008-11-17 16:00:49 +0000

    A new convenience list.

--- CommonUtils/InteractionXML/CorpusElements.py
+++ CommonUtils/InteractionXML/CorpusElements.py
@@ -1,11 +1,28 @@
 from SentenceElements import *
 
+def loadCorpus(filename, parse=None, tokenization=None):
+    try:
+        import xml.etree.cElementTree as ET
+    except ImportError:
+        import cElementTree as ET
+    import sys, gzip
+    
+    print >> sys.stderr, "Loading corpus file", filename
+    if filename.rsplit(".",1)[-1] == "gz":
+        import gzip
+        corpusTree = ET.parse(gzip.open(filename))
+    else:
+        corpusTree = ET.parse(filename)
+    corpusRoot = corpusTree.getroot()
+    return CorpusElements(corpusRoot, parse, tokenization)
+
 class CorpusElements:
     def __init__(self, rootElement, parse, tokenization=None):
         self.rootElement = rootElement
         self.documents = rootElement.findall("document")
         self.documentsById = {}
         self.sentencesById = {}
+        self.sentencesByOrigId = {}
         self.sentences = []
         for documentElement in self.documents:
             self.documentsById[documentElement.attrib["id"]] = documentElement
@@ -13,4 +30,6 @@ class CorpusElements:
             for sentenceElement in sentenceElements:
                 sentenceObj = SentenceElements(sentenceElement, parse, tokenization)
                 self.sentencesById[sentenceElement.attrib["id"]] = sentenceObj
+                if sentenceElement.attrib.has_key("origId"):
+                    self.sentencesByOrigId[sentenceElement.attrib["origId"]] = sentenceObj
                 self.sentences.append(sentenceObj)
