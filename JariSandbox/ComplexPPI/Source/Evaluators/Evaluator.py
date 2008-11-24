97687f9551c52b63e17c261896ecb98c8fc5ee28
jari <jari>
2008-11-24 18:11:39 +0000

    Updated to work with EvaluateCSC.py

--- JariSandbox/ComplexPPI/Source/Evaluators/Evaluator.py
+++ JariSandbox/ComplexPPI/Source/Evaluators/Evaluator.py
@@ -34,19 +34,76 @@ class Evaluator:
                 d["fold"] = fold
         TableUtils.addToCSV(dicts, filename, g_evaluatorFieldnames)
 
-def calculateFromCSV(rows, EvaluatorClass, fold=None, classSet=None):
+def calculateFromCSV(rows, EvaluatorClass, classSet=None):
     predictions = []
     for row in rows:
         if classSet != None:
             predictions.append( ((row["id"],classSet.getId(row["prediction"])),classSet.getId(row["class"])) )
         else:
-            predictions.append( ((row["id"],int(row["prediction"])),int(row["class"])) )
+            predictions.append( ((row["id"],float(row["prediction"])),int(row["class"])) )
     # Calculate statistics
-    evaluator = EvaluatorClass(predictions, classSet)
-    print >> sys.stderr, evaluator.toStringConcise("  ")
-    if options.output != None:
-        evaluator.saveCSV(options.output, fold)
-    return evaluator
+    return EvaluatorClass(predictions, classSet)
+
+def evaluateCSV(rows, options, EvaluatorClass = None):
+    import sys, os
+    sys.path.append("..")
+    from Core.IdSet import IdSet
+    import Utils.TableUtils as TableUtils
+    
+    if EvaluatorClass == None:
+        print >> sys.stderr, "Importing modules"
+        exec "from Evaluators." + options.evaluator + " import " + options.evaluator + " as EvaluatorClass"
+
+    foldDict = {}
+    classNames = set()
+    for row in rows:
+        classNames.add(row["class"])
+        classNames.add(row["prediction"])
+        if row["fold"] != None and row["fold"] != "":
+            if not foldDict.has_key(row["fold"]):
+                foldDict[row["fold"]] = []
+            foldDict[row["fold"]].append(row)
+    
+    # In the case of multiclass, give integer id:s for the classes
+    classSet = None
+    if EvaluatorClass().type == "multiclass":
+        classSet = IdSet()
+        assert(not ("1" in classNames and "neg" in classNames))
+        assert("1" in classNames or "neg" in classNames)
+        if "1" in classNames:
+            classSet.defineId("1",1)
+        else:
+            classSet.defineId("neg",1)
+        for i in sorted(list(classNames)):
+            if i != "1" and i != "neg":
+                classSet.getId(i)
+    
+    # Calculate performance per fold and the averages
+    if len(foldDict) == 0:
+        evaluator = calculateFromCSV(rows, EvaluatorClass, classSet)
+        print >> sys.stderr, evaluator.toStringConcise("  ")
+        if options.output != None:
+            evaluator.saveCSV(options.output)
+    else:
+        evaluators = []
+        for key in sorted(foldDict.keys()):
+            print >> sys.stderr, "Fold", key
+            evaluator = calculateFromCSV(foldDict[key], EvaluatorClass, classSet)
+            print >> sys.stderr, evaluator.toStringConcise("  ")
+            if options.output != None:
+                evaluator.saveCSV(options.output, key)
+            evaluators.append(evaluator)
+
+        print >> sys.stderr, "Averages:"
+        print >> sys.stderr, "Avg"
+        averageResult = EvaluatorClass.average(evaluators)
+        print >> sys.stderr, averageResult.toStringConcise("  ")
+        pooledResult = EvaluatorClass.pool(evaluators)
+        print >> sys.stderr, "Pool"
+        print >> sys.stderr, pooledResult.toStringConcise("  ")
+        if options.output != None:
+            averageResult.saveCSV(options.output, "Avg")
+            pooledResult.saveCSV(options.output, "Pool")
 
 if __name__=="__main__":
     import sys, os
@@ -74,46 +131,4 @@ if __name__=="__main__":
     # Read input data
     fieldnames = ["class","prediction","id","fold"]
     rows = TableUtils.readCSV(options.input, fieldnames)
-    foldDict = {}
-    classNames = set()
-    for row in rows:
-        classNames.add(row["class"])
-        classNames.add(row["prediction"])
-        if row["fold"] != None and row["fold"] != "":
-            if not foldDict.has_key(row["fold"]):
-                foldDict[row["fold"]] = []
-            foldDict[row["fold"]].append(row)
-    
-    # In the case of multiclass, give integer id:s for the classes
-    classSet = None
-    if EvaluatorClass().type == "multiclass":
-        classSet = IdSet()
-        assert(not ("1" in classNames and "neg" in classNames))
-        assert("1" in classNames or "neg" in classNames)
-        if "1" in classNames:
-            classSet.defineId("1",1)
-        else:
-            classSet.defineId("neg",1)
-        for i in sorted(list(classNames)):
-            if i != "1" and i != "neg":
-                classSet.getId(i)
-    
-    # Calculate performance per fold and the averages
-    if len(foldDict) == 0:
-        calculateFromCSV(rows, EvaluatorClass, None, classSet)
-    else:
-        evaluators = []
-        for key in sorted(foldDict.keys()):
-            print >> sys.stderr, "Fold", key
-            evaluators.append(calculateFromCSV(foldDict[key], EvaluatorClass, key, classSet))
-        print >> sys.stderr, "Averages:"
-        print >> sys.stderr, "Avg"
-        averageResult = EvaluatorClass.average(evaluators)
-        print >> sys.stderr, averageResult.toStringConcise("  ")
-        pooledResult = EvaluatorClass.pool(evaluators)
-        print >> sys.stderr, "Pool"
-        print >> sys.stderr, pooledResult.toStringConcise("  ")
-        if options.output != None:
-            averageResult.saveCSV(options.output, "Avg")
-            pooledResult.saveCSV(options.output, "Pool")
-        
+    evaluateCSV(rows, options, EvaluatorClass)
