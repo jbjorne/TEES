d5e5d053b2032c570d880b9f541ccb56ccf76c52
jari <jari>
2008-11-24 18:11:27 +0000

    For evaluating Louhi runs.

--- JariSandbox/ComplexPPI/Source/Evaluators/EvaluateCSC.py
+++ JariSandbox/ComplexPPI/Source/Evaluators/EvaluateCSC.py
@@ -1,27 +1,56 @@
+import sys, os
+sys.path.append("..")
+from Utils.ProgressCounter import ProgressCounter
+from Utils.Parameters import splitParameters
+from optparse import OptionParser
+import Core.ExampleUtils as ExampleUtils
+from Core.IdSet import IdSet
+import Utils.TableUtils as TableUtils
+import Evaluator
+
 if __name__=="__main__":
-    import sys, os
-    sys.path.append("..")
-    from Utils.ProgressCounter import ProgressCounter
-    from Utils.Parameters import splitParameters
-    from optparse import OptionParser
-    import Core.ExampleUtils as ExampleUtils
-    from Core.IdSet import IdSet
-    import Utils.TableUtils as TableUtils
     optparser = OptionParser(usage="%prog [options]\nCalculate f-score and other statistics.")
     optparser.add_option("-i", "--input", default=None, dest="input", help="Input file in csv-format", metavar="FILE")
     optparser.add_option("-p", "--parameters", default=None, dest="parameters", help="Input file in csv-format", metavar="FILE")
     optparser.add_option("-o", "--output", default=None, dest="output", help="Output file for the statistics")
     optparser.add_option("-e", "--evaluator", default="BinaryEvaluator", dest="evaluator", help="Prediction evaluator class")
     (options, args) = optparser.parse_args()
+
+    if options.output != None:
+        print >> sys.stderr, "Outputfile exists, removing", options.output
+        if os.path.exists(options.output):
+            os.remove(options.output)
     
-    # Read input data
+    print >> sys.stderr, "Importing modules"
+    exec "from Evaluators." + options.evaluator + " import " + options.evaluator + " as EvaluatorClass"
     fieldnames = ["class","prediction","id","fold","c"]
-    rows = TableUtils.readCSV(options.input, fieldnames)
+    
+    # Find best c-parameter from parameter estimation data
+    print >> sys.stderr, "Finding optimal c-parameters from", options.parameters    
+    rows = TableUtils.readCSV(options.parameters, fieldnames)
     folds = sorted(list(TableUtils.getValueSet(rows, "fold")))
+    cParameterByFold = {}
     for fold in folds:
+        print >> sys.stderr, "  Processing fold", fold
         foldRows = TableUtils.selectRowsCSV(rows, {"fold":fold})
         cParameters = sorted(list(TableUtils.getValueSet(foldRows, "c")))
+        evaluators = []
+        cParameterByEvaluator = {}
         for cParameter in cParameters:
+            print >> sys.stderr, "    Processing c-parameter", cParameter
             paramRows = TableUtils.selectRowsCSV(rows, {"c":cParameter})
-            evaluator = Evaluator.calculateFromCSV(paramRows)
-            (cParameter, evaluator)
+            evaluator = Evaluator.calculateFromCSV(paramRows, EvaluatorClass)
+            cParameterByEvaluator[evaluator] = cParameter
+            evaluators.append(evaluator)
+        evaluators.sort(Evaluator.compare)
+        print >> sys.stderr, "  Optimal C-parameter:", cParameterByEvaluator[evaluators[-1]]
+        cParameterByFold[fold] = cParameterByEvaluator[evaluators[-1]]
+    
+    print >> sys.stderr, "Evaluating test data from", options.parameters
+    rows = TableUtils.readCSV(options.input, fieldnames)
+    selectedRows = []
+    for fold in folds:
+        foldRows = TableUtils.selectRowsCSV(rows, {"fold":fold})
+        selectedRows.extend( TableUtils.selectRowsCSV(foldRows, {"c":cParameterByFold[fold]}) )
+    
+    Evaluator.evaluateCSV(selectedRows, options, EvaluatorClass)
