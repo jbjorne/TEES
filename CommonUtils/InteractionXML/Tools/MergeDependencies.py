69b467ed14336849a5eae0d8fd5625f1abdbaacf
jari <jari>
2008-11-28 13:36:26 +0000

    Remove dependencies.

--- CommonUtils/InteractionXML/Tools/MergeDependencies.py
+++ CommonUtils/InteractionXML/Tools/MergeDependencies.py
@@ -10,8 +10,9 @@ if __name__=="__main__":
     optparser = OptionParser(usage="%prog [options]\nCopy elements from one interaction XML file to another.")
     optparser.add_option("-i", "--input", default=None, dest="input", help="File from which is read the XML-structure from which elements are copied", metavar="FILE")
     optparser.add_option("-o", "--output", default=None, dest="output", help="The file to which the new XML structure is saved. If None, will be the same as target.", metavar="FILE")
-    optparser.add_option("-p", "--parse1", default=None, dest="parse1", help="The path to elements between which subelements are copied")
-    optparser.add_option("-q", "--parse2", default=None, dest="parse2", help="A comma-separated list of attributes by which 'parent'-elements are matched between source and target")
+    optparser.add_option("-p", "--parse1", default=None, dest="parse1", help="parse element name")
+    optparser.add_option("-q", "--parse2", default=None, dest="parse2", help="parse element name")
+    optparser.add_option("-n", "--name", default=None, dest="name", help="New parse element name")
     (options, args) = optparser.parse_args()
 
     print >> sys.stderr, "Loading input file", options.input
@@ -21,11 +22,13 @@ if __name__=="__main__":
     print >> sys.stderr, "Merging parses"
     parsesElements = sourceRoot.getiterator("parses")
     for parsesElement in parsesElements:
-        newParse = ET.Element("bioinfer_gs_uncollapsed_union")
-        bioinferGSParse = parsesElement.find("bioinfer_gs")
+        newParse = ET.Element(options.name)
+        for parseElement in parsesElement.findall("parse"):
+            if parseElement.
+                parse1 = parsesElement
         for dependencyElement in bioinferGSParse.findall("dependency"):
             newParse.append(dependencyElement)
-        bioinferUncollapsedParse = parsesElement.find("bioinfer_uncollapsed_stanford")
+        parse2 = parsesElement.find(options.parse2)
         for dependencyElement in bioinferUncollapsedParse.findall("dependency"):
             newParse.append(dependencyElement)
         parsesElement.append(newParse)
