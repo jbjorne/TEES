54fd2f805365526a880ed74f0fb9fef259057592
jari <jari>
2009-01-22 14:30:34 +0000

    Script to visualize GENIA sample.

---
+++ GeniaChallenge/formatConversion/visualize-sample-data.sh
@@ -0,0 +1,3 @@
+cd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python VisualizeCorpus.py -i /usr/share/biotext/GeniaChallenge/sample-data/sample-data.xml -o /usr/share/biotext/GeniaChallenge/sample-data/visualization -p Charniak-Lease
+cd ~/cvs_checkout/GeniaChallenge/formatConversion
