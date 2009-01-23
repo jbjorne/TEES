38a29a00ef96922ed4748fc9f692baffd7e12837
juho <juho>
2009-01-23 15:02:53 +0000

    added script for conversion that preserves duplicate nodes and edges

---
+++ GeniaChallenge/formatConversion/convert-all-with-duplicates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DDIR=/usr/share/biotext/GeniaChallenge/orig-data
+THIS=`pwd`
+
+cd $DDIR
+
+echo "Total number of documents `ls *.a1 | wc -l`"
+echo "Training: 600"
+echo "Devel: 200"
+
+
+# Division to sets, only needed once
+# ls *.a1 | perl -pe 's/\.a1//' | awk '{print rand(), $1 }' | sort -n | cut -f 2 -d' ' > IDsSCRAMBLED
+# cat IDsSCRAMBLED | head -n 600 > IDsTRAIN
+# cat IDsSCRAMBLED | tail -n 200 > IDsDEVEL
+
+cat IDsTRAIN | xargs python $THIS/geniaToGifxml.py -p -i . -o ../xml/train-with-duplicates.xml
