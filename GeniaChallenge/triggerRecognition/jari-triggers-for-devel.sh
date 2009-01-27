bc11b51b40c95a99390611e76fa4f0c15e0aabf4
jari <jari>
2009-01-27 13:25:29 +0000

    For running the trigger word detector.

---
+++ GeniaChallenge/triggerRecognition/jari-triggers-for-devel.sh
@@ -0,0 +1,3 @@
+cd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python SplitAnalysis.py -b GeneralEntityTypeRecognizer -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y "c:10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000,20000,50000,80000,100000,150000,200000,500000,1000000,5000000,10000000;timeout:600" -i /usr/share/biotext/GeniaChallenge/xml/train.xml -s /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers -p Charniak-Lease -v /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/visualization
+cd ~/cvs_checkout/GeniaChallenge/triggerRecognition
