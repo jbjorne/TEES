92baac524b6034010d985e60b45f86c66fc79610
jari <jari>
2009-02-04 14:55:12 +0000

    Added a command line interface and the option to pass the test and training set as file names in addition to lists of objects.

--- JariSandbox/ComplexPPI/Source/Core/Classifier.py
+++ JariSandbox/ComplexPPI/Source/Core/Classifier.py
@@ -4,9 +4,12 @@ import combine
 #import Evaluators.Evaluation as EvaluationBase
 import ExampleUtils
 import tempfile
-sys.path.append("..")
+sys.path.append(os.path.dirname(__file__)+"/..")
 import Utils.TableUtils as TableUtils
+from Utils.Parameters import *
+from Utils.Timer import Timer
 import time
+import types
 
 defaultOptimizationParameters = {"c":[0.0001,0.001,0.01,0.1,1,10,100]}
 
@@ -14,6 +17,7 @@ class Classifier:
     def __init__(self):
         self.classSet = None
         self.featureSet = None
+        self.defaultEvaluator = None
         #self.notOptimizedParameters = []
     
     def _makeTempDir(self, workDir=None):
@@ -35,6 +39,8 @@ class Classifier:
         raise NotImplementedError
     
     def filterTrainingSet(self, examples):
+        if self.featureSet == None:
+            return examples
         trainingSet = []
         for example in examples:
             if not example[2].has_key(self.featureSet.getId("always_negative")):
@@ -44,6 +50,8 @@ class Classifier:
     def filterClassificationSet(self, examples, isBinary):
         classificationSet = []
         predictions = []
+        if self.featureSet == None:
+            return examples, predictions
         for example in examples:
             if not example[2].has_key(self.featureSet.getId("always_negative")):
                 classificationSet.append(example)
@@ -111,22 +119,31 @@ class Classifier:
             fold = 1
             foldResults = []
             for classifyExamples in classifySets:
-                trainExamples = []
-                for trainSet in trainSets:
-                    if trainSet != classifyExamples:
-                        trainExamples.extend(trainSet)
-                trainExamplesCopy = ExampleUtils.copyExamples(trainExamples)
-                classifyExamplesCopy = ExampleUtils.copyExamples(classifyExamples)
+                if type(trainSets[0]) == types.StringType:
+                    trainExamples = trainSets[0]
+                else:
+                    trainExamples = []
+                    for trainSet in trainSets:
+                        if trainSet != classifyExamples:
+                            trainExamples.extend(trainSet)
+                trainExamplesCopy = trainExamples
+                if type(trainExamples) == types.ListType:
+                    trainExamplesCopy = trainExamples #ExampleUtils.copyExamples(trainExamples)
+                classifyExamplesCopy = classifyExamples
+                if type(classifyExamples) == types.ListType:
+                    classifyExamplesCopy = classifyExamples #ExampleUtils.copyExamples(classifyExamples)
                 if hasattr(self, "tempDir"):
                     self.tempDir = mainTempDir+"/parameters"+str(combinationCount)+"/optimization"+str(fold)
                     if not os.path.exists(self.tempDir):
                         os.makedirs(self.tempDir)
                     self.debugFile = open(self.tempDir + "/debug.txt", "wt")
                 
-                trainStartTime = time.time()
+                timer = Timer()
+                #trainStartTime = time.time()
                 trainRV = self.train(trainExamplesCopy, combination)
-                trainTime = time.time() - trainStartTime
-                print >> sys.stderr, " Time spent:", trainTime, "s"
+                #trainTime = time.time() - trainStartTime
+                #print >> sys.stderr, " Time spent:", trainTime, "s"
+                print >> sys.stderr, " Time spent:", timer.elapsedTimeToString()
                 if trainRV == 0:
                     predictions = self.classify(classifyExamplesCopy)        
                     evaluation = evaluationClass(predictions, **evaluationArgs)
@@ -165,3 +182,79 @@ class Classifier:
             self.tempDir = mainTempDir
             self.debugFile = mainDebugFile
         return bestResult
+
+if __name__=="__main__":
+    # Import Psyco if available
+    try:
+        import psyco
+        psyco.full()
+        print >> sys.stderr, "Found Psyco, using"
+    except ImportError:
+        print >> sys.stderr, "Psyco not installed"
+
+    from optparse import OptionParser
+    import os
+    optparser = OptionParser(usage="%prog [options]\nCreate an html visualization for a corpus.")
+    optparser.add_option("-t", "--train", default=None, dest="train", help="Corpus in analysis format", metavar="FILE")
+    optparser.add_option("-s", "--test", default=None, dest="test", help="Corpus in analysis format", metavar="FILE")
+    optparser.add_option("-z", "--optimize", default=None, dest="optimize", help="Corpus in analysis format", metavar="FILE")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="Output directory, useful for debugging")
+    optparser.add_option("-c", "--classifier", default="SVMLightClassifier", dest="classifier", help="Classifier Class")
+    optparser.add_option("-p", "--parameters", default=None, dest="parameters", help="Parameters for the classifier")
+    optparser.add_option("-e", "--evaluator", default=None, dest="evaluator", help="Prediction evaluator class")
+    optparser.add_option("-v", "--visualization", default=None, dest="visualization", help="Visualization output directory. NOTE: If the directory exists, it will be deleted!")
+    (options, args) = optparser.parse_args()
+
+    # import classifier
+    print >> sys.stderr, "Importing classifier module"
+    exec "from Classifiers." + options.classifier + " import " + options.classifier + " as Classifier"
+
+    # Create classifier object
+    if options.output != None:
+        classifier = Classifier(workDir = options.output)
+    else:
+        classifier = Classifier()
+    #classifier.featureSet = exampleBuilder.featureSet
+    #if hasattr(exampleBuilder,"classSet"):
+    #    classifier.classSet = None
+    
+    print >> sys.stderr, "Importing evaluator module"
+    if options.evaluator != None:
+        exec "from Evaluators." + options.evaluator + " import " + options.evaluator + " as Evaluator"
+    else:
+        exec "from Evaluators." + classifier.defaultEvaluator + " import " + classifier.defaultEvaluator + " as Evaluator"
+    
+    # Optimize
+    if options.optimize == None:
+        print >> sys.stderr, "Reading train set"
+        trainExamples = ExampleUtils.readExamples(options.train)
+        #print trainExamples[0][0]
+        #print trainExamples[0][1]
+        optimizationSets = ExampleUtils.divideExamples(trainExamples)
+        #print len(optimizationSets[0]), len(optimizationSets[1])
+    else:
+        optimizationSets = [options.train, options.optimize]
+    evaluationArgs = {}#"classSet":exampleBuilder.classSet}
+    if options.parameters != None:
+        paramDict = splitParameters(options.parameters)
+        bestResults = classifier.optimize([optimizationSets[0]], [optimizationSets[1]], paramDict, Evaluator, evaluationArgs)
+    else:
+        bestResults = classifier.optimize([optimizationSets[0]], [optimizationSets[1]], evaluationClass=Evaluator, evaluationArgs=evaluationArgs)
+    
+    # Classify
+    print >> sys.stderr, "Classifying test data"    
+    print >> sys.stderr, "Parameters:", bestResults[2]
+    print >> sys.stderr, "Training",
+    startTime = time.time()
+    classifier.train(options.train, bestResults[2])
+    print >> sys.stderr, "(Time spent:", time.time() - startTime, "s)"
+    print >> sys.stderr, "Testing",
+    startTime = time.time()
+    predictions = classifier.classify(options.test)
+    print >> sys.stderr, "(Time spent:", time.time() - startTime, "s)"
+    
+    # Calculate statistics
+    evaluator = Evaluator(predictions, classSet=None)#exampleBuilder.classSet)
+    print >> sys.stderr, evaluator.toStringConcise()
+    if options.output != None:
+        evaluator.saveCSV(options.output + "/results.csv")
