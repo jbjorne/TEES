d5fce23a697d93e67f1899b808e4cf3c5fae5b55
jari <jari>
2009-02-04 14:50:42 +0000

    For running stuff.

--- GeniaChallenge/triggerRecognition/jari-edges-from-gold-entities.sh
+++ GeniaChallenge/triggerRecognition/jari-edges-from-gold-entities.sh
@@ -1,3 +1,13 @@
-cd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
-python SplitAnalysis.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,maxFeatures" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y "c:10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000,20000,50000,80000,100000,150000,200000,500000,1000000,5000000,10000000;timeout:6000" -i /usr/share/biotext/GeniaChallenge/xml/train.xml -s /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-edges-from-gold-entities -p Charniak-Lease
-cd ~/cvs_checkout/GeniaChallenge/triggerRecognition
+TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train.xml"
+TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel.xml"
+CLASSIFIER_PARAMS="c:10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000,20000,50000,80000,100000,150000,200000,500000,1000000,5000000,10000000;timeout:6000"
+if [ "$1" == "mini" ]; then
+	TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train-mini.xml"
+	TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-mini.xml"
+	#CLASSIFIER_PARAMS="c:10,100,1000,10000,100000,1000000;timeout:600"
+	CLASSIFIER_PARAMS="c:10,100,1000,10000;timeout:600"
+fi
+
+pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python SplitAnalysis.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,no_ne_interactions,maxFeatures" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y $CLASSIFIER_PARAMS -i $TRAIN_FILE -s $TEST_FILE -o /usr/share/biotext/GeniaChallenge/xml/jari-edges-from-gold-entities -m /usr/share/biotext/GeniaChallenge/xml/jari-edges-from-gold-entities/jari-edges-from-gold-entities.xml -p split-Charniak-Lease
+popd
--- GeniaChallenge/triggerRecognition/jari-triggers-for-devel.sh
+++ GeniaChallenge/triggerRecognition/jari-triggers-for-devel.sh
@@ -1,3 +1,12 @@
-cd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
-python SplitAnalysis.py -b GeneralEntityTypeRecognizer -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y "c:10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000,20000,50000,80000,100000,150000,200000,500000,1000000,5000000,10000000;timeout:6000" -i /usr/share/biotext/GeniaChallenge/xml/train.xml -s /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers -p Charniak-Lease -v /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/visualization
-cd ~/cvs_checkout/GeniaChallenge/triggerRecognition
+TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train.xml"
+TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel.xml"
+CLASSIFIER_PARAMS="c:10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000,20000,50000,80000,100000,150000,200000,500000,1000000,5000000,10000000;timeout:6000"
+if [ "$1" == "mini" ]; then
+	TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train-mini.xml"
+	TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-mini.xml"
+	CLASSIFIER_PARAMS="c:1000,10000,100000,1000000;timeout:600"
+fi
+
+pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python SplitAnalysis.py -b GeneralEntityTypeRecognizer -x "style:typed" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y $CLASSIFIER_PARAMS -i $TRAIN_FILE -s $TEST_FILE -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers -m /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers.xml -p split-Charniak-Lease
+popd
