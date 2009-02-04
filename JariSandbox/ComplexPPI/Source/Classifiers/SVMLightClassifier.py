d15e633d23336288bef62515c4c8a7b6f41d6474
jari <jari>
2009-02-04 14:53:36 +0000

    Updated to use JoachimsSVMBase.

--- JariSandbox/ComplexPPI/Source/Classifiers/SVMLightClassifier.py
+++ JariSandbox/ComplexPPI/Source/Classifiers/SVMLightClassifier.py
@@ -6,63 +6,16 @@ import Core.ExampleUtils as Example
 import combine
 #from Core.Evaluation import Evaluation
 from Core.Classifier import Classifier
+from Classifiers.JoachimsSVMBase import JoachimsSVMBase
 import copy
 import time
 import killableprocess
+import types
 
-binDir = "/usr/share/biotext/ComplexPPI/SVMLight"
-
-defaultOptimizationParameters = {"c":[0.0001,0.001,0.01,0.1,1,10,100]}
-
-class SVMLightClassifier(Classifier):
+class SVMLightClassifier(JoachimsSVMBase):
     def __init__(self, workDir=None):
-        #global tempDir
-        
-        self._makeTempDir(workDir)        
-    
-    def __del__(self):
-        self.debugFile.close()
-        if self._workDir == None and os.path.exists(self.tempDir):
-            print >> sys.stderr, "Removing temporary SVM-light work directory", self.tempDir
-            shutil.rmtree(self.tempDir)
-    
-    def train(self, examples, parameters=None):
-        examples = self.filterTrainingSet(examples)
-        timeout = -1
-        parameters = copy.copy(parameters)
-        if parameters.has_key("style"):
-            if "no_duplicates" in parameters["style"]:
-                examples = Example.removeDuplicates(examples)
-            del parameters["style"]
-        if parameters.has_key("timeout"):
-            timeout = parameters["timeout"]
-            del parameters["timeout"]
-        Example.writeExamples(examples, self.tempDir+"/train.dat")
-        args = [binDir+"/svm_learn"]
-        if parameters != None:
-            self.__addParametersToSubprocessCall(args, parameters)
-        args += [self.tempDir+"/train.dat", self.tempDir+"/model"]
-        return killableprocess.call(args, stdout = self.debugFile, timeout = timeout)
-        
-    def classify(self, examples, parameters=None):
-        examples, predictions = self.filterClassificationSet(examples, True)
-        Example.writeExamples(examples, self.tempDir+"/test.dat")
-        args = [binDir+"/svm_classify"]
-        if parameters != None:
-            self.__addParametersToSubprocessCall(args, parameters)
-        args += [self.tempDir+"/test.dat", self.tempDir+"/model", self.tempDir+"/predictions"]
-        #print args
-        subprocess.call(args, stdout = self.debugFile)
-        os.remove(self.tempDir+"/model")
-        predictionsFile = open(self.tempDir+"/predictions", "rt")
-        lines = predictionsFile.readlines()
-        predictionsFile.close()
-        #predictions = []
-        for i in range(len(lines)):
-            predictions.append( (examples[i],float(lines[i]),"binary") )
-        return predictions
-    
-    def __addParametersToSubprocessCall(self, args, parameters):
-        for k,v in parameters.iteritems():
-            args.append("-"+k)
-            args.append(str(v))
+        JoachimsSVMBase.__init__(self, workDir=workDir)
+        self.trainBin = "/usr/share/biotext/ComplexPPI/SVMLight/svm_learn"
+        self.classifyBin = "/usr/share/biotext/ComplexPPI/SVMLight/svm_classify"
+        self.type = "binary"
+        self.defaultEvaluator = "BinaryEvaluator"
--- JariSandbox/ComplexPPI/Source/Classifiers/SVMPerfClassifier.py
+++ JariSandbox/ComplexPPI/Source/Classifiers/SVMPerfClassifier.py
@@ -6,60 +6,30 @@ import Core.ExampleUtils as Example
 import combine
 #from Core.Evaluation import Evaluation
 from Core.Classifier import Classifier
+from Classifiers.JoachimsSVMBase import JoachimsSVMBase
+import types
+import killableprocess
 
-binDir = "/usr/share/biotext/ComplexPPI/SVMPerf"
-
-defaultOptimizationParameters = {"c":[0.0001,0.001,0.01,0.1,1,10,100]}
-
-class SVMPerfClassifier(Classifier):
+class SVMPerfClassifier(JoachimsSVMBase):
     def __init__(self, workDir=None):
-        #global tempDir
+        JoachimsSVMBase.__init__(self, workDir=workDir)
+        self.trainBin = "/usr/share/biotext/ComplexPPI/SVMPerf/svm_perf_learn"
+        self.classifyBin = "/usr/share/biotext/ComplexPPI/SVMPerf/svm_perf_classify"
+        self.type = "binary"
+        self.defaultEvaluator = "BinaryEvaluator"
         
-        self._makeTempDir(workDir)
         self.numberOfTrainingExamples = None
     
-    def __del__(self):
-        self.debugFile.close()
-        if self._workDir == None and os.path.exists(self.tempDir):
-            print >> sys.stderr, "Removing temporary SVM-perf work directory", self.tempDir
-            shutil.rmtree(self.tempDir)
-    
     def train(self, examples, parameters=None):
-        examples = self.filterTrainingSet(examples)
-        parameters = copy.copy(parameters)
-        if parameters.has_key("style") and "no_duplicates" in parameters["style"]:
-            examples = Example.removeDuplicates(examples)
-            del parameters["style"]
+        if type(examples) == types.StringType:
+            trainFilePath = examples
+            f = open(trainFilePath,"rt")
+            self.numberOfTrainingExamples = len(f.readlines())
+            f.close()
+        elif type(examples) == types.ListType:
+            self.numberOfTrainingExamples = len(examples)
         # Convert SVM-light c-values to SVM-perf c-values
-        self.numberOfTrainingExamples = len(examples)
         parameters = copy.deepcopy(parameters)
         parameters["c"] = str(float(parameters["c"]) * self.numberOfTrainingExamples / 100.0)
         # Train
-        Example.writeExamples(examples, self.tempDir+"/train.dat")
-        args = [binDir+"/svm_perf_learn"]
-        if parameters != None:
-            self.__addParametersToSubprocessCall(args, parameters)
-        args += [self.tempDir+"/train.dat", self.tempDir+"/model"]
-        subprocess.call(args, stdout = self.debugFile)
-        
-    def classify(self, examples, parameters=None):
-        examples, predictions = self.filterClassificationSet(examples, True)
-        Example.writeExamples(examples, self.tempDir+"/test.dat")
-        args = [binDir+"/svm_perf_classify"]
-        if parameters != None:
-            self.__addParametersToSubprocessCall(args, parameters)
-        args += [self.tempDir+"/test.dat", self.tempDir+"/model", self.tempDir+"/predictions"]
-        subprocess.call(args, stdout = self.debugFile)
-        os.remove(self.tempDir+"/model")
-        predictionsFile = open(self.tempDir+"/predictions", "rt")
-        lines = predictionsFile.readlines()
-        predictionsFile.close()
-        predictions = []
-        for i in range(len(lines)):
-            predictions.append( (examples[i],float(lines[i]),"binary") )
-        return predictions
-    
-    def __addParametersToSubprocessCall(self, args, parameters):
-        for k,v in parameters.iteritems():
-            args.append("-"+k)
-            args.append(str(v))
+        return JoachimsSVMBase.train(self, examples, parameters)
