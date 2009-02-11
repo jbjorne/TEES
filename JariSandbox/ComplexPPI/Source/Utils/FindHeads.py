979db54d532c05608db32f5f6d8ab173a9abee45
jari <jari>
2009-02-11 16:17:41 +0000

    Hope the path trick works.

--- JariSandbox/ComplexPPI/Source/Utils/FindHeads.py
+++ JariSandbox/ComplexPPI/Source/Utils/FindHeads.py
@@ -1,6 +1,6 @@
 import cElementTreeUtils as ETUtils
-import sys
-sys.path.append("..")
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
 import Core.SentenceGraph as SentenceGraph
 
 if __name__=="__main__":
