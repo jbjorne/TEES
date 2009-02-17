9fcce0fb665d6189078e5ee65578870204a97ed4
jari <jari>
2009-02-17 14:46:16 +0000

    Hopefully a bit more accurate.

--- GeniaChallenge/formatConversion/gifxmlToGenia.py
+++ GeniaChallenge/formatConversion/gifxmlToGenia.py
@@ -29,7 +29,7 @@ def processCorpus(inputCorpus, outputFolder):
         
         writeEventTriggers(document, inputCorpus, outputFile)
         findThemeCausePairs(document, inputCorpus)
-        writeEvents(document, inputCorpus, outputFile)
+        makeEvents(document, inputCorpus, outputFile)
         
         outputFile.close()
 
@@ -40,59 +40,77 @@ def writeEventTriggers(document, inputCorpus, outputFile):
         sentence = inputCorpus.sentencesById[sentenceElement.get("id")]
         entityIndex = getEntityIndex(sentence.entities, entityIndex)
     # Write entities
+    offsetMap = {}
     entityIndex += 1
     for sentenceElement in document.findall("sentence"):
         sentence = inputCorpus.sentencesById[sentenceElement.get("id")]
         sentenceOffset = Range.charOffsetToSingleTuple(sentenceElement.get("charOffset"))
         for entity in sentence.entities:
             if entity.get("isName") == "False":
-                newId = "T" + str(entityIndex)
-                entity.set("temp_newId", newId)
                 entityOffset = Range.charOffsetToSingleTuple(entity.get("charOffset"))
                 newOffset = [entityOffset[0] + sentenceOffset[0], entityOffset[1] + sentenceOffset[1]]
-                outputFile.write( newId + "\t" + entity.get("type") + " " + str(newOffset[0]) + " " + str(newOffset[1]) + "\t" + entity.get("text") + "\n" )
-                entityIndex += 1
+                match = Range.tuplesToCharOffset(newOffset) + "_" + entity.get("type")
+                if match in offsetMap.keys():
+                    entity.set("temp_newId", offsetMap[match].get("temp_newId"))
+                else:
+                    newId = "T" + str(entityIndex)
+                    entity.set("temp_newId", newId)
+                    outputFile.write( newId + "\t" + entity.get("type") + " " + str(newOffset[0]) + " " + str(newOffset[1]) + "\t" + entity.get("text") + "\n" )
+                    offsetMap[match] = entity
+                    entityIndex += 1
             else:
                 entity.set("temp_newId", entity.get("origId").split(".")[-1])
 
-def findThemeCausePairs(document, inputCorpus):
-    for sentenceElement in document.findall("sentence"): 
-        sentence = inputCorpus.sentencesById[sentenceElement.get("id")]
-        sentenceOffset = sentenceElement.get("charOffset")
-        for interaction in sentence.interactions:
-            if interaction.get("type") == "Cause":
-                for interaction2 in sentence.interactions:
-                    if interaction2.get("type") == "Theme":
-                        if interaction2.get("temp_eventCause") != None:
-                            assert(interaction2.get("temp_eventCause") != interaction)
-                        else:
-                            interaction2.set("temp_eventCause", interaction)
-                            interaction.set("temp_eventTheme", interaction2)
-                            print sentence.get("origId")
-
 def writeEvents(document, inputCorpus, outputFile):
-    # Write events
+    """
+    Writes events defined as trigger words that have one or more interactions
+    leaving from them. When the Theme or Cause of such an event refers to a
+    trigger that is also an event, the Theme or Cause will point to that event (E).
+    Note that even if the Theme or Cause is an event-trigger, if that trigger
+    has no defined interactions, it will be annotated as trigger (T), not as 
+    a nested event (E).
+    """
+    events = {} # event trigger entity : list of interactions pairs
     eventIndex = 1
+    eventIds = {}
     for sentenceElement in document.findall("sentence"):
         sentence = inputCorpus.sentencesById[sentenceElement.get("id")]
-        sentenceOffset = sentenceElement.get("charOffset")
-        interactions = sentence.interactions + sentence.pairs
-        for interaction in interactions:
-            type = interaction.get("type")
-            if type == "Theme" or (type == "Cause" and interaction.get("temp_eventTheme") == None):
-                eventId = "E" + str(eventIndex)
-                e1 = sentence.entitiesById[interaction.get("e1")]
-                eventType = e1.get("type")
-                outputLine = eventId + "\t" + eventType + ":" + e1.get("temp_newId")
-                # Add primary Theme/Cause
-                e2 = sentence.entitiesById[interaction.get("e2")]
-                outputLine += " " + type + ":" + e2.get("temp_newId")
-                # Add secondary Cause if it exists
-                if interaction.get("temp_eventCause") != None:
-                    causeTarget = sentence.entitiesById[interaction.get("temp_eventCause").get("e2")]
-                    outputLine += " Cause:" + causeTarget.get("temp_newId")
-                outputFile.write( outputLine + "\n" )
+        sentenceOffset = Range.charOffsetToSingleTuple(sentenceElement.get("charOffset"))
+        
+        # Group interactions by their interaction word, i.e. the event trigger
+        for interaction in sentence.interactions + sentence.pairs:
+            if interaction.get("type") == "neg": # negative prediction
+                continue
+            # All interaction are directed (e1->e2), so e1 is always the trigger
+            e1 = sentence.entitiesById[interaction.get("e1")]
+            assert(e1.get("isName") == "False")
+            if not events.has_key(e1):
+                events[e1] = [] # mark entity as an event trigger
+            events[e1].append(interaction)
+        
+        # Predefine event ids because all ids must be defined so that
+        # we can refer to nested events
+        for entity in sentence.entities:
+            if events.has_key(entity): # entity is an event trigger
+                eventIds[entity] = "E" + str(eventIndex)
                 eventIndex += 1
+        
+        # Write the events
+        for entity in sentence.entities:
+            if events.has_key(entity):
+                eventType = entity.get("type")
+                outputLine = eventIds[entity] + "\t" + eventType + ":" + entity.get("temp_newId")
+                for interaction in events[entity]:
+                    e1 = sentence.entitiesById[interaction.get("e1")]
+                    e2 = sentence.entitiesById[interaction.get("e2")]
+                    assert(e1 == entity)
+                    if eventIds.has_key(e2): # e2 is a nested event
+                        e2Id = eventIds[e2]
+                    else: # e2 is a named entity, i.e. protein
+                        e2Id = e2.get("temp_newId")
+                        #assert(e2.get("isName") == "True")
+                    outputLine += " " + interaction.get("type") + ":" + e2Id
+                outputFile.write( outputLine + "\n" )           
 
 if __name__=="__main__":
     # Import Psyco if available
