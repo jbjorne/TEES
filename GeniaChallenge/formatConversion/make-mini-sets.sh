ff54830bb5e5240b14a53a4f6e7a13bb89e7cd70
jari <jari>
2009-02-25 11:24:22 +0000

    Slightly smaller sets with a different seed.

--- GeniaChallenge/formatConversion/make-mini-sets.sh
+++ GeniaChallenge/formatConversion/make-mini-sets.sh
@@ -1,6 +1,6 @@
 # Mini sets for faster development and testing
 echo "Making mini-sets"
 pushd ~/cvs_checkout/CommonUtils/InteractionXML
-python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train.xml -o /usr/share/biotext/GeniaChallenge/xml/train-mini.xml -f 0.05
-python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -f 0.1
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train.xml -o /usr/share/biotext/GeniaChallenge/xml/train-mini.xml -s 1 -f 0.025
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -s 1 -f 0.05
 popd
