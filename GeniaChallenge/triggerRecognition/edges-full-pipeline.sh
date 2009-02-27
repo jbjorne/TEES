642f89b36b85f899900c370dee6c5aa5a5cdad8c
jari <jari>
2009-02-27 12:43:02 +0000

    Pipeline for P/R optimizing.

---
+++ GeniaChallenge/triggerRecognition/edges-full-pipeline.sh
@@ -0,0 +1,13 @@
+TEST_FILE="$1"
+CLASSIFIER_PARAMS="predefined:$2"
+OUTDIR="$3"
+
+pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python SplitAnalysis.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y $CLASSIFIER_PARAMS -s $TEST_FILE -o $OUTDIR -m $OUTDIR/edges.xml -p split-Charniak-Lease
+python Evaluators/EvaluateInteractionXML.py -i /usr/share/biotext/GeniaChallenge/xml/$OUTDIR/edges.xml -g $GOLD_FILE
+popd
+pushd ~/cvs_checkout/GeniaChallenge/unflattening
+python prune.py -c -i $OUTDIR/edges.xml -o $OUTDIR/pruned.xml
+python unflatten.py -i $OUTDIR/pruned.xml -o $OUTDIR/unflattened.xml
+python ../formatConversion/gifxmlToGenia.py -i $OUTDIR/unflattened.xml -o $OUTDIR/final
+popd
