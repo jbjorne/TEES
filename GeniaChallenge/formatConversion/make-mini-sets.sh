8ce3bad49b1c5ce19e1672efbca1dedc412d48b5
jari <jari>
2009-03-02 18:52:35 +0000

    Make an empty devel-mini-file.

--- GeniaChallenge/formatConversion/make-mini-sets.sh
+++ GeniaChallenge/formatConversion/make-mini-sets.sh
@@ -3,4 +3,5 @@ echo "Making mini-sets"
 pushd ~/cvs_checkout/CommonUtils/InteractionXML
 python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train.xml -o /usr/share/biotext/GeniaChallenge/xml/train-mini.xml -s 1 -f 0.025
 python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -s 1 -f 0.05
+python DeleteElements.py -i /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini-empty.xml -r "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
 popd
