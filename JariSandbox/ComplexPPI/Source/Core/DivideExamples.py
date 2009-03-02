ae69c39b00000957bf40a12b0d099cb5f67480a8
jari <jari>
2009-03-02 18:54:31 +0000

    Now usable as a module.

--- JariSandbox/ComplexPPI/Source/Core/DivideExamples.py
+++ JariSandbox/ComplexPPI/Source/Core/DivideExamples.py
@@ -8,22 +8,9 @@ def getIdFromLine(line):
     assert(line.find("#") != -1)
     return line.split("#")[-1].strip()
 
-if __name__=="__main__":
-    from optparse import OptionParser
-    defaultAnalysisFilename = "/usr/share/biotext/ComplexPPI/BioInferForComplexPPIVisible.xml"
-    optparser = OptionParser(usage="%prog [options]\nCreate an html visualization for a corpus.")
-    optparser.add_option("-i", "--input", default=defaultAnalysisFilename, dest="input", help="Corpus in analysis format", metavar="FILE")
-    optparser.add_option("-o", "--output", default="", dest="output", help="Output directory")
-    optparser.add_option("-f", "--folds", type="int", default=10, dest="folds", help="X-fold cross validation")
-    (options, args) = optparser.parse_args()
-    
-    outputFiles = []
-    for i in range(options.folds):
-        outputFiles.append(open(options.output + options.input + ".fold" + str(i), "wt"))
-    
-    print >> sys.stderr, "Reading document ids"
+def getDocumentIds(filename):
     documentIds = []
-    inputFile = open(options.input, "rt")
+    inputFile = open(filename, "rt")
     try:
         for line in inputFile:
             if len(line) == 0 or line[0] == "#":
@@ -33,15 +20,29 @@ if __name__=="__main__":
                 documentIds.append(docId)
     finally:
         inputFile.close()
+    return documentIds
 
-    print >> sys.stderr, "Dividing documents into folds"
-    sample = Split.getFolds(len(documentIds),options.folds)
+def getDocumentFolds(documentIds, folds):
+    sample = Split.getFolds(len(documentIds),folds)
     division = {}
     for i in range(len(documentIds)): 
         division[documentIds[i]] = sample[i]
+    return division
+
+def divideExamples(filename, outputFilenames):
+    print >> sys.stderr, "Reading document ids"
+    documentIds = getDocumentIds(filename)
+
+    print >> sys.stderr, "Dividing documents into folds"
+    division = getDocumentFolds(documentIds, len(outputFilenames))
 
     print >> sys.stderr, "Dividing examples"
-    inputFile = open(options.input, "rt")
+    
+    outputFiles = []
+    for name in outputFilenames:
+        outputFiles.append(open(name, "wt"))
+
+    inputFile = open(filename, "rt")
     try:
         for line in inputFile:
             if len(line) == 0 or line[0] == "#":
@@ -50,6 +51,21 @@ if __name__=="__main__":
             outputFiles[division[docId]].write(line)
     finally:
         inputFile.close()
-    
+
     for outputFile in outputFiles:
         outputFile.close()
+        
+if __name__=="__main__":
+    from optparse import OptionParser
+    defaultAnalysisFilename = "/usr/share/biotext/ComplexPPI/BioInferForComplexPPIVisible.xml"
+    optparser = OptionParser(usage="%prog [options]\nCreate an html visualization for a corpus.")
+    optparser.add_option("-i", "--input", default=defaultAnalysisFilename, dest="input", help="Corpus in analysis format", metavar="FILE")
+    optparser.add_option("-o", "--output", default="", dest="output", help="Output directory")
+    optparser.add_option("-f", "--folds", type="int", default=10, dest="folds", help="X-fold cross validation")
+    (options, args) = optparser.parse_args()
+    
+    outputFilenames = []
+    for i in range(options.folds):
+        outputFilenames.append(options.output + options.input + ".fold" + str(i))
+
+    divideExamples(options.input, outputFilenames)
