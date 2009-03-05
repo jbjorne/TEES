d898d5ce5cc299dec28fb1a6ee17a14b4f107e78
jari <jari>
2009-03-05 18:25:06 +0000

    *** empty log message ***

---
+++ JariSandbox/ComplexPPI/Source/RealignExamples.py
@@ -0,0 +1,40 @@
+import sys, os, shutil
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import Core.ExampleUtils as Example
+import Core.IdSet as IdSet
+from optparse import OptionParser
+
+if __name__=="__main__":
+    # Import Psyco if available
+    try:
+        import psyco
+        psyco.full()
+        print >> sys.stderr, "Found Psyco, using"
+    except ImportError:
+        print >> sys.stderr, "Psyco not installed"
+    
+    defaultAnalysisFilename = "/usr/share/biotext/ComplexPPI/BioInferForComplexPPIVisible.xml"
+    optparser = OptionParser(usage="%prog [options]\nCreate an html visualization for a corpus.")
+    optparser.add_option("-i", "--invariant", default=None, dest="invariant", help="Corpus in analysis format", metavar="FILE")
+    optparser.add_option("-v", "--variant", default=None, dest="variant", help="Corpus in analysis format", metavar="FILE")
+    
+    #invariantExamples = ExampleUtils.readExamples(os.path.join(options.invariant, "examples.txt"))
+    variantExamples = ExampleUtils.readExamples(os.path.join(options.variant, "test-triggers.examples"))
+    
+    invariantFeatureSet = IdSet()
+    invariantFeatureSet.load(os.path.join(options.invariant, "feature_names.txt"))
+    invariantClassSet = IdSet()
+    invariantClassSet.load(os.path.join(options.invariant, "class_names.txt"))
+
+    variantFeatureSet = IdSet()
+    variantFeatureSet.load(os.path.join(options.variant, "feature_names.txt"))
+    variantClassSet = IdSet()
+    variantClassSet.load(os.path.join(options.variant, "class_names.txt"))
+    
+    for example in variantExamples:
+        example[1] = invariantClassSet.getId(variantClassSet.getName(example[1]))
+        for k,v in example[2]:
+            del example[2][k]
+            example[2][ invariantFeatureSet.getId(variantFeatureSet.getName(k)) ] = v
+    
+    ExampleUtils.writeExamples(os.path.join(options.variant, "realignedExamples.txt"))
