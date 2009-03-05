a7d51ad7d5b5b0a2fbf6b42851a2873c0b3581b1
jari <jari>
2009-03-05 16:24:50 +0000

    There's nothing to split in example files.

--- GeniaChallenge/triggerRecognition/jari-trigger-examples.sh
+++ GeniaChallenge/triggerRecognition/jari-trigger-examples.sh
@@ -1,4 +1,3 @@
 pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
-python Core/ExampleBuilder.py -b GeneralEntityTypeRecognizer -x "style:typed" -i $1 -o $2-with-merged
-python ../../../CommonUtils/InteractionXML/SplitMergedElements.py -i $2-with-merged -o $2
+python Core/ExampleBuilder.py -b GeneralEntityTypeRecognizer -x "style:typed" -i $1 -o $2
 popd
