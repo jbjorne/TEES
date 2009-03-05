cafae08aded6edc97f567284e66b05d58888468b
jari <jari>
2009-03-05 16:23:30 +0000

    Recalculate ids before splitting merged entities.

--- GeniaChallenge/triggerRecognition/jari-triggers-for-devel.sh
+++ GeniaChallenge/triggerRecognition/jari-triggers-for-devel.sh
@@ -23,7 +23,8 @@ fi
 
 pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
 python $PIPELINE -b GeneralEntityTypeRecognizer -x "style:typed" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y $CLASSIFIER_PARAMS -i $TRAIN_FILE -s $TEST_FILE -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers -m /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers-with-merged.xml -p split-Charniak-Lease
-python ../../../CommonUtils/InteractionXML/SplitMergedElements.py -i /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers-with-merged.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers.xml
+python ../../../CommonUtils/InteractionXML/RecalculateIds.py -i /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers-with-merged.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers-with-merged-new-ids.xml
+python ../../../CommonUtils/InteractionXML/SplitMergedElements.py -i /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers-with-merged-new-ids.xml -o /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers.xml
 python Evaluators/EvaluateInteractionXML.py -i /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/jari-devel-triggers.xml -g $GOLD_TEST_FILE
 popd
 cp nohup.out /usr/share/biotext/GeniaChallenge/xml/jari-devel-triggers/nohup.out
