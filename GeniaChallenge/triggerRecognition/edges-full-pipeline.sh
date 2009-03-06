f223c4b7c99fefb101bb20271cc85269cdb5e9b5
ginter <ginter>
2009-03-06 13:08:08 +0000

    *** empty log message ***

--- GeniaChallenge/triggerRecognition/edges-full-pipeline.sh
+++ GeniaChallenge/triggerRecognition/edges-full-pipeline.sh
@@ -5,10 +5,13 @@ GOLD_FILE="/usr/share/biotext/GeniaChallenge/xml/devel.xml"
 
 pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
 python SplitAnalysis.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures" -c SVMMultiClassClassifier -e AveragingMultiClassEvaluator -y $CLASSIFIER_PARAMS -s $TEST_FILE -o $OUTDIR -m $OUTDIR/edges.xml -p split-Charniak-Lease
-python Evaluators/EvaluateInteractionXML.py -i $OUTDIR/edges.xml -g $GOLD_FILE
 popd
 pushd ~/cvs_checkout/GeniaChallenge/unflattening
 python prune.py -c -i $OUTDIR/edges.xml -o $OUTDIR/pruned.xml
 python unflatten.py -i $OUTDIR/pruned.xml -o $OUTDIR/unflattened.xml
 python ../formatConversion/gifxmlToGenia.py -i $OUTDIR/unflattened.xml -o $OUTDIR/final
 popd
+
+cd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
+python Evaluators/EvaluateInteractionXML.py -i $OUTDIR/edges.xml -g $GOLD_FILE
+cd -
