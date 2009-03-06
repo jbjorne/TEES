0526297bfe9221243fd72106a058086c62938fa0
juho <juho>
2009-03-06 19:33:49 +0000

    scripts to print basic statistics from pruning/unflattening logs

---
+++ GeniaChallenge/validation/pruneLog.py
@@ -0,0 +1,54 @@
+import sys
+import re
+
+def process(infile):
+    log = re.compile(r'^(.*?) (.*)')
+    node = re.compile(r'^(.*?) \(.*\)$')
+    edge = re.compile(r'^(.*?) \(.*\) from .*? \(.*\) to .*? \(.*\)$')
+    docs = 0
+    preserved = []
+    removed = []
+    broken = 0
+    for line in infile.readlines():
+        m = log.match(line.strip())
+        if not m:
+            sys.stderr.write("Invalid log line - %s\n"%line.strip())
+            continue
+        if m.group(1) == 'Pruning':
+            docs += 1
+        elif m.group(1) == 'Breaking':
+            broken += 1
+        elif m.group(1) == 'Removed':
+            if edge.match(m.group(2)):
+                removed.append(('e',edge.match(m.group(2)).group(1)))
+            elif node.match(m.group(2)):
+                removed.append(('n',node.match(m.group(2)).group(1)))
+            else:
+                sys.stderr.write("Invalid 'Removed' line\n")
+        elif m.group(1) == 'Preserved':
+            if edge.match(m.group(2)):
+                preserved.append(('e',edge.match(m.group(2)).group(1)))
+            elif node.match(m.group(2)):
+                preserved.append(('n',node.match(m.group(2)).group(1)))
+            else:
+                sys.stderr.write("Invalid 'Preserved' line\n")
+    if docs==0:
+        sys.stderr.write("No documents\n")
+    else:
+        d = float(docs)
+        np = [x[1] for x in preserved if x[0]=='n']
+        ep = [x[1] for x in preserved if x[0]=='e']
+        nr = [x[1] for x in removed if x[0]=='n']
+        er = [x[1] for x in removed if x[0]=='e']
+        sys.stderr.write("Number of documents: %d\n"%(docs))
+        sys.stderr.write("Cycles broken per document: %.3f\n"%(broken/d))
+        sys.stderr.write("Nodes preserved per document: %.3f\n"%(len(np)/d))
+        sys.stderr.write("Edges preserved per document: %.3f\n"%(len(ep)/d))
+        sys.stderr.write("Nodes removed per document: %.3f\n"%(len(nr)/d))
+        sys.stderr.write("Edges removed per document: %.3f\n"%(len(er)/d))
+        sys.stderr.write("Node preserve:remove ratio: %.3f\n"%(len(np)/float(len(nr))))
+        sys.stderr.write("Edge preserve:remove ratio: %.3f\n"%(len(ep)/float(len(er))))
+        
+
+if __name__=="__main__":
+    process(sys.stdin)
---
+++ GeniaChallenge/validation/unflattenLog.py
@@ -0,0 +1,47 @@
+import sys
+import re
+
+def process(infile):
+    log = re.compile(r'^(.*?) (.*)')
+    spl = re.compile(r'^.*? \((.*?)\) into [0-9]+ - .*$')
+    gen = re.compile(r'^Cause-Theme combinations for .*? \((.*?)\).*$')
+    gen2 = re.compile(r'^inter-group pairs for .*? \((.*?)\).*$')
+    docs = 0
+    skips = 0
+    split = []
+    causetheme = []
+    intergroup = []
+    for line in infile.readlines():
+        m = log.match(line.strip())
+        if not m:
+            sys.stderr.write("Invalid log line - %s\n"%line.strip())
+            continue
+        if m.group(1) == 'Unflattening':
+            docs += 1
+        elif m.group(1) == 'Skipping':
+            skips += 1
+        elif m.group(1) == 'Splitting':
+            if spl.match(m.group(2)):
+                split.append(spl.match(m.group(2)).group(1))
+            else:
+                sys.stderr.write("Invalid 'Splitting' line\n")
+        elif m.group(1) == 'Generating':
+            if gen.match(m.group(2)):
+                causetheme.append(gen.match(m.group(2)).group(1))
+            elif gen2.match(m.group(2)):
+                intergroup.append(gen2.match(m.group(2)).group(1))
+            else:
+                sys.stderr.write("Invalid 'Generating' line\n")
+    if docs==0:
+        sys.stderr.write("No documents\n")
+    else:
+        d = float(docs)
+        sys.stderr.write("Number of documents: %d\n"%(docs))
+        sys.stderr.write("Number of skips per document: %d\n"%(skips/d))
+        sys.stderr.write("(all events) Splitting-to-1 per document: %.3f\n"%(len(split)/d))
+        sys.stderr.write("(regulations) Cause-Theme splitting per document: %.3f\n"%(len(causetheme)/d))
+        sys.stderr.write("(binding) Inter-group splitting per document: %.3f\n"%(len(intergroup)/d))
+        sys.stderr.write("(binding) Splitting-to-1:inter-group-pairs ratio: %.3f\n"%(len([x for x in split if x=='Binding'])/float(len(intergroup))))
+
+if __name__=="__main__":
+    process(sys.stdin)
