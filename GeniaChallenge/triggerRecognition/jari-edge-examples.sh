82eed844a09c365e7ac4d1be9c14427ffbf6793b
jari <jari>
2009-03-06 11:20:31 +0000

    added option for predefined classes and features.

--- GeniaChallenge/triggerRecognition/jari-edge-examples.sh
+++ GeniaChallenge/triggerRecognition/jari-edge-examples.sh
@@ -1,3 +1,7 @@
 pushd ~/cvs_checkout/JariSandbox/ComplexPPI/Source
-python Core/ExampleBuilder.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures" -i $1 -o $2
+if [ -n "$3" ]; then
+	python Core/ExampleBuilder.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures" -i $1 -o $2 -d $3
+else
+	python Core/ExampleBuilder.py -b MultiEdgeExampleBuilder -x "style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures" -i $1 -o $2
+fi
 popd
