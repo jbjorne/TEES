524a6d8a3e0d7b0e8d6f9c26c7af7526bd37df3e
jari <jari>
2009-06-22 12:32:44 +0000

    simplified trigger pipeline

--- JariSandbox/ComplexPPI/Source/Pipelines/MiniSharedTask.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/MiniSharedTask.py
@@ -4,20 +4,24 @@ TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train-mini.xml"
 TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-mini.xml"
 GOLD_TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-mini.xml"
 CLASSIFIER_PARAMS="c:1000,10000,100000"
+optimizeLoop = True
 WORKDIR="/usr/share/biotext/GeniaChallenge/temp"
 PARSE_TOK="split-Charniak-Lease"
 
-cwd = os.getcwd()
-os.chdir(WORKDIR)
-Stream.setLog("log.txt", True)
-Stream.setTimeStamp("[%H:%M:%S]", True)
+workdir(WORKDIR, False)
+log()
 
 # Trigger detection
 GeneralEntityTypeRecognizer.run(TRAIN_FILE, "trigger-train-examples", PARSE_TOK, PARSE_TOK, "style:typed", "ids")
 GeneralEntityTypeRecognizer.run(TEST_FILE, "trigger-test-examples", PARSE_TOK, PARSE_TOK, "style:typed", "ids")
-best = optimize(Cls, Ev, "trigger-train-examples", "trigger-test-examples",\
-    "ids.class_names", CLASSIFIER_PARAMS, "trigger-param-opt")
-evaluator = best[0]
+if optimizeLoop:
+    best = optimize(Cls, Ev, "trigger-train-examples", "trigger-test-examples",\
+        "ids.class_names", CLASSIFIER_PARAMS, "trigger-param-opt")
+    evaluator = best[0]
+else:
+    Cls.train("trigger-train-examples", CLASSIFIER_PARAMS, "trigger-model")
+    Cls.test("trigger-test-examples", "trigger-model", "trigger-test-classifications")
+    evaluator = Ev.evaluate("trigger-test-examples", "trigger-test-classifications", "trigger-ids.class_names")
 ExampleUtils.writeToInteractionXML(evaluator.classifications, TEST_FILE, "test-predicted-triggers.xml", "ids.class_names", PARSE_TOK, PARSE_TOK)
 ix.splitMergedElements("test-predicted-triggers.xml", "test-predicted-triggers.xml")
 ix.recalculateIds("test-predicted-triggers.xml", "test-predicted-triggers.xml", True)
@@ -37,5 +41,3 @@ ExampleUtils.writeToInteractionXML(evaluator.classifications, TEST_FILE, "test-p
 ix.splitMergedElements("test-predicted-edges.xml", "test-predicted-edges.xml")
 ix.recalculateIds("test-predicted-edges.xml", "test-predicted-edges.xml", True)
 EvaluateInteractionXML.run(Ev, "test-predicted-edges.xml", GOLD_TEST_FILE, PARSE_TOK, PARSE_TOK)
-
-os.chdir(cwd)
