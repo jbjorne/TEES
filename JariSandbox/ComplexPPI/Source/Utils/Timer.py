49bb80ff34d4ee8dc4b7b34385b072f4e6526dcd
jari <jari>
2009-06-25 15:46:39 +0000

    Added starting and stopping to the timer.

--- JariSandbox/ComplexPPI/Source/Utils/Timer.py
+++ JariSandbox/ComplexPPI/Source/Utils/Timer.py
@@ -1,30 +1,40 @@
 import time
 
 class Timer:
-    def __init__(self):
-        self.startTime = time.time()
-        self.endTime = None
+    def __init__(self, startCount=True):
+        if startCount:
+            self.startTime = time.clock()
+        else:
+            self.startTime = None
+        self.totalTime = 0
     
     def stop(self):
-        self.endTime = time.time()
+        assert(self.startTime != None)
+        self.totalTime += time.clock() - self.startTime
+        self.startTime = None
     
-    def reset(self):
-        self.startTime = time.time()
+    def start(self):
+        assert(self.startTime == None)
+        self.startTime = time.clock()
+        self.endTime = None
     
     def startTimeToString(self):
         return time.ctime(self.startTime)
     
     def elapsedTimeToString(self):
-        if self.endTime == None:
-            now = time.time()
+        if self.startTime != None:
+            now = time.clock()
+            elapsedTime = now-self.startTime + self.totalTime
         else:
-            now = self.endTime
-        secs = int(now-self.startTime)
-        hours = secs / 3600
-        secs = secs % 3600
-        mins = secs / 60
-        secs = secs % 60
-        return str(hours) + ":" + str(mins) + ":" + str(secs)
+            elapsedTime = self.totalTime
+        msecs = int(elapsedTime*1000)
+        hours = msecs / (3600*1000)
+        msecs = msecs % (3600*1000)
+        mins = msecs / (60*1000)
+        msecs = msecs % (60*1000)
+        secs = msecs / (1000)
+        msecs = msecs % (1000)
+        return str(hours) + ":" + str(mins) + ":" + str(secs) + ":" + str(msecs)
     
     def toString(self):
         return "Time: " + self.elapsedTimeToString() + " (start: " + time.ctime(self.startTime) + ", now: " + time.ctime(time.time()) + ")"
