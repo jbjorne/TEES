3784135410d2beea8ac6dce42bc8463af53f9593
ginter <ginter>
2009-08-04 14:39:21 +0000

    Taking the first offset from disc. entities

--- JariSandbox/ComplexPPI/Source/Core/Gazetteer.py
+++ JariSandbox/ComplexPPI/Source/Core/Gazetteer.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
 from Utils.ProgressCounter import ProgressCounter
 
 def charOffStr2tuple(cStr):
-    b,e=cStr.split("-")
+    b,e=(cStr.split(",")[0]).split("-")
     b,e=int(b),int(e)
     return b,e
 
