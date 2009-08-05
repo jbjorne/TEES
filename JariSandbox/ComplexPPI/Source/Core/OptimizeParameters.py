a7586cb02445f159eb956ed39e628bed0e6332ed
jari <jari>
2009-08-05 14:08:06 +0000

    Alternative path with CSCConnection

--- JariSandbox/ComplexPPI/Source/Core/OptimizeParameters.py
+++ JariSandbox/ComplexPPI/Source/Core/OptimizeParameters.py
@@ -1,9 +1,12 @@
 import sys, os, types
 import combine
+import time
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
 from Utils.Parameters import *
 from Utils.Timer import Timer
 import Utils.Stream as Stream
+from Murska.CSCConnection import CSCConnection
+import ExampleUtils
 
 def getCombinationString(combination):
     string = ""
@@ -29,7 +32,7 @@ def getParameterCombinations(parameters):
             combinations[-1][value[0]] = value[1]
     return combinations
 
-def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, parameters, workDir=None, timeout=None):
+def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, parameters, workDir=None, timeout=None, cscConnection=None):
     print >> sys.stderr, "Optimizing parameters"
     if workDir != None:
         if not os.path.exists(workDir):
@@ -39,7 +42,13 @@ def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, param
     if type(parameters) == types.StringType:
         parameters = splitParameters(parameters)
     combinations = getParameterCombinations(parameters)
+    
+    if cscConnection == None:
+        return optimizeLocal(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir, timeout)
+    else:
+        return optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir, timeout, cscConnection)
 
+def optimizeLocal(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir=None, timeout=None):
     bestResult = None
     combinationCount = 1
     for combination in combinations:
@@ -76,4 +85,59 @@ def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, param
         combinationCount += 1
     Stream.setIndent()
     print >> sys.stderr, "Selected parameters", bestResult[-1]
+    return bestResult
+
+def optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir=None, timeout=None, cscConnection=None):
+    bestResult = None
+    combinationCount = 1
+    combinationIds = []
+    print >> sys.stderr, "Initializing runs"
+    for combination in combinations:
+        Stream.setIndent(" ")
+        print >> sys.stderr, "Parameters "+str(combinationCount)+"/"+str(len(combinations))+":", str(combination)
+        # Train
+        combinationIds.append(Classifier.initTrainAndTestOnLouhi(trainExamples, testExamples, combination, cscConnection) )
+        combinationCount += 1
+
+    print >> sys.stderr, "Waiting for results"
+    finished = 0
+    louhiTimer = Timer()
+    while(finished < len(combinations)):
+        # count finished
+        finished = 0
+        for id in combinationIds:
+            if Classifier.getLouhiStatus(id, cscConnection):
+                finished += 1
+        if finished == len(combinations):
+            print >> sys.stderr, "All runs have finished"
+            break
+        # decide what to do
+        if timeout == None or louhiTimer.getElapsedTime() < timeout:
+            print >> sys.stderr, "Waiting for Louhi,", louhiTimer.elapsedTimeToString()
+            time.sleep(60)
+        else:
+            print >> sys.stderr, "Timed out, ", louhiTimer.elapsedTimeToString()
+            break
+    
+    print >> sys.stderr, "Evaluating results"
+    if type(testExamples) != types.ListType:
+        print >> sys.stderr, "Loading examples from file", testExamples
+        testExamples = ExampleUtils.readExamples(testExamples,False)
+    for id in combinationIds:
+        Stream.setIndent(" ")
+        # Evaluate
+        predictions = Classifier.getLouhiPredictions(id, testExamples, cscConnection, workDir)
+        if predictions == None:
+            print >> sys.stderr, "No results for combination" + id
+        else:
+            print >> sys.stderr, "Evaluating results for combination" + id
+            evaluationOutput = "evaluation" + id + ".csv"
+            if workDir != None:
+                evaluationOutput = os.path.join(workDir, evaluationOutput)
+            evaluator = Evaluator.evaluate(testExamples, predictions, classIds, evaluationOutput)
+            if bestResult == None or evaluator.compare(bestResult[0]) > 0: #: averageResult.fScore > bestResult[1].fScore:
+                bestResult = [evaluator, None, None, evaluationOutput, id]
+    Stream.setIndent()
+    print >> sys.stderr, "Selected parameters", bestResult[-1]
+    Classifier.downloadModel(bestResult[-1], cscConnection, workDir)
     return bestResult
