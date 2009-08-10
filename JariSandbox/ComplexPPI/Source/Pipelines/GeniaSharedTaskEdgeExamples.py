38749b2f1b304811d2a78e03758464c1b61aadd7
jari <jari>
2009-08-10 09:34:39 +0000

    support for the new and old files

--- JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskEdgeExamples.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskEdgeExamples.py
@@ -5,13 +5,19 @@ from Pipeline import *
 import os
 
 # define shortcuts for commonly used files
-XMLDIR="/usr/share/biotext/GeniaChallenge/xml"
-TRAIN_FILE=XMLDIR+"/train.xml"
-DEVEL_FILE=XMLDIR+"/devel.xml"
-TEST_FILE="/usr/share/biotext/GeniaChallenge/extension-data/genia/trigger-model/devel-predicted-triggers.xml"
-EVERYTHING_FILE=XMLDIR+"/everything.xml"
+PARSE_TOK="McClosky" #"split-Charniak-Lease"
+CORPUS_DIR=None
+if PARSE_TOK == "split-Charniak-Lease":
+    CORPUS_DIR="/usr/share/biotext/GeniaChallenge/xml/old-interaction-xml-files"
+elif PARSE_TOK == "McClosky":
+    CORPUS_DIR="/usr/share/biotext/GeniaChallenge/xml"
+assert(CORPUS_DIR != None)
+
+TRAIN_FILE=CORPUS_DIR+"/train.xml"
+DEVEL_FILE=CORPUS_DIR+"/devel.xml"
+#TEST_FILE="/usr/share/biotext/GeniaChallenge/extension-data/genia/trigger-model/devel-predicted-triggers.xml"
+EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
 WORKDIR="/usr/share/biotext/GeniaChallenge/extension-data/genia/edge-examples"
-PARSE_TOK="split-Charniak-Lease"
 EDGE_FEATURE_PARAMS="style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures"
 
 # These commands will be in the beginning of most pipelines
@@ -31,6 +37,6 @@ if not os.path.exists("edge-devel-examples-"+PARSE_TOK):
 if not os.path.exists("edge-everything-examples-"+PARSE_TOK):
     MultiEdgeExampleBuilder.run(EVERYTHING_FILE, "edge-everything-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
 
-# This one needs predicted triggers
-if not os.path.exists("edge-test-examples-"+PARSE_TOK):
-    MultiEdgeExampleBuilder.run(TEST_FILE, "edge-test-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
+## This one needs predicted triggers
+#if not os.path.exists("edge-test-examples-"+PARSE_TOK):
+#    MultiEdgeExampleBuilder.run(TEST_FILE, "edge-test-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
