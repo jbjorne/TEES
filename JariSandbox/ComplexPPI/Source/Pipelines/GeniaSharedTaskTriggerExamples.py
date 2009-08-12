3b86633f19493d539210b1f1bf60538fc1a834e6
jari <jari>
2009-08-12 16:08:25 +0000

    task 2

--- JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskTriggerExamples.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskTriggerExamples.py
@@ -15,32 +15,41 @@ elif PARSE_TOK == "split-McClosky":
     CORPUS_DIR="/usr/share/biotext/GeniaChallenge/xml"
 assert(CORPUS_DIR != None)
 
-TRAIN_FILE=CORPUS_DIR+"/train.xml"
-DEVEL_FILE=CORPUS_DIR+"/devel.xml"
-TEST_FILE=CORPUS_DIR+"/test.xml"
-EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
+task = 2
+if task == 1:
+    TRAIN_FILE=CORPUS_DIR+"/train.xml"
+    DEVEL_FILE=CORPUS_DIR+"/devel.xml"
+    TEST_FILE=CORPUS_DIR+"/test.xml"
+    EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
+    TASK_TAG=""
+else:
+    TRAIN_FILE=CORPUS_DIR+"/train12.xml"
+    DEVEL_FILE=CORPUS_DIR+"/devel12.xml"
+    TEST_FILE=CORPUS_DIR+"/test12.xml"
+    EVERYTHING_FILE=CORPUS_DIR+"/everything12.xml"
+    TASK_TAG == "-t12"
 WORKDIR="/usr/share/biotext/GeniaChallenge/extension-data/genia/trigger-examples"
 
 # These commands will be in the beginning of most pipelines
 workdir(WORKDIR, False) # Select a working directory, don't remove existing files
 log() # Start logging into a file in working directory
-print >> sys.stderr, "Trigger example pipeline for parse", PARSE_TOK
+print >> sys.stderr, "Trigger example pipeline for parse", PARSE_TOK, "task", task
 
 ###############################################################################
 # Trigger example generation
 ###############################################################################
 # Old McClosky-Charniak parses
-if not os.path.exists("gazetteer-train-"+PARSE_TOK):
-    Gazetteer.run(TRAIN_FILE, "gazetteer-train-"+PARSE_TOK, PARSE_TOK)
-if not os.path.exists("gazetteer-everything-"+PARSE_TOK):
-    Gazetteer.run(EVERYTHING_FILE, "gazetteer-everything-"+PARSE_TOK, PARSE_TOK)
+if not os.path.exists("gazetteer-train-"+PARSE_TOK+TASK_TAG):
+    Gazetteer.run(TRAIN_FILE, "gazetteer-train-"+PARSE_TOK+TASK_TAG, PARSE_TOK)
+if not os.path.exists("gazetteer-everything-"+PARSE_TOK+TASK_TAG):
+    Gazetteer.run(EVERYTHING_FILE, "gazetteer-everything-"+PARSE_TOK+TASK_TAG, PARSE_TOK)
 # generate the files for the old charniak
-if not os.path.exists("trigger-train-examples-"+PARSE_TOK):
-    GeneralEntityTypeRecognizerGztr.run(TRAIN_FILE, "trigger-train-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train")
-if not os.path.exists("trigger-devel-examples-"+PARSE_TOK):
-    GeneralEntityTypeRecognizerGztr.run(DEVEL_FILE, "trigger-devel-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train")
+if not os.path.exists("trigger-train-examples-"+PARSE_TOK+TASK_TAG):
+    GeneralEntityTypeRecognizerGztr.run(TRAIN_FILE, "trigger-train-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train")
+if not os.path.exists("trigger-devel-examples-"+PARSE_TOK+TASK_TAG):
+    GeneralEntityTypeRecognizerGztr.run(DEVEL_FILE, "trigger-devel-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train")
 
-if not os.path.exists("trigger-everything-examples-"+PARSE_TOK):
-    GeneralEntityTypeRecognizerGztr.run(EVERYTHING_FILE, "trigger-everything-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything")
-if not os.path.exists("trigger-test-examples-"+PARSE_TOK):
-    GeneralEntityTypeRecognizerGztr.run(TEST_FILE, "trigger-test-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything")
+if not os.path.exists("trigger-everything-examples-"+PARSE_TOK+TASK_TAG):
+    GeneralEntityTypeRecognizerGztr.run(EVERYTHING_FILE, "trigger-everything-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything")
+if not os.path.exists("trigger-test-examples-"+PARSE_TOK+TASK_TAG):
+    GeneralEntityTypeRecognizerGztr.run(TEST_FILE, "trigger-test-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything")
