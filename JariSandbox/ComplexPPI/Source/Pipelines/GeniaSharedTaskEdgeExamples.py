42b5f819689199d9c97b27ae3dbd6160380c6857
jari <jari>
2009-08-12 16:14:44 +0000

    task 2

--- JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskEdgeExamples.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskEdgeExamples.py
@@ -13,10 +13,19 @@ elif PARSE_TOK == "split-McClosky":
     CORPUS_DIR="/usr/share/biotext/GeniaChallenge/xml"
 assert(CORPUS_DIR != None)
 
-TRAIN_FILE=CORPUS_DIR+"/train.xml"
-DEVEL_FILE=CORPUS_DIR+"/devel.xml"
-#TEST_FILE="/usr/share/biotext/GeniaChallenge/extension-data/genia/trigger-model/devel-predicted-triggers.xml"
-EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
+task = 2
+if task == 1:
+    TRAIN_FILE=CORPUS_DIR+"/train.xml"
+    DEVEL_FILE=CORPUS_DIR+"/devel.xml"
+    TEST_FILE=CORPUS_DIR+"/test.xml"
+    EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
+    TASK_TAG=""
+else:
+    TRAIN_FILE=CORPUS_DIR+"/train12.xml"
+    DEVEL_FILE=CORPUS_DIR+"/devel12.xml"
+    TEST_FILE=CORPUS_DIR+"/test12.xml"
+    EVERYTHING_FILE=CORPUS_DIR+"/everything12.xml"
+    TASK_TAG="-t12"
 WORKDIR="/usr/share/biotext/GeniaChallenge/extension-data/genia/edge-examples"
 EDGE_FEATURE_PARAMS="style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures"
 
@@ -29,13 +38,13 @@ log() # Start logging into a file in working directory
 ###############################################################################
 # Old McClosky-Charniak parses
 # generate the files for the old charniak
-if not os.path.exists("edge-train-examples-"+PARSE_TOK):
-    MultiEdgeExampleBuilder.run(TRAIN_FILE, "edge-train-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
-if not os.path.exists("edge-devel-examples-"+PARSE_TOK):
-    MultiEdgeExampleBuilder.run(DEVEL_FILE, "edge-devel-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
+if not os.path.exists("edge-train-examples-"+PARSE_TOK+TASK_TAG):
+    MultiEdgeExampleBuilder.run(TRAIN_FILE, "edge-train-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
+if not os.path.exists("edge-devel-examples-"+PARSE_TOK+TASK_TAG):
+    MultiEdgeExampleBuilder.run(DEVEL_FILE, "edge-devel-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
 
-if not os.path.exists("edge-everything-examples-"+PARSE_TOK):
-    MultiEdgeExampleBuilder.run(EVERYTHING_FILE, "edge-everything-examples-"+PARSE_TOK, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
+if not os.path.exists("edge-everything-examples-"+PARSE_TOK+TASK_TAG):
+    MultiEdgeExampleBuilder.run(EVERYTHING_FILE, "edge-everything-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-edge-ids")
 
 ## This one needs predicted triggers
 #if not os.path.exists("edge-test-examples-"+PARSE_TOK):
