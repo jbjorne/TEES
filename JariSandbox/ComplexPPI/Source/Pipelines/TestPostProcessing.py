9e89b8dd99217b08ce97bbf87b6bd3f21aa1b895
juho <juho>
2009-08-19 18:28:17 +0000

    updated new-post-processing section

--- JariSandbox/ComplexPPI/Source/Pipelines/TestPostProcessing.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/TestPostProcessing.py
@@ -3,11 +3,13 @@ import os
 
 # Define working directory
 WORKDIR="/usr/share/biotext/GeniaChallenge/Post-processing-test"
-workdir(WORKDIR, True) # Select a working directory, remove existing files
+workdir(WORKDIR, False) # Select a working directory, remove existing files
 log() # Start logging into a file in working directory
 
 # Current Parse
 PARSE_TOK = "split-McClosky"
+# Current model
+MODEL = 'train'
 # File to be post-processed
 PREDICTED_DEVEL_FILE="/usr/share/biotext/GeniaChallenge/extension-data/genia/test-set-split-McClosky-develDebug/test-predicted-edges.xml"
 
@@ -25,9 +27,25 @@ evaluateSharedTask("geniaformat", 1)
 ###########################################################
 # New post-processing
 ###########################################################
-# NewPostProcessing.interface(PREDICTED_DEVEL_FILE, new-post-process-output.xml)
-# gifxmlToGenia("new-post-process-output.xml", "new-post-process-geniaformat")
-# evaluateSharedTask("new-post-process-geniaformat", 1)
+import subprocess as SP
+args = ['grep', '-v', 'type="neg"', PREDICTED_DEVEL_FILE]
+f = open('tmp','w')
+stdout,stderr = SP.Popen(args,
+                         stderr=None,
+                         stdout=f,
+                         stdin=None).communicate()
+import sys
+sys.path.append("/usr/share/biotext/GeniaChallenge/BEG")
+import BioEventGraph as BEG
+sys.path.pop()
+BEG.scripts.pipeline.interface(['-c', '/usr/share/biotext/GeniaChallenge/BEG/cmds/bionlp09.unmerge-predicted.cmds',
+                                '--input=tmp',
+                                '--output=unflattened-new-pp.xml',
+                                '--parse=parse_%s'%PARSE_TOK,
+                                '--weka=/usr/share/biotext/GeniaChallenge/BEG/models/delmemodels',
+                                '--refweka=/usr/share/biotext/GeniaChallenge/BEG/models/bionlp09.%s'%MODEL])
+gifxmlToGenia("unflattened-new-pp.xml", "new-post-process-geniaformat")
+evaluateSharedTask("new-post-process-geniaformat", 1)
 
 # Compare outputs and make sure approx span & approx recursive f-score
 # is higher for the new post-processing!!!
