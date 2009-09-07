bca1eb4eb50f90917a3280ea41387bd143196c82
jari <jari>
2009-09-07 10:41:31 +0000

    *** empty log message ***

--- JariSandbox/ComplexPPI/Source/Pipelines/BioInferTestSet.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/BioInferTestSet.py
@@ -33,43 +33,43 @@ EDGE_FEATURE_PARAMS="style:typed,directed,no_linear,entities,noMasking,maxFeatur
 
 # start the experiment
 workdir(WORKDIR, False) # Select a working directory, don't remove existing files
-copyIdSetsToWorkdir(TRIGGER_EXAMPLEDIR+"/bioinfer-trigger-ids")
-copyIdSetsToWorkdir(EDGE_EXAMPLEDIR+"/bioinfer-edge-ids")
 log() # Start logging into a file in working directory
-
-print >> sys.stderr, "BioInfer Test Set"
-print >> sys.stderr, "Trigger params", TRIGGER_CLASSIFIER_PARAMS
-#print >> sys.stderr, "Recall Booster params", str(RECALL_BOOST_PARAM)
-print >> sys.stderr, "Edge params", EDGE_CLASSIFIER_PARAMS
-###############################################################################
-# Triggers
-###############################################################################
-#c = CSCConnection(EXPERIMENT_NAME+"/trigger-model", "jakrbj@murska.csc.fi")
-best = optimize(Cls, Ev, TRIGGER_TRAIN_AND_DEVEL_EXAMPLE_FILE, TRIGGER_TEST_EXAMPLE_FILE,\
-    TRIGGER_IDS+".class_names", TRIGGER_CLASSIFIER_PARAMS, "test-trigger-param-opt", None)#, c)
-# The evaluator is needed to access the classifications (will be fixed later)
-ExampleUtils.writeToInteractionXML(TRIGGER_TEST_EXAMPLE_FILE, best[2], TEST_FILE, "test-predicted-triggers.xml", TRIGGER_IDS+".class_names", PARSE, TOK)
-# NOTE: Merged elements must not be split, as recall booster may change their class
-ix.splitMergedElements("test-predicted-triggers.xml", "test-predicted-triggers.xml")
-ix.recalculateIds("test-predicted-triggers.xml", "test-predicted-triggers.xml", True)
-
-###############################################################################
-# Edges
-###############################################################################
-#boostedTriggerFile = "test-predicted-triggers-boost.xml"
-#RecallAdjust.run("test-predicted-triggers.xml", RECALL_BOOST_PARAM, boostedTriggerFile)
-#ix.splitMergedElements(boostedTriggerFile, boostedTriggerFile)
-#ix.recalculateIds(boostedTriggerFile, boostedTriggerFile, True)
-# Build edge examples
-#MultiEdgeExampleBuilder.run(boostedTriggerFile, "test-edge-examples", PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
-MultiEdgeExampleBuilder.run("test-predicted-triggers.xml", "test-edge-examples", PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
-# Classify with pre-defined model
-#c = CSCConnection(EXPERIMENT_NAME+"/edge-model", "jakrbj@murska.csc.fi")
-best = optimize(Cls, Ev, EDGE_TRAIN_AND_DEVEL_EXAMPLE_FILE, "test-edge-examples",\
-    EDGE_IDS+".class_names", EDGE_CLASSIFIER_PARAMS, "test-edge-param-opt", None)#, c)
+#copyIdSetsToWorkdir(TRIGGER_EXAMPLEDIR+"/bioinfer-trigger-ids")
+#copyIdSetsToWorkdir(EDGE_EXAMPLEDIR+"/bioinfer-edge-ids")
+#
+#print >> sys.stderr, "BioInfer Test Set"
+#print >> sys.stderr, "Trigger params", TRIGGER_CLASSIFIER_PARAMS
+##print >> sys.stderr, "Recall Booster params", str(RECALL_BOOST_PARAM)
+#print >> sys.stderr, "Edge params", EDGE_CLASSIFIER_PARAMS
+################################################################################
+## Triggers
+################################################################################
+##c = CSCConnection(EXPERIMENT_NAME+"/trigger-model", "jakrbj@murska.csc.fi")
+#best = optimize(Cls, Ev, TRIGGER_TRAIN_AND_DEVEL_EXAMPLE_FILE, TRIGGER_TEST_EXAMPLE_FILE,\
+#    TRIGGER_IDS+".class_names", TRIGGER_CLASSIFIER_PARAMS, "test-trigger-param-opt", None)#, c)
+## The evaluator is needed to access the classifications (will be fixed later)
+#ExampleUtils.writeToInteractionXML(TRIGGER_TEST_EXAMPLE_FILE, best[2], TEST_FILE, "test-predicted-triggers.xml", TRIGGER_IDS+".class_names", PARSE, TOK)
+## NOTE: Merged elements must not be split, as recall booster may change their class
+#ix.splitMergedElements("test-predicted-triggers.xml", "test-predicted-triggers.xml")
+#ix.recalculateIds("test-predicted-triggers.xml", "test-predicted-triggers.xml", True)
+#
+################################################################################
+## Edges
+################################################################################
+##boostedTriggerFile = "test-predicted-triggers-boost.xml"
+##RecallAdjust.run("test-predicted-triggers.xml", RECALL_BOOST_PARAM, boostedTriggerFile)
+##ix.splitMergedElements(boostedTriggerFile, boostedTriggerFile)
+##ix.recalculateIds(boostedTriggerFile, boostedTriggerFile, True)
+## Build edge examples
+##MultiEdgeExampleBuilder.run(boostedTriggerFile, "test-edge-examples", PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
+#MultiEdgeExampleBuilder.run("test-predicted-triggers.xml", "test-edge-examples", PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
+## Classify with pre-defined model
+##c = CSCConnection(EXPERIMENT_NAME+"/edge-model", "jakrbj@murska.csc.fi")
+#best = optimize(Cls, Ev, EDGE_TRAIN_AND_DEVEL_EXAMPLE_FILE, "test-edge-examples",\
+#    EDGE_IDS+".class_names", EDGE_CLASSIFIER_PARAMS, "test-edge-param-opt", None)#, c)
 # Write to interaction xml
 xmlFilename = "test-predicted-edges.xml"
-ExampleUtils.writeToInteractionXML("test-edge-examples", best[2], boostedTriggerFile, xmlFilename, "genia-edge-ids.class_names", PARSE_TOK, PARSE_TOK)
+ExampleUtils.writeToInteractionXML("test-edge-examples", best[2], "test-predicted-triggers.xml", xmlFilename, "bioinfer-edge-ids.class_names", PARSE, TOK)
 ix.splitMergedElements(xmlFilename, xmlFilename)
 ix.recalculateIds(xmlFilename, xmlFilename, True)
 # EvaluateInteractionXML differs from the previous evaluations in that it can
