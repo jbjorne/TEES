5882f4db9df524832d8d7712c74501c0eef455a7
jari <jari>
2009-09-07 13:15:11 +0000

    let's try NE-recognition

--- JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskTriggerExamples.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/GeniaSharedTaskTriggerExamples.py
@@ -15,20 +15,21 @@ elif PARSE_TOK == "split-McClosky":
     CORPUS_DIR="/usr/share/biotext/GeniaChallenge/xml"
 assert(CORPUS_DIR != None)
 
-task = 1
+task = 2
 if task == 1:
     TRAIN_FILE=CORPUS_DIR+"/train.xml"
     DEVEL_FILE=CORPUS_DIR+"/devel.xml"
     TEST_FILE=CORPUS_DIR+"/test.xml"
     EVERYTHING_FILE=CORPUS_DIR+"/everything.xml"
-    TASK_TAG="-alltokens"
+    TASK_TAG="-gazetteer"
 else:
     TRAIN_FILE=CORPUS_DIR+"/train12.xml"
     DEVEL_FILE=CORPUS_DIR+"/devel12.xml"
     TEST_FILE=CORPUS_DIR+"/test.xml"
     EVERYTHING_FILE=CORPUS_DIR+"/everything12.xml"
-    TASK_TAG="-t12"
+    TASK_TAG="-NE-t12"
 WORKDIR="/usr/share/biotext/GeniaChallenge/extension-data/genia/trigger-examples"
+TRIGGER_FEATURE_PARAMS="style:typed,names"
 
 # These commands will be in the beginning of most pipelines
 workdir(WORKDIR, False) # Select a working directory, don't remove existing files
@@ -45,11 +46,11 @@ if not os.path.exists("gazetteer-everything-"+PARSE_TOK+TASK_TAG):
     Gazetteer.run(EVERYTHING_FILE, "gazetteer-everything-"+PARSE_TOK+TASK_TAG, PARSE_TOK)
 # generate the files for the old charniak
 if not os.path.exists("trigger-train-examples-"+PARSE_TOK+TASK_TAG):
-    GeneralEntityTypeRecognizerGztr.run(TRAIN_FILE, "trigger-train-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train-"+PARSE_TOK+TASK_TAG)
+    GeneralEntityTypeRecognizerGztr.run(TRAIN_FILE, "trigger-train-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, TRIGGER_FEATURE_PARAMS, "genia-trigger-ids", "gazetteer-train-"+PARSE_TOK+TASK_TAG)
 if not os.path.exists("trigger-devel-examples-"+PARSE_TOK+TASK_TAG):
-    GeneralEntityTypeRecognizerGztr.run(DEVEL_FILE, "trigger-devel-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-train-"+PARSE_TOK+TASK_TAG)
+    GeneralEntityTypeRecognizerGztr.run(DEVEL_FILE, "trigger-devel-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, TRIGGER_FEATURE_PARAMS, "genia-trigger-ids", "gazetteer-train-"+PARSE_TOK+TASK_TAG)
 
 if not os.path.exists("trigger-everything-examples-"+PARSE_TOK+TASK_TAG):
-    GeneralEntityTypeRecognizerGztr.run(EVERYTHING_FILE, "trigger-everything-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything-"+PARSE_TOK+TASK_TAG)
+    GeneralEntityTypeRecognizerGztr.run(EVERYTHING_FILE, "trigger-everything-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, TRIGGER_FEATURE_PARAMS, "genia-trigger-ids", "gazetteer-everything-"+PARSE_TOK+TASK_TAG)
 if not os.path.exists("trigger-test-examples-"+PARSE_TOK+TASK_TAG):
-    GeneralEntityTypeRecognizerGztr.run(TEST_FILE, "trigger-test-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, "style:typed", "genia-trigger-ids", "gazetteer-everything-"+PARSE_TOK+TASK_TAG)
+    GeneralEntityTypeRecognizerGztr.run(TEST_FILE, "trigger-test-examples-"+PARSE_TOK+TASK_TAG, PARSE_TOK, PARSE_TOK, TRIGGER_FEATURE_PARAMS, "genia-trigger-ids", "gazetteer-everything-"+PARSE_TOK+TASK_TAG)
