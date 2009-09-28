5c676c8bd1c18b8e1d9bd10d90696f8e94c9782e
jari <jari>
2009-09-28 16:12:36 +0000

    Tested with empty devel set

--- JariSandbox/ComplexPPI/Source/Pipelines/GeniaDirectEventTest.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/GeniaDirectEventTest.py
@@ -10,10 +10,12 @@ FULL_TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train.xml"
 if True: # mini
     TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train-with-duplicates-mini.xml"
     TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-mini.xml"
+    EMPTY_TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-mini-empty.xml"
 else:
     TRAIN_FILE="/usr/share/biotext/GeniaChallenge/xml/train-with-duplicates.xml"
     TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates.xml"
-EDGE_CLASSIFIER_PARAMS="c:0.1,1,10,100,1000,10000,100000,1000000"#"c:10000,28000,50000"
+    EMPTY_TEST_FILE="/usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-empty.xml"
+EDGE_CLASSIFIER_PARAMS="c:1000,10000,100000,500000,1000000,5000000,10000000"#"c:10000,28000,50000"
 optimizeLoop = True # search for a parameter, or use a predefined one
 WORKDIR="/usr/share/biotext/GeniaChallenge/GeniaDirectEventTest"
 PARSE_TOK="split-McClosky"
@@ -35,19 +37,20 @@ else:
 # reduce performance. The gazetteer is built from the full training file,
 # even though the mini-sets are used in the slower parts of this demonstration
 # pipeline.
-if False:
-    Gazetteer.run(FULL_TRAIN_FILE, "gazetteer-train", PARSE_TOK)
+if True:
+    Gazetteer.run(FULL_TRAIN_FILE, "gazetteer-train", PARSE_TOK, "headOffset")
 
 ###############################################################################
 # Edge detection
 ###############################################################################
-if False:
+if True:
     #EDGE_FEATURE_PARAMS="style:typed,directed,entities,genia_limits,noMasking,maxFeatures"
     EDGE_FEATURE_PARAMS="style:typed,directed,no_linear,entities,genia_limits,noMasking,maxFeatures"
         
     # Build examples, see trigger detection
     DirectEventExampleBuilder.run(TRAIN_FILE, "event-train-examples", PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-direct-event-ids", "gazetteer-train")
     DirectEventExampleBuilder.run(TEST_FILE, "event-test-examples", PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-direct-event-ids", "gazetteer-train")
+    DirectEventExampleBuilder.run(EMPTY_TEST_FILE, "event-test-empty-examples", PARSE_TOK, PARSE_TOK, EDGE_FEATURE_PARAMS, "genia-direct-event-ids", "gazetteer-train")
     # Run the optimization loop. Note that here we must optimize against the gold
     # standard examples, because we do not know real classes of edge examples built between
     # predicted triggers
@@ -59,21 +62,24 @@ if True:
         c = CSCConnection("GeniaDirectEventTest-event-model", "jakrbj@murska.csc.fi", False)
     best = optimize(MyCls, Ev, "event-train-examples", "event-test-examples",\
         "genia-direct-event-ids.class_names", EDGE_CLASSIFIER_PARAMS, "event-param-opt", None, c)
+    MyCls.test("event-test-empty-examples", best[1], "event-test-empty-classifications")
     
-if False:
+if True:
+    best = [None,None,"event-test-empty-classifications"]
+    #best = [None,None,"event-param-opt/predictions-c_1000000"]
     # Write the predicted edges to an interaction xml which has predicted triggers.
     # This function handles both trigger and edge example classifications
-    edgeXml = ExampleUtils.writeToInteractionXML("edge-test-examples", "edge-test-classifications", TEST_WITH_PRED_TRIGGERS_FILE, None, "ids.edge.class_names", PARSE_TOK, PARSE_TOK)
+    edgeXml = ExampleUtils.writeToInteractionXML("event-test-empty-examples", best[2], TEST_FILE, None, "genia-direct-event-ids.class_names", PARSE_TOK, PARSE_TOK)
     # Split overlapping, merged elements (e.g. "Upregulate---Phosphorylate")
-    ix.splitMergedElements(edgeXml)
+    #ix.splitMergedElements(edgeXml)
     ## Always remember to fix ids
     ix.recalculateIds(edgeXml, None, True)
-    writeXML(edgeXml, "test-predicted-edges.xml")
+    writeXML(edgeXml, "test-predicted-events.xml")
     # EvaluateInteractionXML differs from the previous evaluations in that it can
     # be used to compare two separate GifXML-files. One of these is the gold file,
     # against which the other is evaluated by heuristically matching triggers and
     # edges. Note that this evaluation will differ somewhat from the previous ones,
     # which evaluate on the level of examples.
-    EvaluateInteractionXML.run(Ev, edgeXml, GOLD_TEST_FILE, PARSE_TOK, PARSE_TOK)
-    gifxmlToGenia("test-predicted-edges.xml", "geniaformat")
+    EvaluateInteractionXML.run(Ev, "test-predicted-events.xml", TEST_FILE, PARSE_TOK, PARSE_TOK)
+    gifxmlToGenia("test-predicted-events.xml", "geniaformat", 1)
     evaluateSharedTask("geniaformat", 1)
