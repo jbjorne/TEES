b9eca541cccccb3e181fe199e2420471ac70fca3
jari <jari>
2009-09-28 16:04:19 +0000

    *** empty log message ***

--- GeniaChallenge/formatConversion/make-mini-sets.sh
+++ GeniaChallenge/formatConversion/make-mini-sets.sh
@@ -1,6 +1,7 @@
 # Mini sets for faster development and testing
 echo "Making mini-sets"
 pushd ~/cvs_checkout/CommonUtils/InteractionXML
+# mini files for flattened
 python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train.xml -o /usr/share/biotext/GeniaChallenge/xml/train-mini.xml -s 1 -f 0.025
 python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -s 1 -f 0.05
 python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train12.xml -o /usr/share/biotext/GeniaChallenge/xml/train12-mini.xml -s 1 -f 0.125
@@ -8,4 +9,16 @@ python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel12.xml -o /usr/sh
 
 python DeleteElements.py -i /usr/share/biotext/GeniaChallenge/xml/devel-mini.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-mini-empty.xml -r "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
 python DeleteElements.py -i /usr/share/biotext/GeniaChallenge/xml/devel12-mini.xml -o /usr/share/biotext/GeniaChallenge/xml/devel12-mini-empty.xml -r "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
+
+# mini files for nonflattened
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train-with-duplicates.xml -o /usr/share/biotext/GeniaChallenge/xml/train-with-duplicates-mini.xml -s 1 -f 0.025
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-mini.xml -s 1 -f 0.05
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/train-with-duplicates12.xml -o /usr/share/biotext/GeniaChallenge/xml/train-with-duplicates12-mini.xml -s 1 -f 0.125
+python Subset.py -i /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates12.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates12-mini.xml -s 1 -f 0.25
+
+python DeleteElements.py -i /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-mini.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-mini-empty.xml -r "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
+
+# other empty sets
+python DeleteElements.py -i /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates.xml -o /usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates-empty.xml -r "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
+
 popd
