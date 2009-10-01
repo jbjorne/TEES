b3da0a11dd43ddf13f6702e41635d6eacfa903ae
jari <jari>
2009-10-01 10:35:56 +0000

    added timed output

--- JariSandbox/ComplexPPI/Source/Utils/ProgressCounter.py
+++ JariSandbox/ComplexPPI/Source/Utils/ProgressCounter.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 
 class ProgressCounter:
     def __init__(self, total, id="UNKNOWN", step=5.0):
@@ -9,6 +9,9 @@ class ProgressCounter:
         self.id = id
         self.prevUpdateString = "None"
         self.step = step
+        
+        self.prevPrintTime = 0
+        self.timeStep = 3000
     
     def __del__(self):
         # If this counter didn't finish, show the info about the last update
@@ -21,7 +24,14 @@ class ProgressCounter:
         self.current += amount
         self.progress = self.current / self.total * 100.0
         self.prevUpdateString = string + "%.2f" % self.progress + " %"
-        if self.progress >= 100.0 or self.progress - self.prevProgress >= self.step:
+        
+        currentTime = time.time()
+        timeStepExceeded = False
+        if currentTime - self.prevPrintTime > self.timeStep:
+            timeStepExceeded = True
+            self.prevPrintTime = currentTime
+        
+        if self.progress >= 100.0 or self.progress - self.prevProgress >= self.step or timeStepExceeded:
             print >> sys.stderr, "\r" + self.prevUpdateString,
             self.prevProgress = self.progress
         if self.progress >= 100.0:
