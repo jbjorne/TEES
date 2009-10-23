787b66916e9ea33983b8acf6e6e6330c6397f49e
jari <jari>
2009-10-23 13:39:06 +0000

    Replicates extension paper devel result of 55.47.

--- JariSandbox/ComplexPPI/Source/Settings.py
+++ JariSandbox/ComplexPPI/Source/Settings.py
@@ -1,4 +1,18 @@
+# External binaries
 SVMMultiClassDir = "/usr/share/biotext/ComplexPPI/SVMMultiClass"
+
+# BioNLP'09 dataset in XML-format
+TrainFile="/usr/share/biotext/GeniaChallenge/xml/train12.xml"
+DevelFile="/usr/share/biotext/GeniaChallenge/xml/devel12.xml"
+
+# Precalculated SVM-multiclass models
+DevelTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/devel-trigger-model-c_200000"
+DevelEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/devel-edge-model-c_28000"
+DevelAndTestTriggerModel=""
+DevelAndTestEdgeModel=""
+
+TriggerIds="/usr/share/biotext/GeniaChallenge/release-files/genia-trigger-ids"
+EdgeIds="/usr/share/biotext/GeniaChallenge/release-files/genia-edge-ids"
 
 # Genia Components
 #evaluationsoftware
