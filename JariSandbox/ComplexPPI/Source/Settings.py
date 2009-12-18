52ad58918ea45b0f9511233b9587166c2c77314e
jari <jari>
2009-12-18 14:26:22 +0000

    Proper settings

--- JariSandbox/ComplexPPI/Source/Settings.py
+++ JariSandbox/ComplexPPI/Source/Settings.py
@@ -3,17 +3,21 @@ thisPath = os.path.dirname(os.path.abspath(__file__))
 
 # External binaries
 SVMMultiClassDir = "/usr/share/biotext/ComplexPPI/SVMMultiClass"
+#IF NOT RELEASE
 CPPTriggerExampleBuilder = os.path.abspath(thisPath + "/../../../GraphIE/exampleTest")
+#ENDIF
 
 # BioNLP'09 dataset in XML-format
-TrainFile="/usr/share/biotext/GeniaChallenge/xml/train12.xml"
-DevelFile="/usr/share/biotext/GeniaChallenge/xml/devel12.xml"
+TrainFile="/usr/share/biotext/GeniaChallenge/xml/train12-mini.xml"
+DevelFile="/usr/share/biotext/GeniaChallenge/xml/devel12-mini.xml"
+EverythingFile="/usr/share/biotext/GeniaChallenge/xml/everything123.xml"
+TestFile="/usr/share/biotext/GeniaChallenge/xml/test.xml"
 
 # Precalculated SVM-multiclass models
-DevelTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/devel-trigger-model-c_200000"
-DevelEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/devel-edge-model-c_28000"
-DevelAndTestTriggerModel=""
-DevelAndTestEdgeModel=""
+TrainTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/train-trigger-model-c_100000"
+TrainEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/train-edge-model-c_60000"
+EverythingTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/everything-trigger-model-c_100000"
+EverythingEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/everything-edge-model-c_60000"
 
 TriggerIds="/usr/share/biotext/GeniaChallenge/release-files/genia-trigger-ids"
 EdgeIds="/usr/share/biotext/GeniaChallenge/release-files/genia-edge-ids"
