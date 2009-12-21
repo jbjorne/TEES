abb04e7d709f35f6c14ddaf475f30c81c73cce52
jari <jari>
2009-12-21 14:08:49 +0000

    Now also removes existing head offsets.

--- JariSandbox/ComplexPPI/Source/Utils/FindHeads.py
+++ JariSandbox/ComplexPPI/Source/Utils/FindHeads.py
@@ -16,15 +16,25 @@ if __name__=="__main__":
     except ImportError:
         print >> sys.stderr, "Psyco not installed"
 
-    defaultCorpusFilename = "BioInfer.xml"
-    defaultOutputName = "BioInfer.xml"
-    optparser = OptionParser(usage="%prog [options]\nPath generator.")
-    optparser.add_option("-i", "--input", default=defaultCorpusFilename, dest="input", help="Corpus in interaction xml format", metavar="FILE")
-    optparser.add_option("-o", "--output", default=defaultOutputName, dest="output", help="Output file in interaction xml format.")
-    optparser.add_option("-t", "--tokenization", default=None, dest="tokenization", help="Tokenization element name")
-    optparser.add_option("-p", "--parse", default=None, dest="parse", help="Parse element name")
+    optparser = OptionParser(usage="%prog [options]\nRecalculate head token offsets.")
+    optparser.add_option("-i", "--input", default=None, dest="input", help="Corpus in interaction xml format", metavar="FILE")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="Output file in interaction xml format.")
+    optparser.add_option("-t", "--tokenization", default=None, dest="tokenization", help="Tokenization element name for calculating head offsets")
+    optparser.add_option("-p", "--parse", default=None, dest="parse", help="Parse element name for calculating head offsets")
     (options, args) = optparser.parse_args()
-
+    
+    print >> sys.stderr, "Removing existing head offsets"
+    removeCount = 0
+    xml = ETUtils.ETFromObj(options.input)
+    for d in xml.getroot().findall("document"):
+        for s in d.findall("sentence"):
+            for e in s.findall("entity"):
+                if e.get("headOffset") != None:
+                    removeCount += 1
+                    del e.attrib["headOffset"]
+    print >> sys.stderr, "Removed head offsets from", removeCount, "entities"
+    
+    # SentenceGraph automatically calculates head offsets and adds them to entities if they are missing
     corpusElements = SentenceGraph.loadCorpus(options.input, options.parse, options.tokenization)
     print >> sys.stderr, "Writing output to", options.output
     ETUtils.write(corpusElements.rootElement, options.output)
