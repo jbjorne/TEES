5624fcbf57efd4406257b8e4b15c8ab97d06b210
jari <jari>
2010-02-12 16:37:45 +0000

    *** empty log message ***

--- JariSandbox/ComplexPPI/Source/Settings.py
+++ JariSandbox/ComplexPPI/Source/Settings.py
@@ -8,19 +8,26 @@ CPPTriggerExampleBuilder = os.path.abspath(thisPath + "/../../../GraphIE/example
 #ENDIF
 
 # BioNLP'09 dataset in XML-format
-TrainFile="/usr/share/biotext/GeniaChallenge/xml/train12-mini.xml"
-DevelFile="/usr/share/biotext/GeniaChallenge/xml/devel12-mini.xml"
+TrainFile="/usr/share/biotext/GeniaChallenge/xml/train123.xml"
+DevelFile="/usr/share/biotext/GeniaChallenge/xml/devel123.xml"
+DevelFileWithDuplicates="/usr/share/biotext/GeniaChallenge/xml/devel-with-duplicates123.xml"
 EverythingFile="/usr/share/biotext/GeniaChallenge/xml/everything123.xml"
 TestFile="/usr/share/biotext/GeniaChallenge/xml/test.xml"
 
+RELEASE_FILES_PATH = "/usr/share/biotext/GeniaChallenge/CI-release/release-files/release-files-review-version-models/"
 # Precalculated SVM-multiclass models
-TrainTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/train-trigger-model-c_100000"
-TrainEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/train-edge-model-c_60000"
-EverythingTriggerModel="/usr/share/biotext/GeniaChallenge/release-files/everything-trigger-model-c_100000"
-EverythingEdgeModel="/usr/share/biotext/GeniaChallenge/release-files/everything-edge-model-c_60000"
+TrainTriggerModel = RELEASE_FILES_PATH + "train-trigger-model-c_100000"
+TrainEdgeModel = RELEASE_FILES_PATH + "train-edge-model-c_60000"
+TrainSpeculationModel = RELEASE_FILES_PATH + "train-speculation-model-c_13000"
+TrainNegationModel = RELEASE_FILES_PATH + "train-negation-model-c_10000"
+EverythingTriggerModel = RELEASE_FILES_PATH + "everything-trigger-model-c_100000"
+EverythingEdgeModel = RELEASE_FILES_PATH + "everything-edge-model-c_60000"
+EverythingSpeculationModel = RELEASE_FILES_PATH + "everything-speculation-model-c_13000"
+EverythingNegationModel = RELEASE_FILES_PATH + "everything-negation-model-c_10000"
 
-TriggerIds="/usr/share/biotext/GeniaChallenge/release-files/genia-trigger-ids"
-EdgeIds="/usr/share/biotext/GeniaChallenge/release-files/genia-edge-ids"
+TriggerIds = RELEASE_FILES_PATH + "genia-trigger-ids"
+EdgeIds = RELEASE_FILES_PATH + "genia-edge-ids"
+Task3Ids = RELEASE_FILES_PATH + "genia-task3-ids"
 
 # Genia Components
 #evaluationsoftware
