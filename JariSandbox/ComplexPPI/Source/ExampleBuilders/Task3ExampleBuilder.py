4247e958ffe2263852042fc40387ae76acdcae49
jari <jari>
2010-03-19 15:29:47 +0000

    Removed the id that gets accidentally into classSet from the classSet

--- JariSandbox/ComplexPPI/Source/ExampleBuilders/Task3ExampleBuilder.py
+++ JariSandbox/ComplexPPI/Source/ExampleBuilders/Task3ExampleBuilder.py
@@ -1,7 +1,7 @@
 """
 Speculation and negation examples
 """
-__version__ = "$Revision: 1.6 $"
+__version__ = "$Revision: 1.7 $"
 
 import sys, os
 thisPath = os.path.dirname(os.path.abspath(__file__))
@@ -231,6 +231,21 @@ class Task3ExampleBuilder(ExampleBuilder):
 
             # ENTITY TYPE
             entityType = self.classSet.getId(self.getMergedEntityType(entity))
+            del self.classSet.Ids[self.getMergedEntityType(entity)]
+#IF LOCAL
+            # There's a mistake here. The entityType should be the string, not
+            # the id of the type. But there's also another issue. getMergedEntityType
+            # expects a list, not an item. Therefore the type is always empty ->
+            # types don't get used in classification. But this is the code used in
+            # the publication, so it will now be published as is, and fixed in a later
+            # release.
+            #
+            # Besides, using the classSet here generates an unneeded
+            # additional class, that shows up in evaluations etc. However, to be 
+            # able to publish the exact models used for the publication experiments,
+            # this can't be fixed so it breaks feature id consistency. Therefore I'll
+            # now just remove the redundant class id from the classSet.
+#ENDIF            
             features[self.featureSet.getId(entityType)] = 1
             
             features[self.featureSet.getId(namedEntityCountFeature)] = 1
