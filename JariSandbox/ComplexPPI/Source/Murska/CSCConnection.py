d7940d60d088213784014d7c763ebdf26e4bd8fd
jari <jari>
2010-03-22 17:35:16 +0000

    lots of new stuff

--- JariSandbox/ComplexPPI/Source/Murska/CSCConnection.py
+++ JariSandbox/ComplexPPI/Source/Murska/CSCConnection.py
@@ -19,9 +19,19 @@ class CSCConnection:
         self.NOT_EXIST = "NOT_EXIST"
         self.NONZERO = "NONZERO"
         self.ZERO = "ZERO"
+        
+        # Batch job queue
+        self.commands = []
     
     def exists(self, filename):
         p = subprocess.Popen("ssh " + self.account + " 'ls " + self.workDir + "/" + filename + "'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if len(p.stdout.readlines()) > 0:
+            return True
+        else:
+            return False
+    
+    def mkdir(self, dir):
+        p = subprocess.Popen("ssh " + self.account + " 'mkdir -p " + self.workDir + "/" + dir + "'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if len(p.stdout.readlines()) > 0:
             return True
         else:
@@ -67,7 +77,52 @@ class CSCConnection:
         subprocess.call("scp scp-test-file.txt jakrbj@louhi.csc.fi:/home/u1/jakrbj/scp-test-file.txt", shell=True)
         #p = subprocess.Popen("ls", stdout=subprocess.PIPE)
         #print p.stdout.readlines()
+    
+    def beginJob(self):
+        self.commands = []
+    
+    def addCommand(self, string):
+        self.commands.append(string)
+    
+    def submitJob(self, localWorkDir, name):
+        if self.account.find("murska") != -1:
+            isMurska = True
+        else:
+            isMurska = False
+        scriptName = name
+        if cscConnection.exists(scriptName):
+            print >> sys.stderr, "Script already on " + cscConnection.machineName + ", process not queued for", scriptName
+            return False
         
+        # Build script
+        scriptFilePath = scriptName
+        if localWorkDir != None:
+            scriptFilePath = os.path.join(localWorkDir, scriptName)
+        scriptFile = open(scriptFilePath, "wt")
+        scriptFile.write("#!/bin/bash\ncd " + cscConnection.workDir + "\n")
+        # write commands to script
+        for command in commands:
+            if not isMurska: # louhi
+                scriptFile.write("aprun -n 1 ")
+            scriptFile.write(command + "\n")
+        scriptFile.close()
+        
+        return
+        
+        cscConnection.upload(scriptFilePath, scriptName)
+        cscConnection.run("chmod a+x " + cscConnection.workDir + "/" + scriptName)
+        cscScriptPath = self.workDir + "/" + scriptName
+        if isMurska:
+            runCmd = "bsub -o " + cscScriptPath + "-stdout -e " + cscScriptPath + "-stderr -W 10:0 -M " + str(self.memory) 
+            if cscConnection.cores != 1:
+                runCmd += " -n " + str(self.cores)
+            runCmd += " < " + cscScriptPath
+            cscConnection.run(runCmd)
+        else:
+            cscConnection.run("qsub -o " + cscConnection.workDir + "/" + scriptName + "-stdout -e " + cscConnection.workDir + "/" + scriptName + "-stderr " + cscConnection.workDir + "/" + scriptName)
+        
+        self.commands = []
+        return idStr
 
 if __name__=="__main__":
     c = CSCConnection("remoteTest")
