c51e9103c43aae2ab262a7ed3023afff79deadbf
jari <jari>
2010-03-22 17:34:19 +0000

    Added option to submit first, then later check whether they are finished

--- JariSandbox/ComplexPPI/Source/Core/OptimizeParameters.py
+++ JariSandbox/ComplexPPI/Source/Core/OptimizeParameters.py
@@ -37,7 +37,7 @@ def getParameterCombinations(parameters):
             combinations[-1][value[0]] = value[1]
     return combinations
 
-def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, parameters, workDir=None, timeout=None, cscConnection=None, downloadAllModels=False):
+def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, parameters, workDir=None, timeout=None, cscConnection=None, downloadAllModels=False, steps="BOTH"):
     print >> sys.stderr, "Optimizing parameters"
     if workDir != None:
         if not os.path.exists(workDir):
@@ -51,7 +51,7 @@ def optimize(Classifier, Evaluator, trainExamples, testExamples, classIds, param
     if cscConnection == None:
         return optimizeLocal(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir, timeout)
     else:
-        return optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir, timeout, cscConnection, downloadAllModels)
+        return optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir, timeout, cscConnection, downloadAllModels, steps)
 
 def optimizeLocal(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir=None, timeout=None):
     bestResult = None
@@ -93,81 +93,89 @@ def optimizeLocal(Classifier, Evaluator, trainExamples, testExamples, classIds, 
     return bestResult
 
 #IF LOCAL
-def optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir=None, timeout=None, cscConnection=None, downloadAllModels=False):
+def optimizeCSC(Classifier, Evaluator, trainExamples, testExamples, classIds, combinations, workDir=None, timeout=None, cscConnection=None, downloadAllModels=False, steps="BOTH"):
     bestResult = None
     combinationCount = 1
     combinationIds = []
-    print >> sys.stderr, "Initializing runs"
-    for combination in combinations:
-        Stream.setIndent(" ")
-        print >> sys.stderr, "Parameters "+str(combinationCount)+"/"+str(len(combinations))+":", str(combination)
-        # Train
-        combinationIds.append(Classifier.initTrainAndTestOnLouhi(trainExamples, testExamples, combination, cscConnection) )
-        combinationCount += 1
-
-    print >> sys.stderr, "Waiting for results"
-    finished = 0
-    louhiTimer = Timer()
-    combinationStatus = {}
-    while(True):
-        # count finished
-        finished = 0
-        processStatus = {"FINISHED":0, "QUEUED":0, "FAILED":0, "RUNNING":0}
-        for id in combinationIds:
-            status = Classifier.getLouhiStatus(id, cscConnection)
-            combinationStatus[id] = status
-            processStatus[status] += 1
-        p = processStatus
-        processStatusString = str(p["QUEUED"]) + " queued, " + str(p["RUNNING"]) + " running, " + str(p["FINISHED"]) + " finished, " + str(p["FAILED"]) + " failed"
-        if processStatus["QUEUED"] + processStatus["RUNNING"] == 0:
-            print >> sys.stderr
-            print >> sys.stderr, "All runs done (" + processStatusString + ")"
-            break
-        # decide what to do
-        if timeout == None or louhiTimer.getElapsedTime() < timeout:
-            sleepString = " [          ]     "
-            print >> sys.stderr, "\rWaiting for " + str(len(combinations)) + " on " + cscConnection.machineName + "(" + processStatusString + "),", louhiTimer.elapsedTimeToString() + sleepString,
-            #time.sleep(60)
-            sleepTimer = Timer()
-            while sleepTimer.getElapsedTime() < 60:
-                steps = int(10 * sleepTimer.getElapsedTime() / 60) + 1
-                sleepString = " [" + steps * "." + (10-steps) * " " + "]     "
-                print >> sys.stderr, "\rWaiting for " + str(len(combinations)) + " on " + cscConnection.machineName + "(" + processStatusString + "),", louhiTimer.elapsedTimeToString() + sleepString,
-                time.sleep(5)                
-        else:
-            print >> sys.stderr
-            print >> sys.stderr, "Timed out, ", louhiTimer.elapsedTimeToString()
-            break
+    if steps in ["BOTH", "SUBMIT"]:
+        print >> sys.stderr, "Initializing runs"
+        for combination in combinations:
+            Stream.setIndent(" ")
+            print >> sys.stderr, "Parameters "+str(combinationCount)+"/"+str(len(combinations))+":", str(combination)
+            # Train
+            combinationIds.append(Classifier.initTrainAndTestOnLouhi(trainExamples, testExamples, combination, cscConnection) )
+            combinationCount += 1
+    else:
+        for combination in combinations:
+            idStr = ""
+            for key in sorted(combination.keys()):
+                idStr += "-" + str(key) + "_" + str(combination[key])
+            combinationIds.append(idStr)
     
-    print >> sys.stderr, "Evaluating results"
-    #if type(testExamples) != types.ListType:
-    #    print >> sys.stderr, "Loading examples from file", testExamples
-    #    testExamples = ExampleUtils.readExamples(testExamples,False)
-    bestCombinationId = None
-    for i in range(len(combinationIds)):
-        id = combinationIds[i]
-        Stream.setIndent(" ")
-        # Evaluate
-        predictions = Classifier.getLouhiPredictions(id, cscConnection, workDir)
-        if predictions == None:
-            print >> sys.stderr, "No results for combination" + id
-        else:
-            if downloadAllModels:
-                Classifier.downloadModel(id, cscConnection, workDir)
-            print >> sys.stderr, "Evaluating results for combination" + id
-            evaluationOutput = "evaluation" + id + ".csv"
-            if workDir != None:
-                evaluationOutput = os.path.join(workDir, evaluationOutput)
-            evaluator = Evaluator.evaluate(testExamples, predictions, classIds, evaluationOutput)
-            if bestResult == None or evaluator.compare(bestResult[0]) > 0: #: averageResult.fScore > bestResult[1].fScore:
-                bestResult = [evaluator, None, predictions, evaluationOutput, combinations[i]]
-                bestCombinationId = id
-    Stream.setIndent()
-    print >> sys.stderr, "Selected parameters", bestResult[-1]
-
-    modelFileName = Classifier.downloadModel(bestCombinationId, cscConnection, workDir)
-    if workDir != None:
-        modelFileName = os.path.join(workDir, modelFileName)
-    bestResult[1] = modelFileName
-    return bestResult
+    if steps in ["BOTH", "RESULTS"]:
+        print >> sys.stderr, "Waiting for results"
+        finished = 0
+        louhiTimer = Timer()
+        combinationStatus = {}
+        while(True):
+            # count finished
+            finished = 0
+            processStatus = {"FINISHED":0, "QUEUED":0, "FAILED":0, "RUNNING":0}
+            for id in combinationIds:
+                status = Classifier.getLouhiStatus(id, cscConnection)
+                combinationStatus[id] = status
+                processStatus[status] += 1
+            p = processStatus
+            processStatusString = str(p["QUEUED"]) + " queued, " + str(p["RUNNING"]) + " running, " + str(p["FINISHED"]) + " finished, " + str(p["FAILED"]) + " failed"
+            if processStatus["QUEUED"] + processStatus["RUNNING"] == 0:
+                print >> sys.stderr
+                print >> sys.stderr, "All runs done (" + processStatusString + ")"
+                break
+            # decide what to do
+            if timeout == None or louhiTimer.getElapsedTime() < timeout:
+                sleepString = " [          ]     "
+                print >> sys.stderr, "\rWaiting for " + str(len(combinations)) + " on " + cscConnection.machineName + "(" + processStatusString + "),", louhiTimer.elapsedTimeToString() + sleepString,
+                #time.sleep(60)
+                sleepTimer = Timer()
+                while sleepTimer.getElapsedTime() < 60:
+                    steps = int(10 * sleepTimer.getElapsedTime() / 60) + 1
+                    sleepString = " [" + steps * "." + (10-steps) * " " + "]     "
+                    print >> sys.stderr, "\rWaiting for " + str(len(combinations)) + " on " + cscConnection.machineName + "(" + processStatusString + "),", louhiTimer.elapsedTimeToString() + sleepString,
+                    time.sleep(5)                
+            else:
+                print >> sys.stderr
+                print >> sys.stderr, "Timed out, ", louhiTimer.elapsedTimeToString()
+                break
+        
+        print >> sys.stderr, "Evaluating results"
+        #if type(testExamples) != types.ListType:
+        #    print >> sys.stderr, "Loading examples from file", testExamples
+        #    testExamples = ExampleUtils.readExamples(testExamples,False)
+        bestCombinationId = None
+        for i in range(len(combinationIds)):
+            id = combinationIds[i]
+            Stream.setIndent(" ")
+            # Evaluate
+            predictions = Classifier.getLouhiPredictions(id, cscConnection, workDir)
+            if predictions == None:
+                print >> sys.stderr, "No results for combination" + id
+            else:
+                if downloadAllModels:
+                    Classifier.downloadModel(id, cscConnection, workDir)
+                print >> sys.stderr, "Evaluating results for combination" + id
+                evaluationOutput = "evaluation" + id + ".csv"
+                if workDir != None:
+                    evaluationOutput = os.path.join(workDir, evaluationOutput)
+                evaluator = Evaluator.evaluate(testExamples, predictions, classIds, evaluationOutput)
+                if bestResult == None or evaluator.compare(bestResult[0]) > 0: #: averageResult.fScore > bestResult[1].fScore:
+                    bestResult = [evaluator, None, predictions, evaluationOutput, combinations[i]]
+                    bestCombinationId = id
+        Stream.setIndent()
+        print >> sys.stderr, "Selected parameters", bestResult[-1]
+    
+        modelFileName = Classifier.downloadModel(bestCombinationId, cscConnection, workDir)
+        if workDir != None:
+            modelFileName = os.path.join(workDir, modelFileName)
+        bestResult[1] = modelFileName
+        return bestResult
 #ENDIF
