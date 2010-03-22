2c02ce183b6caec836ee7eb98c02cd71ec89144a
jari <jari>
2010-03-22 18:44:01 +0000

    *** empty log message ***

--- GeniaChallenge/evaluation/EvaluateSharedTask.py
+++ GeniaChallenge/evaluation/EvaluateSharedTask.py
@@ -114,7 +114,11 @@ def evaluate(sourceDir, task=1, folds=-1, foldToRemove=-1, evaluations=["strict"
     #IF LOCAL
     goldDir = "/usr/share/biotext/GeniaChallenge/extension-data/genia/evaluation-data/evaluation-tools-devel-gold"
     tempDir = "/usr/share/biotext/GeniaChallenge/extension-data/genia/evaluation-data/evaluation-temp"
+    if os.environ.has_key("METAWRK"): # CSC
+        goldDir = "/v/users/jakrbj/shared-task-evaluation/evaluation-tools-devel-gold"
+        tempDir = "evaluation-temp"
     #ENDIF
+    
     if os.path.exists(tempDir):
         shutil.rmtree(tempDir)
     os.mkdir(tempDir)
