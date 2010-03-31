8a218011b4d11dab279b1b0e2589e0d23089ef8f
jari <jari>
2010-03-31 19:32:36 +0000

    *** empty log message ***

---
+++ JariSandbox/ComplexPPI/Source/Murska/FindFailed.py
@@ -0,0 +1,36 @@
+import sys, os
+
+def main(inDir, outDir, failFilename):
+    failFile = open(failFileName, "at")
+    for triple in os.walk(inDir):
+        print "Processing", triple[0] 
+        inputFiles = []
+        for filename in triple[2]:
+            if filename[-7:] == ".xml.gz" or filename[-4:] == ".xml":
+                inputFiles.append(filename)
+        if len(inputFiles) == 0:
+            continue
+        
+        for inputFile in inputFiles:
+            if inputFile[-7:] == ".xml.gz":
+                fileStem = inputFile[:-7]
+            elif inputFile[-4:] == ".xml":
+                fileStem = inputFile[:-4]
+            if not os.path.exists(os.path.join(outDir,triple[0],fileStem+"-events.xml.gz")):
+                print "Input file", inputFile, "has failed"
+                failFile.write(os.path.join(triple[0],inputFile)+"\n")
+    failFile.close()
+
+if __name__=="__main__":
+    optparser = OptionParser()
+    optparser.add_option("-i", "--input", default=None, dest="input", help="input directory")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="output directory")
+    optparser.add_option("-f", "--failFile", default=None, dest="failFile", help="Failed input files will be listed here")
+    (options, args) = optparser.parse_args()
+    assert options.input != None
+    assert os.path.exists(options.input)
+    assert options.output != None
+    assert os.path.exists(options.output)
+    
+    main(options.input, options.output, options.failFile)
+        
