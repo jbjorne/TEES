9c8a04d12265194e04d4fdc57d6f7f937ec51b7b
jari <jari>
2010-04-01 13:05:22 +0000

    *** empty log message ***

---
+++ JariSandbox/ComplexPPI/Source/Murska/FindExceptions.py
@@ -0,0 +1,36 @@
+import sys, os
+from optparse import OptionParser
+
+def main(inDir, failFileName):
+    failFile = open(failFileName, "at")
+    for triple in os.walk(inDir): 
+        for filename in triple[2]:
+            if filename[-7:] == ".stderr":
+                print "Processing", filename
+                f = open(os.path.join(triple[0],filename), "rt")
+                medlineName = None
+                errorLineCount = 0
+                for line in f.readlines():
+                    medlinePos = line.find("medline")
+                    if medlinePos != -1:
+                        medlineName = line[medlinePos:medlinePos+32]
+                    if line.find("Traceback") != -1:
+                        failFile.write(medlineName)
+                        print medlineName, "failed"
+                        errorLineCount = 20
+                    if errorLineCount > 0:
+                        print "    " + line
+                        errorLineCount -= 1
+                f.close()
+    failFile.close()
+
+if __name__=="__main__":
+    optparser = OptionParser()
+    optparser.add_option("-i", "--input", default=None, dest="input", help="input directory")
+    optparser.add_option("-f", "--failFile", default=None, dest="failFile", help="Failed input files will be listed here")
+    (options, args) = optparser.parse_args()
+    assert options.input != None
+    assert os.path.exists(options.input)
+    
+    main(options.input, options.failFile)
+        
