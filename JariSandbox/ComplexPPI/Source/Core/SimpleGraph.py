512f0443f25751c2c2b6eee456427c48d9c7fc4f
jari <jari>
2010-10-25 08:29:25 +0000

    Path finding for single path seems to work.

--- JariSandbox/ComplexPPI/Source/Core/SimpleGraph.py
+++ JariSandbox/ComplexPPI/Source/Core/SimpleGraph.py
@@ -2,25 +2,25 @@
 For representing the event argument and dependency graphs. Should be deterministic.
 """
 
-def Graph():
+class Graph():
     """
     One edge per-associated data per direction
     """
-    def __init__(directed=True):
+    def __init__(self, directed=True):
         self.__directed = directed
         self.nodes = []
         self.edges = []
         self.__matrix = {}
         self.__resetAnalyses()
     
-    def __resetAnalyses():
+    def __resetAnalyses(self):
         self.__distances = None
         self.__nextInPath = None
     
-    def isDirected():
+    def isDirected(self):
         return self.__directed
     
-    def toUndirected():
+    def toUndirected(self):
         g = Graph(False)
         g.nodes = self.nodes[:]
         for n1 in g.nodes:
@@ -31,7 +31,7 @@ def Graph():
             g.addEdge(edge[0], edge[1], edge[2])
             g.addEdge(edge[1], edge[2], edge[2])
     
-    def addNode(node):
+    def addNode(self, node):
         if node in self.nodes:
             return False
         self.__resetAnalyses()
@@ -43,25 +43,43 @@ def Graph():
             self.__matrix[node][n] = []
         return True
     
-    def addEdge(node1, node2, data=None):
+    def addNodes(self, nodes):
+        addNodes = False
+        for node in nodes:
+            if node not in self.nodes:
+                addNodes = True
+                break
+        if not addNodes:
+            return False
+        self.__resetAnalyses()
+        for node in nodes:
+            for n in self.nodes:
+                self.__matrix[n][node] = []
+            self.nodes.append(node)
+            self.__matrix[node] = {}
+            for n in self.nodes:
+                self.__matrix[node][n] = []
+        return True
+    
+    def addEdge(self, node1, node2, data=None):
         self.__resetAnalyses()
         if not node1 in self.nodes:
-            self.nodes.append(node1)
+            self.addNode(node1)
         if not node2 in self.nodes:
-            self.nodes.append(node2)
+            self.addNode(node2)
         # add forward edge
         forward = not self.hasEdge(node1, node2, data)
         if forward:
             self.__insertEdge(node1, node2, data)
         # add reverse edge
-        reverse = True
+        reverse = False
         if not self.isDirected():
             reverse = not self.hasEdge(node2, node1, data)
-        if reverse:
-            self.__insertEdge(node2, node1, data)
+            if reverse:
+                self.__insertEdge(node2, node1, data)
         return forward or reverse
     
-    def __insertEdge(node1, node2, data):
+    def __insertEdge(self, node1, node2, data):
         """
         Assumes edge doesn't exist already
         """
@@ -69,52 +87,74 @@ def Graph():
         self.edges.append(edge)
         self.__matrix[node1][node2].append(edge)
         
-    def hasEdge(node1, node2, data=None):
+    def hasEdge(self, node1, node2, data=None):
         for edge in self.__matrix[node1][node2]:
             if edge[2] == data:
                 return True
         return False
     
-    def getEdges(node1, node2):
+    def getEdges(self, node1, node2):
         return self.__matrix[node1][node2]
     
-    def FloydWarshall():
+    def FloydWarshall(self):
         n = len(self.nodes)
         self.__distances = {}
         infinity = sys.maxint # at least it's a really large number
         # Init distance matrix
-        for n1 in self.__nodes:
+        for n1 in self.nodes:
             self.__distances[n1] = {}
-            for n2 in self.__nodes:
+            for n2 in self.nodes:
                 if n1 == n2:
                     self.__distances[n1][n2] = 0
+                elif len(self.getEdges(n1, n2)) > 0:
+                    self.__distances[n1][n2] = 1
                 else:
                     self.__distances[n1][n2] = infinity
         # Init path traversal matrix    
         self.__nextInPath = {}
-        for n1 in self.__nodes:
+        for n1 in self.nodes:
             self.__nextInPath[n1] = {}
-            for n2 in self.__nodes:
+            for n2 in self.nodes:
                 self.__nextInPath[n1][n2] = None
         # Calculate distances
         d = self.__distances    
-        for k in self.__nodes:
-            for i in self.__nodes:
-                for j in self.__nodes:
-                    if d[i][k] + d[k][j] <= d[i][j]:
+        for k in self.nodes:
+            for i in self.nodes:
+                for j in self.nodes:
+                    if d[i][k] + d[k][j] < d[i][j]:
                         d[i][j] = d[i][k] + d[k][j]
                         self.__nextInPath[i][j] = k
-
+        print d
+        
     def getPath(self, i, j):
         if self.__nextInPath == None:
             self.FloydWarshall()
-        if self.__nextInPath[i][j] == None:
+        if self.__distances[i][j] == sys.maxint:
             return None
         intermediate = self.__nextInPath[i][j]
-        if intermediate == 0:
+        if intermediate == None:
             return [i, j] #;   /* there is an edge from i to j, with no vertices between */
         else:
-            return self.getPath(i,intermediate) + intermediate + self.getPath(intermediate,j)
+            segment = self.getPath(intermediate,j)
+            return [i] + segment
+
+    def getPathOld(self, i, j):
+        if self.__nextInPath == None:
+            self.FloydWarshall()
+        if self.__distances[i][j] == sys.maxint:
+            return None
+        intermediate = self.__nextInPath[i][j]
+        if intermediate == None:
+            return [i, j] #;   /* there is an edge from i to j, with no vertices between */
+        else:
+            segment1 = self.getPath(i,intermediate)
+            if segment1 == None:
+                return None
+            segment2 = self.getPath(intermediate,j)
+            if segment2 == None:
+                return None
+            #return segment1 + [intermediate] + segment2
+            return segment1 + segment2
     
     def getWalks(self, node1, node2, directed=False):
         path = self.getPath(node1, node2)
@@ -142,3 +182,27 @@ def Graph():
             else:
                 allWalks.append(walk + [edge])
         return allWalks
+
+if __name__=="__main__":
+    import sys
+    
+    from optparse import OptionParser
+    # Import Psyco if available
+    try:
+        import psyco
+        psyco.full()
+        print >> sys.stderr, "Found Psyco, using"
+    except ImportError:
+        print >> sys.stderr, "Psyco not installed"
+
+    g = Graph()
+    print g
+    g.addNodes([1, 2, 3, 4, 5])
+    g.addEdge(1,2)
+    g.addEdge(2,3)
+    g.addEdge(1,4)
+    g.addEdge(4,3)
+    print "nodes", g.nodes
+    print "edges", g.edges
+    print g.getPath(1,3)
+    print g.getPath(4,3)
