1573555070d9cccdba497c05855b9111ad27df87
jari <jari>
2010-10-28 12:40:44 +0000

    Added progress counter.

--- JariSandbox/ComplexPPI/Source/Tools/StanfordParser.py
+++ JariSandbox/ComplexPPI/Source/Tools/StanfordParser.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import tempfile
 import codecs
+from ProcessUtils import *
 
 stanfordParserDir = "/home/jari/biotext/tools/stanford-parser-2008-10-26"
 
@@ -14,11 +15,18 @@ def convert(input, output=None):
         output = os.path.join(workdir, "stanford-output.txt")
     
     input = os.path.abspath(input)
+    numCorpusSentences = 0
+    inputFile = codecs.open(input, "rt", "utf-8")
+    for line in inputFile:
+        numCorpusSentences += 1
+    inputFile.close()
     cwd = os.getcwd()
     os.chdir(stanfordParserDir)
     args = ["java", "-mx150m", "-cp", "stanford-parser.jar", "edu.stanford.nlp.trees.EnglishGrammaticalStructure", "-CCprocessed", "-treeFile", input] 
-    subprocess.call(args, 
+    #subprocess.call(args,
+    process = subprocess.Popen(args, 
         stdout=codecs.open(output, "wt", "utf-8"))
+    waitForProcess(process, numCorpusSentences, True, output, "StanfordParser", "Stanford Conversion")
     os.chdir(cwd)
 
     lines = None    
