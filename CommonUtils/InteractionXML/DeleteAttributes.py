bde0824b975c9633d565f35235460c128c2900a9
jari <jari>
2010-11-16 12:51:10 +0000

    For removing attributes from elements.

---
+++ CommonUtils/InteractionXML/DeleteAttributes.py
@@ -0,0 +1,78 @@
+import sys, os, copy
+extraPath = os.path.dirname(os.path.abspath(__file__))+"/../.."
+#IF LOCAL
+extraPath = os.path.dirname(os.path.abspath(__file__))+"/../../JariSandbox/ComplexPPI/Source"
+#ENDIF
+sys.path.append(extraPath)
+from Utils.ProgressCounter import ProgressCounter
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import cElementTree as ET
+import cElementTreeUtils as ETUtils
+    
+def removeAttributes(parent, elementName, attributes, countsByType):
+    for element in parent.getchildren():
+        if element.tag == elementName:
+            for attribute in attributes:
+                if element.get(attribute) != None:
+                    del element.attrib[attribute]
+                    countsByType[elementName + ":" + attribute] += 1
+        removeAttributes(element, elementName, attributes, countsByType)
+
+def processCorpus(inputFilename, outputFilename, rules):
+    print >> sys.stderr, "Loading corpus file", inputFilename
+    if inputFilename.rsplit(".",1)[-1] == "gz":
+        import gzip
+        corpusTree = ET.parse(gzip.open(inputFilename))
+    else:
+        corpusTree = ET.parse(inputFilename)
+    corpusRoot = corpusTree.getroot()
+    
+    countsByType = {}
+    for key in sorted(rules.keys()):
+        for attribute in rules[key]:
+            countsByType[key + ":" + attribute] = 0
+        removeAttributes(corpusRoot, key, rules[key], countsByType)
+    
+    print >> sys.stderr, "Removed"
+    for k in sorted(countsByType.keys()):
+        print >> sys.stderr, "  " + k + ":", countsByType[k]
+    
+    if outputFilename != None:
+        print >> sys.stderr, "Writing output to", outputFilename
+        ETUtils.write(corpusRoot, outputFilename)
+    return corpusTree
+
+if __name__=="__main__":
+    import sys
+    print >> sys.stderr, "##### Split elements with merged types #####"
+    
+    from optparse import OptionParser
+    # Import Psyco if available
+    try:
+        import psyco
+        psyco.full()
+        print >> sys.stderr, "Found Psyco, using"
+    except ImportError:
+        print >> sys.stderr, "Psyco not installed"
+
+    optparser = OptionParser(usage="%prog [options]\nPath generator.")
+    optparser.add_option("-i", "--input", default=None, dest="input", help="Corpus in interaction xml format", metavar="FILE")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="Output file in interaction xml format.")
+    optparser.add_option("-r", "--rules", default=None, dest="rules", help="dictionary of python dictionaries with attribute:value pairs.")    
+    (options, args) = optparser.parse_args()
+    
+    if options.input == None:
+        print >> sys.stderr, "Error, input file not defined."
+        optparser.print_help()
+        sys.exit(1)
+    if options.output == None:
+        print >> sys.stderr, "Error, output file not defined."
+        optparser.print_help()
+        sys.exit(1)
+
+    # Rules e.g. "{\"pair\":{},\"interaction\":{},\"entity\":{\"isName\":\"False\"}}"
+    rules = eval(options.rules)
+    print >> sys.stderr, "Rules:", rules
+    processCorpus(options.input, options.output, rules)
