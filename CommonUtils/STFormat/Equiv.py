a49a2c36626308d3ae2d69363023e7dc89b430bd
jari <jari>
2011-02-16 19:37:38 +0000

    Takes into account 1) That in an event tree with equivs, only those subtrees that contain equivs need to be duplicated and 2) that duplicated subtrees should be shared between different root-level nodes. The result is a program that finally seems to work, and is a horrible mess.

--- CommonUtils/STFormat/Equiv.py
+++ CommonUtils/STFormat/Equiv.py
@@ -11,16 +11,21 @@ def process(documents):
     for doc in documents:
         #if doc.id != "PMC-2806624-00-TIAB":
         #    continue
-        #print doc.id
+        print doc.id
         numOldEvents += len(doc.events)
         # Get all top-level events
         rootEvents = getRoots(doc)
         # Build new top level events
         newEvents = []
+        # The same event that needs to be duplicated can be nested in several trees
+        # The duplDict makes sure that duplicates are generated only once, and shared
+        # between trees.
+        duplDict = {}
         for rootEvent in rootEvents:
-            newEvents.extend(duplicateEquiv(rootEvent))
+            newEvents.extend(duplicateEquiv(rootEvent, duplDict))
         # Regenerate the flat event list in the document
         doc.events = rebuildEventList(newEvents)
+        doc.events.sort(key = lambda x: (x.id[0], int(x.id[1:].split(".")[0]), x.id[1:].split(".")[-1]) )
         numNewEvents += len(doc.events)
     print >> sys.stderr, "Duplication created", numNewEvents - numOldEvents, "new events (new total", numNewEvents, "events)"
 
@@ -56,11 +61,23 @@ def getArgs(event, argList):
             argList.append([arg[1]] + arg[1].equiv)
             assert arg[1] not in arg[1].equiv
         if arg[1].id[0] == "E": # nested event
+            #if hasNestedEquivs(arg[1]):
             rv = getArgs(arg[1], argList)
             hasEquiv = rv or hasEquiv
+            #else:
+            #    pass # stop recursion
     return hasEquiv
 
-def makeEvent(model, argCombination, count, newEvent = None):
+def hasNestedEquivs(event):
+    rv = False
+    for arg in event.arguments:
+        if len(arg[1].equiv) != 0:
+            rv = True
+        elif arg[1].id[0] == "E": # nested event
+            rv = rv or hasNestedEquivs(arg[1]) 
+    return rv
+
+def makeEvent(model, argCombination, count, newEvent = None, finished=False, duplDict=None):
     """
     Given an argument list in depth-first order (argCombination), make
     a copy of "model" event tree.
@@ -69,27 +86,43 @@ def makeEvent(model, argCombination, count, newEvent = None):
         #print "Mak", argCombination
         newEvent = Annotation()
         newEvent.trigger = model.trigger
-        newEvent.id = model.id + "_d" + str(count)
+        newEvent.id = model.id + ".d" + str(count)
         newEvent.speculation = model.speculation
         newEvent.negation = model.negation
     for arg in model.arguments:
-        #print model.id, [x[1].id for x in model.arguments], "/", arg[1].id, argCombination, "/", newEvent, newEvent.arguments
+        print model.id, [x[1].id for x in model.arguments], "/", arg[1].id, argCombination, "/", newEvent, newEvent.arguments
         if arg[1].id[0] != "E": # not a nested event
             # Non-event arguments never need to be duplicated
-            newEvent.arguments.append([arg[0], argCombination[0], arg[2]])
+            if not finished:
+                newEvent.arguments.append([arg[0], argCombination[0], arg[2]])
             argCombination.pop(0) # pop first (depth-first iteration)
         else: # is a nested event
-            # For event arguments, create a new copy
             assert arg[2] == None, (model.id, arg)
-            newArg = [arg[0], copy.copy(argCombination[0]), None]
-            argCombination.pop(0) # pop first (depth-first iteration)
-            newArg[1].arguments = [] # reset the argument list of the copy
-            newArg[1].id += "_d" + str(count)
-            newEvent.arguments.append(newArg) # add to parent copy
-            makeEvent(arg[1], argCombination, count, newArg[1]) # Continue processing with next level of model and copy
+            if not finished:
+                if hasNestedEquivs(arg[1]):
+                    # For event arguments that have children with equiv, create a new copy
+                    duplId = arg[1].id + ".d" + str(count)
+                    if duplId not in duplDict:
+                        newArg = [arg[0], copy.copy(argCombination[0]), None]
+                        argCombination.pop(0) # pop first (depth-first iteration)
+                        newArg[1].arguments = [] # reset the argument list of the copy
+                        newArg[1].id = duplId
+                        newEvent.arguments.append(newArg) # add to parent copy
+                        duplDict[duplId] = newArg
+                        makeEvent(arg[1], argCombination, count, newArg[1], finished, duplDict) # Continue processing with next level of model and copy
+                    else:
+                        argCombination.pop(0) # pop first (depth-first iteration)
+                        newEvent.arguments.append(duplDict[duplId]) # add to parent copy
+                        makeEvent(arg[1], argCombination, count, duplDict[duplId][1], True, duplDict) # Continue processing with next level of model and copy
+                else:
+                    newArg = [arg[0], argCombination[0], None]
+                    argCombination.pop(0) # pop first (depth-first iteration)
+                    newEvent.arguments.append(newArg) # add to parent copy
+                    # stop recursion here, it has been likewise stopped in getArgs
+                    makeEvent(arg[1], argCombination, count, newArg[1], True, duplDict) # Continue processing with next level of model and copy
     return newEvent
 
-def duplicateEquiv(event):
+def duplicateEquiv(event, duplDict):
     """
     If the event (event tree) has arguments which have Equiv-statements, create a new event
     for each combination. Otherwise, return just the existing event.
@@ -98,14 +131,14 @@ def duplicateEquiv(event):
     hasEquiv = getArgs(event, argList)
     if not hasEquiv:
         return [event]
-    #print event.id
-    #print "a", argList
+    print event.id
+    print "a", argList
     combinations = combine.combine(*argList) # make all combinations
-    #print "b", combinations
+    print "b", combinations
     newEvents = []
     count = 0 # used only for marking duplicates' ids
     for combination in combinations:
-        newEvents.append(makeEvent(event, combination, count))
+        newEvents.append(makeEvent(event, combination, count, duplDict=duplDict))
         count += 1
     return newEvents
 
@@ -116,7 +149,8 @@ def rebuildEventList(events, eventList = None):
     if eventList == None:
         eventList = []
     for event in events:
-        eventList.append(event)
+        if event not in eventList:
+            eventList.append(event)
         for arg in event.arguments:
             if arg[1].id[0] == "E":
                 rebuildEventList([arg[1]], eventList)
