4995209f5147610ddf80262aa55469ea5e4361bb
jari <jari>
2011-03-08 21:46:06 +0000

    Task 3 counts and surviving missing data.

--- CommonUtils/STFormat/Compare.py
+++ CommonUtils/STFormat/Compare.py
@@ -3,6 +3,8 @@ from STTools import *
 
 def getCounts(documents):
     counts = defaultdict(int)
+    counts["task3(spec)"] = 0
+    counts["task3(neg)"] = 0
     for doc in documents:
         if doc.proteins != None:
             for protein in doc.proteins: 
@@ -16,6 +18,10 @@ def getCounts(documents):
             for event in doc.events: 
                 counts["event("+event.type+")"] += 1
                 counts["event"] += 1
+                if event.speculation != None:
+                    counts["task3(spec)"] += 1
+                if event.negation != None:
+                    counts["task3(neg)"] += 1
         if doc.relations != None:
             for relation in doc.relations: 
                 counts["relation("+relation.type+")"] += 1
@@ -26,9 +32,9 @@ def getCounts(documents):
                 counts["arg"] += 1
     return counts
 
-def compare(a, b):
-    docsA = loadSet(a)
-    docsB = loadSet(b)
+def compare(a, b, a2Tag="a2"):
+    docsA = loadSet(a, a2Tag=a2Tag)
+    docsB = loadSet(b, a2Tag=a2Tag)
     countsA = getCounts(docsA)
     countsB = getCounts(docsB)
     allKeys = list(set(countsA.keys() + countsB.keys()))
@@ -36,8 +42,8 @@ def compare(a, b):
     maxKeyLength = max([len(x) for x in allKeys])
     # Sets
     print "Sets"
-    print "A:", len(docsA)
-    print "B:", len(docsB)
+    print "A:", a, "(documents: " + str(len(docsA)) + ")"
+    print "B:", b, "(documents: " + str(len(docsB)) + ")"
     # Make title
     titleLine = "Category"
     while len(titleLine) <= maxKeyLength:
@@ -62,24 +68,31 @@ def compare(a, b):
         valB = (countsB[key] / float(len(docsB)))
         line += "%.2f" % valB
         # Diff
-        diff = valA / valB
         while len(line) <= maxKeyLength + 20:
             line += " "
-        line += "%.2f" % diff
+        if valA == 0 or valB == 0:
+            diff = None
+            line += "N/A"
+        else:
+            diff = valA / valB
+            line += "%.2f" % diff
         # Dist
         while len(line) <= maxKeyLength + 30:
             line += " "
-        dist = abs(1.0 - diff)
-        maxCount = 30
-        step = 0.01
-        count = 0
-        while dist > 0:
-            dist -= step
-            count += 1
-            line += "!"
-            if count >= maxCount:
-                line += "+"
-                break
+        if diff != None:
+            dist = abs(1.0 - diff)
+            maxCount = 30
+            step = 0.01
+            count = 0
+            while dist > 0:
+                dist -= step
+                count += 1
+                line += "!"
+                if count >= maxCount:
+                    line += "+"
+                    break
+        else:
+            line += "-"
         #if dist > 0.05:
         #    line += "!!!!!!!!!!+"
         #else:
@@ -102,7 +115,8 @@ if __name__=="__main__":
     optparser = OptionParser(usage="%prog [options]\n")
     optparser.add_option("-a", "--inputA", default=None, dest="inputA", help="", metavar="FILE")
     optparser.add_option("-b", "--inputB", default=None, dest="inputB", help="")
+    optparser.add_option("-t", "--a2Tag", default="a2", dest="a2Tag", help="")
     #optparser.add_option("-p", "--parse", default=None, dest="parse", help="Name of parse element.")
     (options, args) = optparser.parse_args()
     
-    compare(options.inputA, options.inputB)
+    compare(options.inputA, options.inputB, a2Tag=options.a2Tag)
