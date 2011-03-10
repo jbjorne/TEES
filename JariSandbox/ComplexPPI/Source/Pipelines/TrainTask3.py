f369b11a31443dbce873aa5c1bc8c5a58cb0f654
jari <jari>
2011-03-10 00:27:04 +0000

    Support for multiple train/test files and appending of examples.

--- JariSandbox/ComplexPPI/Source/Pipelines/TrainTask3.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/TrainTask3.py
@@ -10,14 +10,15 @@ optparser.add_option("-e", "--test", default=Settings.DevelFileWithDuplicates, d
 optparser.add_option("-r", "--train", default=Settings.TrainFileWithDuplicates, dest="trainFile", help="Train file in interaction xml")
 optparser.add_option("-o", "--output", default=None, dest="output", help="output directory")
 optparser.add_option("-a", "--task", default=123, type="int", dest="task", help="task number")
-optparser.add_option("-p", "--parse", default="split-McClosky", dest="parse", help="Parse XML element name")
-optparser.add_option("-t", "--tokenization", default="split-McClosky", dest="tokenization", help="Tokenization XML element name")
+optparser.add_option("-p", "--parse", default="split-mccc-preparsed", dest="parse", help="Parse XML element name")
+optparser.add_option("-t", "--tokenization", default=None, dest="tokenization", help="Tokenization XML element name")
 optparser.add_option("--csc", default="", dest="csc", help="")
 # Id sets
 optparser.add_option("-i", "--task3Ids", default=Settings.Task3Ids, dest="task3Ids", help="Speculation & negation SVM example class and feature id file stem (files = STEM.class_names and STEM.feature_names)")
 # Parameters to optimize
-optparser.add_option("-x", "--speculationParams", default="1000,5000,10000,13000,16000,20000,50000", dest="speculationParams", help="Speculation SVM c-parameter values")
-optparser.add_option("-y", "--negationParams", default="1000,5000,10000,13000,16000,20000,50000", dest="negationParams", help="Negation SVM c-parameter values")
+optparser.add_option("-x", "--speculationParams", default="1000,2500,5000,7500,10000,13000,16000,20000,40000,50000,60000,100000,125000,150000,200000,300000,500000,1000000", dest="speculationParams", help="Speculation SVM c-parameter values")
+optparser.add_option("-y", "--negationParams", default="1000,2500,5000,7500,10000,13000,16000,20000,50000", dest="negationParams", help="Negation SVM c-parameter values")
+optparser.add_option("--clearAll", default=False, action="store_true", dest="clearAll", help="Delete all files")
 (options, args) = optparser.parse_args()
 
 # Check options
@@ -27,37 +28,53 @@ if options.csc.find(",") != -1:
     options.csc = options.csc.split(",")
 else:
     options.csc = [options.csc]
+if options.clearAll and "clear" not in options.csc:
+    options.csc.append("clear")
 
 # Main settings
 PARSE=options.parse
 TOK=options.tokenization
-TRAIN_FILE = options.trainFile
-TEST_FILE = options.testFile
+TRAIN_FILES = options.trainFile.split(";")
+TEST_FILES = options.testFile.split(";")
 
 # These commands will be in the beginning of most pipelines
 WORKDIR=options.output
 CSC_WORKDIR = os.path.join("CSCConnection",WORKDIR.lstrip("/"))
-workdir(WORKDIR, False) # Select a working directory, don't remove existing files
+workdir(WORKDIR, options.clearAll) # Select a working directory, don't remove existing files
 log() # Start logging into a file in working directory
 
-PARSE_TAG = PARSE + "_" + TOK
+PARSE_TAG = PARSE # + "_" + TOK
 
 TASK3_IDS = os.path.basename(options.task3Ids)
-if not "download" in options.csc:
+if not "download" in options.csc and not "upload" in options.csc:
     ###############################################################################
     # Speculation Examples
     ###############################################################################
     TASK3_IDS = copyIdSetsToWorkdir(options.task3Ids)
-    print >> sys.stderr, "Speculation examples for parse", PARSE_TAG  
-    Task3ExampleBuilder.run(TRAIN_FILE, "speculation-train-examples", PARSE, TOK, "style:typed,speculation", TASK3_IDS, None)
-    Task3ExampleBuilder.run(TEST_FILE, "speculation-test-examples", PARSE, TOK, "style:typed,speculation", TASK3_IDS, None)
+    print >> sys.stderr, "Speculation train examples for parse", PARSE_TAG
+    appendIndex = 0
+    for TRAIN_FILE in TRAIN_FILES:
+        Task3ExampleBuilder.run(TRAIN_FILE, "speculation-train-examples", PARSE, TOK, "style:typed,speculation", TASK3_IDS, None, appendIndex=appendIndex)
+        appendIndex += 1000
+    print >> sys.stderr, "Speculation test examples for parse", PARSE_TAG
+    appendIndex = 0
+    for TEST_FILE in TEST_FILES:
+        Task3ExampleBuilder.run(TEST_FILE, "speculation-test-examples", PARSE, TOK, "style:typed,speculation", TASK3_IDS, None, appendIndex=appendIndex)
+        appendIndex += 1000
     
     ###############################################################################
     # Negation Examples
     ###############################################################################
-    print >> sys.stderr, "Negation examples for parse", PARSE_TAG  
-    Task3ExampleBuilder.run(TRAIN_FILE, "negation-train-examples", PARSE, TOK, "style:typed,negation", TASK3_IDS, None)
-    Task3ExampleBuilder.run(TEST_FILE, "negation-test-examples", PARSE, TOK, "style:typed,negation", TASK3_IDS, None)
+    print >> sys.stderr, "Negation train examples for parse", PARSE_TAG  
+    appendIndex = 0
+    for TRAIN_FILE in TRAIN_FILES:
+        Task3ExampleBuilder.run(TRAIN_FILE, "negation-train-examples", PARSE, TOK, "style:typed,negation", TASK3_IDS, None, appendIndex=appendIndex)
+        appendIndex += 1000
+    print >> sys.stderr, "Negation test examples for parse", PARSE_TAG  
+    appendIndex = 0
+    for TEST_FILE in TEST_FILES:
+        Task3ExampleBuilder.run(TEST_FILE, "negation-test-examples", PARSE, TOK, "style:typed,negation", TASK3_IDS, None, appendIndex=appendIndex)
+        appendIndex += 1000
 
 speculationCSC = None
 negationCSC = None
