68c76faa41a9b3510de59462b4410e917cc9ace4
jari <jari>
2011-11-01 16:51:27 +0000

    Extended for REN and learning curves.

--- JariSandbox/ComplexPPI/Source/Pipelines/BIFull.py
+++ JariSandbox/ComplexPPI/Source/Pipelines/BIFull.py
@@ -6,6 +6,36 @@ import sys, os
 import STFormat.ConvertXML
 import STFormat.Compare
 
+def makeSubset(filename, output, ratio, seed):
+    if ratio == 1.0:
+        return filename
+    totalFolds = 100
+    selectedFolds = int(ratio * 100.0)
+    print >> sys.stderr, "====== Making subset ======"
+    print >> sys.stderr, "Subset for file", filename, "ratio", ratio, "seed", seed
+    import cElementTreeUtils as ETUtils
+    import Core.Split
+    xml = ETUtils.ETFromObj(filename).getroot()
+    count = 0
+    sentCount = 0
+    for document in xml.findall("document"):
+        sentCount += len(document.findall("sentence"))
+        count += 1
+    division = Core.Split.getFolds(count, totalFolds, seed)
+    #print division, selectedFolds - 1
+    index = 0
+    removeCount = 0
+    sentRemoveCount = 0
+    for document in xml.findall("document"):
+        if division[index] > selectedFolds - 1:
+            xml.remove(document)
+            sentRemoveCount += len(document.findall("sentence"))
+            removeCount += 1
+        index += 1
+    print "Subset", "doc:", count, "removed:", removeCount, "sent:", sentCount, "sentremoved:", sentRemoveCount
+    ETUtils.write(xml, output)
+    return output
+
 from optparse import OptionParser
 optparser = OptionParser()
 optparser.add_option("-e", "--test", default=Settings.DevelFile, dest="testFile", help="Test file in interaction xml")
@@ -18,8 +48,11 @@ optparser.add_option("-t", "--tokenization", default="gold", dest="tokenization"
 optparser.add_option("-c", "--classifier", default="Cls", dest="classifier", help="")
 optparser.add_option("--csc", default="murska", dest="csc", help="")
 # Example builders
+optparser.add_option("--downSampleTrain", default=1.0, type="float", dest="downSampleTrain", help="")
+optparser.add_option("--downSampleSeed", default=1, type="int", dest="downSampleSeed", help="")
+optparser.add_option("--noTestSet", default=False, action="store_true", dest="noTestSet", help="")
 optparser.add_option("-f", "--edgeExampleBuilder", default="MultiEdgeExampleBuilder", dest="edgeExampleBuilder", help="")
-optparser.add_option("-s", "--styles", default="trigger_features,typed,directed,no_linear,entities,noMasking,maxFeatures,bi_limits", dest="edgeStyles", help="")
+optparser.add_option("-s", "--styles", default=None, dest="edgeStyles", help="")
 #optparser.add_option("-g", "--gazetteer", default="none", dest="gazetteer", help="gazetteer options: none, stem, full")
 # Id sets
 optparser.add_option("-v", "--edgeIds", default=None, dest="edgeIds", help="Trigger detector SVM example class and feature id file stem (files = STEM.class_names and STEM.feature_names)")
@@ -30,14 +63,24 @@ optparser.add_option("--clearAll", default=False, action="store_true", dest="cle
 
 # Check options
 assert options.output != None
-assert options.task in ["BI"]
-dataPath = "/home/jari/biotext/BioNLP2011/data/main-tasks/"
-TRAIN_FILE = dataPath + options.task + "/" + options.task + "-train-nodup.xml"
-TEST_FILE = dataPath + options.task + "/" + options.task + "-devel-nodup.xml"
-EVERYTHING_FILE = dataPath + options.task + "/" + options.task + "-devel-and-train.xml"
-FINAL_TEST_FILE = dataPath + options.task + "/" + options.task + "-test.xml"
-
-BXEv.setOptions("genia-BXEv", "BI", TEST_FILE, options.parse, options.tokenization, "edge-ids")
+assert options.task in ["BI", "REN"]
+if options.task == "BI":
+    dataPath = os.path.expanduser("~/biotext/BioNLP2011/data/main-tasks/")
+    TRAIN_FILE = dataPath + options.task + "/" + options.task + "-train-nodup.xml"
+    TEST_FILE = dataPath + options.task + "/" + options.task + "-devel-nodup.xml"
+    if not options.noTestSet:
+        EVERYTHING_FILE = dataPath + options.task + "/" + options.task + "-devel-and-train.xml"
+        FINAL_TEST_FILE = dataPath + options.task + "/" + options.task + "-test.xml"
+    BXEv.setOptions("genia-BXEv", "BI", TEST_FILE, options.parse, options.tokenization, "edge-ids")
+    EVALUATOR = BXEv
+else:
+    dataPath = os.path.expanduser("~/biotext/BioNLP2011/data/REN/")
+    TRAIN_FILE = dataPath + "ren-train.xml"
+    TEST_FILE = dataPath + "ren-devel.xml"
+    if not options.noTestSet:
+        EVERYTHING_FILE = dataPath + "ren-devel-and-train.xml"
+        FINAL_TEST_FILE = dataPath + "ren-test.xml"
+    EVALUATOR = Ev
 
 if options.csc.find(",") != -1:
     options.csc = options.csc.split(",")
@@ -60,15 +103,27 @@ CSC_WORKDIR = os.path.join("CSCConnection",WORKDIR.lstrip("/"))
 workdir(WORKDIR, options.clearAll) # Select a working directory, don't remove existing files
 log() # Start logging into a file in working directory
 
+# Make downsampling for learning curve
+downSampleTag = "-r" + str(options.downSampleTrain) + "_s" + str(options.downSampleSeed)
+newTrainFile = makeSubset(TRAIN_FILE, options.task + "-train-nodup" + downSampleTag + ".xml", options.downSampleTrain, options.downSampleSeed)
+makeSubset(TRAIN_FILE.replace("-nodup", ""), options.task + "-train" + downSampleTag + ".xml", options.downSampleTrain, options.downSampleSeed)
+TRAIN_FILE = newTrainFile
+
 # Example generation parameters
-#EDGE_FEATURE_PARAMS="style:typed"
-EDGE_FEATURE_PARAMS="style:" + options.edgeStyles
+if options.edgeStyles != None:
+    EDGE_FEATURE_PARAMS="style:"+options.edgeStyles
+elif options.task == "BI":
+    EDGE_FEATURE_PARAMS="style:trigger_features,typed,directed,no_linear,entities,noMasking,maxFeatures,bi_limits"
+elif options.task == "REN":
+    EDGE_FEATURE_PARAMS="style:trigger_features,typed,no_linear,entities,noMasking,maxFeatures,bacteria_renaming"
+    options.edgeParams = "10,100,1000,2000,3000,4000,4500,5000,5500,6000,7500,10000,20000,25000,28000,50000,60000"
 print >> sys.stderr, "Edge feature style:", EDGE_FEATURE_PARAMS
 
 EDGE_TRAIN_EXAMPLE_FILE = "edge-train-examples-"+PARSE_TAG
-EDGE_EVERYTHING_EXAMPLE_FILE = "edge-everything-examples-"+PARSE_TAG
 EDGE_TEST_EXAMPLE_FILE = "edge-test-examples-"+PARSE_TAG
-EDGE_FINAL_TEST_EXAMPLE_FILE = "edge-final-examples-"+PARSE_TAG
+if not options.noTestSet:
+    EDGE_EVERYTHING_EXAMPLE_FILE = "edge-everything-examples-"+PARSE_TAG
+    EDGE_FINAL_TEST_EXAMPLE_FILE = "edge-final-examples-"+PARSE_TAG
 EDGE_IDS = "edge-ids"
 #STEPS = [""]
 #STEPS = ["themeOnly", "causeAfterTheme"]
@@ -81,8 +136,9 @@ if not "eval" in options.csc:
     ###############################################################################
     EDGE_EXAMPLE_BUILDER.run(TEST_FILE, EDGE_TEST_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
     EDGE_EXAMPLE_BUILDER.run(TRAIN_FILE, EDGE_TRAIN_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
-    EDGE_EXAMPLE_BUILDER.run(EVERYTHING_FILE, EDGE_EVERYTHING_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
-    EDGE_EXAMPLE_BUILDER.run(FINAL_TEST_FILE, EDGE_FINAL_TEST_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
+    if not options.noTestSet:
+        EDGE_EXAMPLE_BUILDER.run(EVERYTHING_FILE, EDGE_EVERYTHING_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
+        EDGE_EXAMPLE_BUILDER.run(FINAL_TEST_FILE, EDGE_FINAL_TEST_EXAMPLE_FILE, PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
 
 print >> sys.stderr, "Edge models for", PARSE_TAG
 EDGE_CLASSIFIER_PARAMS="c:" + options.edgeParams
@@ -95,7 +151,7 @@ if "local" not in options.csc:
         c = CSCConnection(CSC_WORKDIR+"/edge-models", "jakrbj@murska.csc.fi", clear)
 else:
     c = None
-bestEdgeModel = optimize(CLASSIFIER, BXEv, EDGE_TRAIN_EXAMPLE_FILE, EDGE_TEST_EXAMPLE_FILE,\
+bestEdgeModel = optimize(CLASSIFIER, EVALUATOR, EDGE_TRAIN_EXAMPLE_FILE, EDGE_TEST_EXAMPLE_FILE,\
 EDGE_IDS+".class_names", EDGE_CLASSIFIER_PARAMS, "edge-models", None, c, False)
 
 print >> sys.stderr, "------------ Check devel classification ------------"
@@ -105,7 +161,10 @@ xml = ix.splitMergedElements(xml, None)
 xml = ix.recalculateIds(xml, "devel-predicted-edges.xml", True)
 EvaluateInteractionXML.run(Ev, xml, TEST_FILE, PARSE, TOK)
 STFormat.ConvertXML.toSTFormat(xml, "devel-geniaformat", outputTag="a2")
-evaluateBX("devel-geniaformat", "BI")
+if options.task == "BI":
+    evaluateBX("devel-geniaformat", "BI")
+else:
+    evaluateREN("devel-geniaformat")
 
 print >> sys.stderr, "------------ Empty devel classification ------------"
 EDGE_EXAMPLE_BUILDER.run(TEST_FILE.replace(".xml", "-empty.xml"), "empty-devel-examples", PARSE, TOK, EDGE_FEATURE_PARAMS, EDGE_IDS)
@@ -115,25 +174,29 @@ xml = ix.splitMergedElements(xml, None)
 xml = ix.recalculateIds(xml, "empty-devel-predicted-edges.xml", True)
 EvaluateInteractionXML.run(Ev, xml, TEST_FILE, PARSE, TOK)
 STFormat.ConvertXML.toSTFormat(xml, "empty-devel-geniaformat", outputTag="a2")
-evaluateBX("empty-devel-geniaformat", "BI")
-
-print >> sys.stderr, "------------ Test set classification ------------"
-if "local" not in options.csc:
-    clear = False
-    if "clear" in options.csc: clear = True
-    if "louhi" in options.csc:
-        c = CSCConnection(CSC_WORKDIR+"/edge-everything-models", "jakrbj@louhi.csc.fi", clear)
-    else:
-        c = CSCConnection(CSC_WORKDIR+"/edge-everything-models", "jakrbj@murska.csc.fi", clear)
+if options.task == "BI":
+    evaluateBX("empty-devel-geniaformat", "BI")
 else:
-    c = None
-finalEdgeModel = optimize(CLASSIFIER, Ev, EDGE_EVERYTHING_EXAMPLE_FILE, EDGE_TEST_EXAMPLE_FILE,\
-EDGE_IDS+".class_names", "c:"+bestEdgeModel[2].split("_")[-1], "edge-everything-models", None, c, False)[1]
-Cls.test(EDGE_FINAL_TEST_EXAMPLE_FILE, finalEdgeModel, "final-edge-test-classifications")
-xml = BioTextExampleWriter.write(EDGE_FINAL_TEST_EXAMPLE_FILE, "final-edge-test-classifications", FINAL_TEST_FILE, None, EDGE_IDS+".class_names", PARSE, TOK)
-xml = ix.splitMergedElements(xml, None)
-xml = ix.recalculateIds(xml, "final-predicted-edges.xml", True)
-EvaluateInteractionXML.run(Ev, xml, FINAL_TEST_FILE, PARSE, TOK)
-STFormat.ConvertXML.toSTFormat(xml, "final-geniaformat", outputTag="a2")
-# Sanity Check
-STFormat.Compare.compare("final-geniaformat", "empty-devel-geniaformat", "a2")
+    evaluateREN("empty-devel-geniaformat")
+
+if not options.noTestSet:
+    print >> sys.stderr, "------------ Test set classification ------------"
+    if "local" not in options.csc:
+        clear = False
+        if "clear" in options.csc: clear = True
+        if "louhi" in options.csc:
+            c = CSCConnection(CSC_WORKDIR+"/edge-everything-models", "jakrbj@louhi.csc.fi", clear)
+        else:
+            c = CSCConnection(CSC_WORKDIR+"/edge-everything-models", "jakrbj@murska.csc.fi", clear)
+    else:
+        c = None
+    finalEdgeModel = optimize(CLASSIFIER, Ev, EDGE_EVERYTHING_EXAMPLE_FILE, EDGE_TEST_EXAMPLE_FILE,\
+    EDGE_IDS+".class_names", "c:"+bestEdgeModel[2].split("_")[-1], "edge-everything-models", None, c, False)[1]
+    Cls.test(EDGE_FINAL_TEST_EXAMPLE_FILE, finalEdgeModel, "final-edge-test-classifications")
+    xml = BioTextExampleWriter.write(EDGE_FINAL_TEST_EXAMPLE_FILE, "final-edge-test-classifications", FINAL_TEST_FILE, None, EDGE_IDS+".class_names", PARSE, TOK)
+    xml = ix.splitMergedElements(xml, None)
+    xml = ix.recalculateIds(xml, "final-predicted-edges.xml", True)
+    EvaluateInteractionXML.run(Ev, xml, FINAL_TEST_FILE, PARSE, TOK)
+    STFormat.ConvertXML.toSTFormat(xml, "final-geniaformat", outputTag="a2")
+    # Sanity Check
+    STFormat.Compare.compare("final-geniaformat", "empty-devel-geniaformat", "a2")
