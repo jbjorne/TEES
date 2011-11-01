e2688acf3bcaa5ae66d55fdbadb3926c735abc40
jari <jari>
2011-11-01 15:42:02 +0000

    Disabled temporary DDI hack (September 24, 2011 3:21:28 AM).

--- JariSandbox/ComplexPPI/Source/Core/SimpleGraph.py
+++ JariSandbox/ComplexPPI/Source/Core/SimpleGraph.py
@@ -172,7 +172,7 @@ class Graph:
                 if n1 == n2:
                     self.__distances[n1][n2] = 0
                 elif len(self.getEdges(n1, n2)) > 0:
-                    if True: # temporary hack for DDI
+                    if False: # temporary hack for DDI
                         edgeCount = 0
                         for edge in self.getEdges(n1, n2):
                             if edge[2].get("type") != "conj_and":
