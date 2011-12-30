e19e863caf8447c7cabdcf507c0a33c4506a1276
jari <jari.bjorne@utu.fi>
2011-12-30 19:06:41 +0200

    Cluster processing system

---
+++ Test/PredictTask.py
@@ -0,0 +1,104 @@
+import time, datetime
+startTime = time.time()
+
+# most imports are defined in Pipeline
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../CommonUtils") # new CommonUtils
+from Pipeline import *
+import STFormat.ConvertXML
+import STFormat.Compare
+import STFormat.STTools
+import subprocess
+import shutil
+from Detectors.EventDetector import EventDetector
+from Detectors.StepSelector import StepSelector
+from Detectors.Preprocessor import Preprocessor
+import tempfile
+
+from optparse import OptionParser
+optparser = OptionParser()
+optparser.add_option("-i", "--input", default=None, dest="input", help="input data")
+optparser.add_option("-c", "--corpus", default="PMC11", dest="corpus", help="corpus name for preprocessing")
+optparser.add_option("-o", "--output", default=None, dest="output", help="output directory")
+optparser.add_option("-w", "--workdir", default=None, dest="workdir", help="work directory")
+optparser.add_option("-m", "--model", default=None, dest="model", help="model file or directory")
+optparser.add_option("-p", "--parse", default="split-McClosky", dest="parse", help="Parse XML element name")
+optparser.add_option("--eventTag", default="GE", dest="eventTag", help="")
+optparser.add_option("--step", default=None, dest="step", help="")
+optparser.add_option("--detectorStep", default=None, dest="detectorStep", help="")
+optparser.add_option("--csc", default="", dest="csc", help="")
+optparser.add_option("--noLog", default=False, action="store_true", dest="noLog", help="")
+optparser.add_option("--clearAll", default=False, action="store_true", dest="clearAll", help="Delete all files")
+optparser.add_option("--debug", default=False, action="store_true", dest="debug", help="")
+(options, args) = optparser.parse_args()
+
+selector = StepSelector(["PREPROCESS", "EVENTS"], fromStep=options.step)
+
+# Get the input stem, which will be used for naming the output files
+options.input = options.input.rstrip("/")
+#INPUT_TAG = options.input.rsplit("/", 1)[-1]
+#if os.path.isfile(options.input):
+#    if INPUT_TAG.endswith(".tar.gz"):
+#        INPUT_TAG = INPUT_TAG[:-len(".tar.gz")]
+open(options.input+"-STARTED", "w").close() # Mark process status
+        
+# Start logging
+WORKDIR = options.workdir
+if WORKDIR == None:
+    WORKDIR = tempfile.mkdtemp()
+workdir(WORKDIR, options.clearAll) # Select a working directory, optionally remove existing files
+if not options.noLog:
+    log(options.clearAll, True, options.input + "-" + options.eventTag + ".log") # Start logging into a file in working directory
+
+eventDetectionInput = None
+preprocessor = Preprocessor()
+preprocessor.debug = options.debug
+preprocessor.source = options.input # This has to be defined already here, needs to be fixed later
+preprocessor.compressIntermediateFiles = True # save space
+preprocessor.intermediateFilesAtSource = True # create output at source file location
+preprocessor.requireEntitiesForParsing = True # parse only sentences which contain BANNER entities
+if selector.check("PREPROCESS"):
+    if os.path.exists(preprocessor.getOutputPath("FIND-HEADS")):
+        print >> sys.stderr, "Preprocessor output", preprocessor.getOutputPath("FIND-HEADS"), "exists, skipping preprocessing."
+        eventDetectionInput = preprocessor.getOutputPath("FIND-HEADS")
+    else:
+        print >> sys.stderr, "Preprocessor output", preprocessor.getOutputPath("FIND-HEADS"), "does not exist"
+        print >> sys.stderr, "------------ Preprocessing ------------"
+        # Remove some of the unnecessary intermediate files
+        preprocessor.setIntermediateFile("CONVERT", None)
+        preprocessor.setIntermediateFile("SPLIT-SENTENCES", None)
+        preprocessor.setIntermediateFile("PARSE", None)
+        preprocessor.setIntermediateFile("CONVERT-PARSE", None)
+        preprocessor.setIntermediateFile("SPLIT-NAMES", None)
+        # Process input into interaction XML
+        eventDetectionInput = preprocessor.process(options.input, options.corpus, options.output, [], fromStep=options.detectorStep, toStep=None, omitSteps=["DIVIDE-SETS"])
+
+eventDetector = EventDetector()
+eventDetector.stEvaluator = None # ST evaluation won't work for external data
+eventDetector.stWriteScores = True # write confidence scores into additional st-format files
+eventDetector.setCSCConnection(options.csc, os.path.join("CSCConnection",WORKDIR.lstrip("/")))
+if selector.check("EVENTS"):
+    print >> sys.stderr, "------------ Event Detection ------------"
+    if options.model != None:
+        if eventDetectionInput == None:
+            #if options.input != None:
+            eventDetectionInput = options.input
+            #else:
+            #    eventDetectionInput = preprocessor.getOutputPath("FIND-HEADS")
+        eventDetector.classify(eventDetectionInput, options.model, "events", fromStep=options.detectorStep, parse=options.parse)
+        STFormat.STTools.getStatistics("events-events.tar.gz", True, ",")
+        # Move final predicted files to output directory
+        if os.path.exists("events-modifier-pred.xml.gz"):
+            shutil.copy("events-modifier-pred.xml.gz", options.input + "-events-"+options.eventTag+"-modifiers.xml.gz")
+        else:
+            shutil.copy("events-unmerging-pred.xml.gz", options.input + "-events-"+options.eventTag+"-unmerged.xml.gz")
+        shutil.copy("events-events.tar.gz", options.input + "-events-"+options.eventTag+".tar.gz")
+        #STFormat.Compare.compare("predicted.tar.gz", "predicted-devel.tar.gz", "a2")
+        open(options.input+options.eventTag+"-FINISHED", "w").close() # Mark process status
+    else:
+        print >> sys.stderr, "No model defined, skipping event detection"
+
+if options.workdir == None: # remove temporary working directory
+    shutil.rmtree(WORKDIR)
+    
+print >> sys.stderr, "Total processing time:", str(datetime.timedelta(seconds=time.time()-startTime))
---
+++ Test/ProcessBatches.py
@@ -0,0 +1,156 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../CommonUtils") # new CommonUtils
+import JariSandbox.ComplexPPI.Source.Murska.MakeJobScript as MakeJobScript
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import time
+
+def numJobs(username="jakrbj"):
+    """
+    Get number of queued (pending or running) jobs
+    """
+    if MakeJobScript.isVuori():
+        p = subprocess.Popen("squeue | grep " + username + " | wc -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        p = subprocess.Popen("bjobs | grep " + username + " | wc -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lines = p.stdout.readlines()
+    assert len(lines) == 1
+    return int(lines[0])
+
+def getMaxJobsSetting(controlFilename):
+    f = open(controlFilename, "rt")
+    lines = f.readlines()
+    f.close()
+    assert len(lines) == 1
+    value = int(lines[0])
+    if value == 0:
+        print >> sys.stderr, "Exit by control file request"
+        sys.exit() 
+    return value
+
+def getSources(triple):
+    sources = []
+    for filename in sorted(triple[2]):
+        if filename.endswith(".txt"): # process directories with txt-files
+            sources.append(triple[0])
+        if filename.endswith(".tar.gz"): # process compressed st-format directories
+            sources.append(os.path.join(triple[0], filename))
+    if triple[0] in sources:
+        assert len(sources) == 0, sources
+    return sources
+
+def getSkip(input, tags):
+    missing = []
+    for tag in tags:
+        if not os.path.exists(input + tag):
+            missing.append(tag)
+    if len(missing) > 0:
+        print >> sys.stderr, "Tags", missing, "missing for input", input
+        return True
+    else:
+        return False
+
+def getJobs(username="jakrbj"):
+    jobNames = []
+    queueCommand = "squeue -o \"%.7i %.64j %.8u %.8T %.9M %.6D %R\" | grep " + username
+    p = subprocess.Popen(queueCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lines = p.stdout.readlines()
+    for line in lines:
+        jobNames.append(line.strip().split()[1])
+    return jobNames
+
+def submitJob(input, model, dummy=False, saveWorkDirs=False, rerun=False, skipTags=[".QUEUED"], hideFinished=False):
+    # Check for output files
+    if not (rerun or getSkip(input, skipTags)): # this input has already been submitted
+        if not hideFinished:
+            print >> sys.stderr, "Processing", input
+            print >> sys.stderr, "input already processed"
+        return False
+    print >> sys.stderr, "Processing", input
+    # Check if the job is still running
+    queuedAndRunningJobNames = getJobs()
+    stemDir, finalDir = os.path.normpath(input).rsplit("/", 1)
+    if finalDir in queuedAndRunningJobNames:
+        print >> sys.stderr, "Job", finalDir, "is currently queued/running"
+        return False
+    # We can queue the job
+    args = ["python", "PredictTask.py", "-i", input]
+    if model != None:
+        args += ["-m", model]
+    if saveWorkDirs:
+        args += ["-w", input + ".workdir"]
+    script = MakeJobScript.makeJob(finalDir, "48:00", stemDir, args) # make job script
+    if not dummy:
+        print >> sys.stderr, "Submitting job"
+        if MakeJobScript.isVuori():
+            #f = open(input + ".jobscript", "wt")
+            #f.write(script)
+            #f.close
+            #subprocess.call("sbatch", shell=True, stdin=script) # submit job
+            p = Popen(['sbatch'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+            pstdout = p.communicate(input=script)[0]
+            print(pstdout)
+        else: # assume Murska
+            subprocess.call(script + " | bsub", shell=True) # submit job
+        open(input + ".QUEUED", "w").close() # mark job as submitted
+    else:
+        print >> sys.stderr, "Dummy mode"
+        #print >> sys.stderr, "Script", finalDir
+        #print >> sys.stderr, script
+    return True
+
+def queue(input, model, controlFilename=None, maxJobs=10, sleepTime=10, loop=False, dummy=False, 
+          saveWorkDirs=False, rerun=False, skipTags=[".QUEUED"], hideFinished=False, limit=None):
+    firstLoop = True
+    submitCount = 0
+    if os.path.exists(input) and os.path.isfile(input): # single file
+        if submitJob(input, model, dummy, saveWorkDirs, rerun, skipTags=skipTags, hideFinished=hideFinished):
+            submitCount += 1
+    else: # walk directory tree
+        while firstLoop or loop:
+            for triple in os.walk(input):
+                print "Processing directory", triple[0]
+                for source in getSources(triple):
+                    currentJobs = numJobs()
+                    if maxJobs == None and controlFilename != None:
+                        currentMaxJobs = getMaxJobsSetting(controlFilename)
+                    else:
+                        currentMaxJobs = maxJobs
+                    while(currentJobs > currentMaxJobs):
+                        print >> sys.stderr, currentJobs, "jobs in queue/running, sleeping for", sleepTime, "seconds"
+                        time.sleep(sleepTime)
+                        currentJobs = numJobs()
+                        if maxJobs == None and controlFilename != None:
+                            currentMaxJobs = getMaxJobsSetting(controlFilename)
+                        else:
+                            currentMaxJobs = maxJobs
+                        print >> sys.stderr, "Current jobs:", currentJobs, "max jobs", currentMaxJobs
+                    if submitJob(source, model, dummy, saveWorkDirs, rerun, skipTags=skipTags, hideFinished=hideFinished):
+                        submitCount += 1
+                        print >> sys.stderr, "Current jobs:", currentJobs, "max jobs", currentMaxJobs, "submitted jobs", submitCount
+                firstLoop = False
+
+from optparse import OptionParser
+optparser = OptionParser()
+optparser.add_option("-i", "--input", default=None, dest="input", help="input data")
+optparser.add_option("-m", "--model", default=None, dest="model", help="")
+optparser.add_option("-c", "--control", default="/v/users/jakrbj/ProcessBatchesControl.txt", dest="control", help="")
+optparser.add_option("-l", "--limit", default=None, dest="limit", help="")
+optparser.add_option("--noLog", default=False, action="store_true", dest="noLog", help="")
+optparser.add_option("--dummy", default=False, action="store_true", dest="dummy", help="Print jobs on screen, don't submit them")
+optparser.add_option("--saveWorkDirs", default=False, action="store_true", dest="saveWorkDirs", help="Create working directories at input file location")
+optparser.add_option("--rerun", default=False, action="store_true", dest="rerun", help="Rerun jobs which have already been queued")
+optparser.add_option("--maxJobs", default=None, type="int", dest="maxJobs", help="Maximum number of jobs in queue/running")
+optparser.add_option("--skipTags", default=".QUEUED", dest="skipTags", help="")
+optparser.add_option("--hideFinished", default=False, action="store_true", dest="hideFinished", help="")
+(options, args) = optparser.parse_args()
+
+if options.limit != None: options.limit = int(options.limit)
+options.skipTags = options.skipTags.split(",")
+print "Skip tags", options.skipTags
+queue(options.input, options.model, options.control, options.maxJobs, dummy=options.dummy, 
+      saveWorkDirs=options.saveWorkDirs, rerun=options.rerun, 
+      skipTags=options.skipTags, hideFinished=options.hideFinished, limit=options.limit)
+
+
