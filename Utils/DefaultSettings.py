6a7f71de142bc3b674e19c6f8201765218c1d42b
jari <jari.bjorne@utu.fi>
2012-06-26 15:24:34 +0300

    BioNLP'09 and DDI'11 download locations.

--- Utils/DefaultSettings.py
+++ Utils/DefaultSettings.py
@@ -2,19 +2,23 @@ import os
 
 # The default DATAPATH for installing data and tools
 DATAPATH=os.path.expanduser("~/TEES_DATA")
+# A dictionary for installation URLs. If there is a problem with a 
+# download location, it is possible to override a URL in the "TEES_SETTINGS" 
+# local settings file.
+URL = {}
 
-# The component download locations are listed here.
-# If there is a problem with a download location, it is possible to 
-# override a URL in the "TEES_SETTINGS" local settings file.
-URL={}
+# External Tools ##############################################################
+
 URL["SVM_MULTICLASS_SOURCE"] = "http://download.joachims.org/svm_multiclass/current/svm_multiclass.tar.gz"
 URL["SVM_MULTICLASS_LINUX"] = "http://download.joachims.org/svm_multiclass/current/svm_multiclass_linux.tar.gz"
-# Tools
 URL["GENIA_SENTENCE_SPLITTER"] = "http://www.nactem.ac.uk/y-matsu/geniass/geniass-1.00.tar.gz"
 URL["BANNER_SOURCE"] = "http://banner.svn.sourceforge.net/viewvc/banner/trunk/?view=tar"
-#URL["JAVA_TROVE"] = "http://www.java2s.com/Code/JarDownload/trove/trove-2.1.0.jar.zip"
+URL["BLLIP_SOURCE"] = "https://github.com/dmcc/bllip-parser/zipball/master"
+URL["STANFORD_PARSER"] = "http://nlp.stanford.edu/software/stanford-parser-2012-03-09.tgz"
 
-# BioNLP'11 Corpora
+# Corpora #####################################################################
+
+# BioNLP'11
 urlBase = "http://weaver.nlplab.org/~bionlp-st/BioNLP-ST/downloads/files/"
 # Main tasks
 URL["GE_DEVEL"] = urlBase + "BioNLP-ST_2011_genia_devel_data_rev1.tar.gz" 
@@ -89,3 +93,29 @@ URL["BI_TRAIN_McCC"] = urlBase + "McCC-parses-BioNLP-ST_2011_bacteria_interactio
 URL["BI_TEST_McCC"] = urlBase + "McCC-parses-BioNLP-ST_2011_bacteria_interactions_test_data.tar.gz"
 # Parses not provided in the shared tasks
 URL["TEES_PARSES"] = "/home/jari/biotext/BioNLP2011/data/parseExport/TEES-parses-2.0.tar.gz"
+
+
+# BioNLP'09 Shared Task
+urlBase = "http://www.nactem.ac.uk/tsujii/GENIA/SharedTask/dat/"
+URL["GE09_DEVEL"] = urlBase + "bionlp09_shared_task_development_data_rev1.tar.gz" 
+URL["GE09_TRAIN"] = urlBase + "bionlp09_shared_task_training_data_rev2.tar.gz"
+URL["GE09_TEST"] = urlBase + "bionlp09_shared_task_test_data_without_gold_annotation.tar.gz"
+# BioNLP'09 Evaluator
+URL["GE09_EVALUATOR"] = urlBase + "bionlp09_shared_task_evaluation_tools_v1.tar.gz"
+# BioNLP'09 Shared Task parses
+urlBase = "http://www-tsujii.is.s.u-tokyo.ac.jp/GENIA/SharedTask/dat/"
+URL["GE09_DEVEL_ANALYSES"] = urlBase + "bionlp09_shared_task_development_analyses_rev2.tar.gz" 
+URL["GE09_TRAIN_ANALYSES"] = urlBase + "bionlp09_shared_task_training_analyses_rev2.tar.gz"
+URL["GE09_TEST_ANALYSES"] = urlBase + "bionlp09_shared_task_test_analyses_rev2.tar.gz"
+
+
+# DDI
+urlBase = "http://labda.inf.uc3m.es/DDIExtraction2011/"
+URL["DDI_TRAIN_UNIFIED"] = urlBase + "DrugDDI_Unified.zip"
+URL["DDI_TRAIN_MTMX"] = urlBase + "DrugDDI_MTMX.zip"
+# If you have registered for the DDI Shared Task, insert the paths of your downloaded
+# test files in the following variables (in your local settings file) to have them 
+# converted for use with TEES
+URL["DDI_TEST_UNIFIED"] = None
+URL["DDI_TEST_MTMX"] = None
+
