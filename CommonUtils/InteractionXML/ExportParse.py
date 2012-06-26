4c50a54c95b24f59dc6a885ee7ac1d48c168b0ec
jari <jari.bjorne@utu.fi>
2012-06-26 15:21:19 +0300

    Process multiple files at a time.

--- CommonUtils/InteractionXML/ExportParse.py
+++ CommonUtils/InteractionXML/ExportParse.py
@@ -70,58 +70,66 @@ def exportStanfordDependencies(parseElement, tokenizationElement, outFile):
             outFile.write(dependency.get("type") + "(" + t1[2]+"-"+t1[-1] + ", " + t2[2]+"-"+t2[-1] + ")\n")
     outFile.write("\n") # one more newline to end the sentence (or to mark a sentence with no dependencies)
 
-def export(input, output, parse, tokenization=None, toExport=["tok", "ptb", "sd"], clear=False):
+def export(input, output, parse, tokenization=None, toExport=["tok", "ptb", "sd"], inputSuffixes=None, clear=False):
     print >> sys.stderr, "##### Export Parse #####"
-    corpusRoot = ETUtils.ETFromObj(input).getroot()
     
     if os.path.exists(output) and clear:
         shutil.rmtree(output)
     if not os.path.exists(output):
         os.makedirs(output)
-    
-    documents = corpusRoot.findall("document")
-    counter = ProgressCounter(len(documents), "Documents")
-    counts = defaultdict(int)
-    for document in documents:
-        counter.update()
-        docId = document.get("pmid")
-        counts["document"] += 1
-        # Open document output files
-        outfiles = {}
-        for fileExt in toExport:
-            outfilePath = output + "/" + docId + "." + fileExt
-            assert not os.path.exists(outfilePath)
-            outfiles[fileExt] = codecs.open(outfilePath, "wt", "utf-8")
-        # Process all the sentences in the document
-        for sentence in document.findall("sentence"):
-            counts["sentence"] += 1
-            parseElement = None
-            for e in sentence.getiterator("parse"):
-                if e.get("parser") == parse:
-                    parseElement = e
-                    counts["parse"] += 1
-                    break
-            if tokenization == None:
-                tokenization = parseElement.get("tokenizer")
-            tokenizationElement = None
-            for e in sentence.getiterator("tokenization"):
-                if e.get("tokenizer") == tokenization:
-                    tokenizationElement = e
-                    counts["tokenization"] += 1
-                    break
-            if "tok" in outfiles:
-                exportTokenization(tokenizationElement, parseElement, sentence, outfiles["tok"])
-                counts["tok"] += 1
-            if "ptb" in outfiles:
-                exportPennTreeBank(parseElement, outfiles["ptb"])
-                counts["ptb"] += 1
-            if "sd" in outfiles:
-                exportStanfordDependencies(parseElement, tokenizationElement, outfiles["sd"])
-                counts["sd"] += 1
-        # Close document output files
-        for fileExt in outfiles:
-            outfiles[fileExt].close()
-            outfiles[fileExt] = None
+    if inputSuffixes != None:
+        inputFileNames = []
+        for suffix in inputSuffixes:
+            inputFileNames.append(input + suffix)
+    else:
+        inputFileNames = [input]
+
+    for inputFileName in inputFileNames:
+        print >> sys.stderr, "Processing input file", inputFileName
+        corpusRoot = ETUtils.ETFromObj(inputFileName).getroot()
+        documents = corpusRoot.findall("document")
+        counter = ProgressCounter(len(documents), "Documents")
+        counts = defaultdict(int)
+        for document in documents:
+            counter.update()
+            docId = document.get("pmid")
+            counts["document"] += 1
+            # Open document output files
+            outfiles = {}
+            for fileExt in toExport:
+                outfilePath = output + "/" + docId + "." + fileExt
+                assert not os.path.exists(outfilePath) # check for overlapping files
+                outfiles[fileExt] = codecs.open(outfilePath, "wt", "utf-8")
+            # Process all the sentences in the document
+            for sentence in document.findall("sentence"):
+                counts["sentence"] += 1
+                parseElement = None
+                for e in sentence.getiterator("parse"):
+                    if e.get("parser") == parse:
+                        parseElement = e
+                        counts["parse"] += 1
+                        break
+                if tokenization == None:
+                    tokenization = parseElement.get("tokenizer")
+                tokenizationElement = None
+                for e in sentence.getiterator("tokenization"):
+                    if e.get("tokenizer") == tokenization:
+                        tokenizationElement = e
+                        counts["tokenization"] += 1
+                        break
+                if "tok" in outfiles:
+                    exportTokenization(tokenizationElement, parseElement, sentence, outfiles["tok"])
+                    counts["tok"] += 1
+                if "ptb" in outfiles:
+                    exportPennTreeBank(parseElement, outfiles["ptb"])
+                    counts["ptb"] += 1
+                if "sd" in outfiles:
+                    exportStanfordDependencies(parseElement, tokenizationElement, outfiles["sd"])
+                    counts["sd"] += 1
+            # Close document output files
+            for fileExt in outfiles:
+                outfiles[fileExt].close()
+                outfiles[fileExt] = None
         
     print >> sys.stderr, "Parse export counts:"
     for k in sorted(counts.keys()):
@@ -139,9 +147,12 @@ if __name__=="__main__":
 
     optparser = OptionParser(usage="%prog [options]\n")
     optparser.add_option("-i", "--input", default=None, dest="input", help="Corpus in interaction xml format", metavar="FILE")
+    optparser.add_option("-s", "--inputSuffixes", default=None, dest="inputSuffixes", help="e.g. '-train.xml,-devel.xml,-test.xml'", metavar="FILE")
     optparser.add_option("-o", "--output", default=None, dest="output", help="Output directory.")
     optparser.add_option("-p", "--parse", default=None, dest="parse", help="")
     optparser.add_option("-c", "--clear", default=False, action="store_true", dest="clear", help="")
     (options, args) = optparser.parse_args()
-
-    export(options.input, options.output, options.parse, clear=options.clear)
+    
+    if options.inputSuffixes != None:
+        options.inputSuffixes = options.inputSuffixes.split(",")
+    export(options.input, options.output, options.parse, clear=options.clear, inputSuffixes=options.inputSuffixes)
