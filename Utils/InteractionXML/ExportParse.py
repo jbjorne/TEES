2a65f87c299c7fbb9ed1114c28284f79bdd48b6f
jari <jari.bjorne@utu.fi>
2012-07-06 20:32:38 +0300

    Undo the protein name splitting, if it has been done for the parse.

--- Utils/InteractionXML/ExportParse.py
+++ Utils/InteractionXML/ExportParse.py
@@ -17,14 +17,42 @@ def getTokenText(tokenElement):
     # it's unlikely there would be newlines inside tokens
     return tokenElement.get("text").replace("\n", " ").replace("\r", " ").strip()
 
+def getTokens(tokenizationElement):
+    # order tokens by charOffset
+    tokenElements = []
+    for tokenElement in tokenizationElement.findall("token"):
+        charOffset = tokenElement.get("charOffset")
+        begin, end = charOffset.split("-")
+        tokenElements.append( [int(begin), int(end), tokenElement] )
+    tokenElements.sort()
+    
+    # Get token texts, and mark indices moved by splitting
+    index = 0
+    tokenTexts = []
+    tokenIdMap = {} # zero-based
+    splitFrom = None
+    for tokenElement in tokenElements:
+        token = tokenElement[2]
+        if token.get("splitFrom") != None:
+            if splitFrom != token.get("splitFrom"): # this token begins a new set of split tokens
+                splitFrom = token.get("splitFrom")
+                tokenTexts.append(getTokenText(token))
+            else: # this token continues an existing set of split tokens
+                tokenTexts[-1] = tokenTexts[-1] + getTokenText(token)
+        else: # a non-split token
+            splitFrom = None
+            tokenTexts.append(getTokenText(token))
+        tokenIdMap[index] = len(tokenTexts) - 1
+        index += 1
+    return tokenTexts, tokenIdMap
+
 def exportTokenization(tokenizationElement, parseElement, sentenceElement, outFile):
     pennstring = None
     if parseElement != None:
         pennstring = parseElement.get("pennstring")
     if tokenizationElement != None and pennstring != None and pennstring.strip() != "":
         tokenTexts = []
-        for token in tokenizationElement.findall("token"):
-            tokenTexts.append(getTokenText(token))
+        tokenTexts, tokenIdMap = getTokens(tokenizationElement)
         outFile.write(" ".join(tokenTexts) + "\n")
     else:
         outFile.write(" ".join(sentenceElement.get("text").strip().split()) + "\n")
@@ -47,31 +75,25 @@ def exportStanfordDependencies(parseElement, tokenizationElement, outFile):
     escDictKeys = sorted(unEscDict.keys())
     
     tokens = []
-    tokenById = {}
     # Collect tokens
     if tokenizationElement != None:
-        for token in tokenizationElement.findall("token"):
-            charOffset = token.get("charOffset")
-            begin, end = charOffset.split("-")
-            tokenId = token.get("id")
-            tokenList = [int(begin), int(end), getTokenText(token), tokenId, None]
-            for key in escDictKeys:
-                tokenList[2] = tokenList[2].replace(key, unEscDict[key])
-            tokens.append(tokenList)
-            assert tokenId not in tokenById
-            tokenById[tokenId] = tokenList
-        # Order tokens by charOffset
-        tokens.sort()
-        # Set token indices for dependencies
+        tokens, tokenIdMap = getTokens(tokenizationElement)
         for i in range(len(tokens)):
-            tokens[i][-1] = str(i+1)
+            for key in escDictKeys:
+                tokens[i] = tokens[i].replace(key, unEscDict[key])
         
     # Process dependencies
     if parseElement != None:   
         for dependency in parseElement.findall("dependency"):
-            t1 = tokenById[dependency.get("t1")]
-            t2 = tokenById[dependency.get("t2")]
-            outFile.write(dependency.get("type") + "(" + t1[2]+"-"+t1[-1] + ", " + t2[2]+"-"+t2[-1] + ")\n")
+            if dependency.get("split") != None: # ignore dependencies created by protein name splitter
+                continue
+            t1Index = tokenIdMap[int(dependency.get("t1").split("_")[-1]) - 1] # convert to zero-based
+            t2Index = tokenIdMap[int(dependency.get("t2").split("_")[-1]) - 1] # convert to zero-based
+            assert t1Index < len(tokens), (t1Index, tokens, tokenIdMap, dependency.attrib)
+            assert t2Index < len(tokens), (t2Index, tokens, tokenIdMap, dependency.attrib)
+            t1 = tokens[t1Index] + "-" + str(t1Index + 1)
+            t2 = tokens[t2Index] + "-" + str(t2Index + 1)
+            outFile.write(dependency.get("type") + "(" + t1 + ", " + t2 + ")\n")
     outFile.write("\n") # one more newline to end the sentence (or to mark a sentence with no dependencies)
     if parseElement != None:
         return True
