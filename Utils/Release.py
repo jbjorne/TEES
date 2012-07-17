594f21579d10a10de78d306b97205e80562e9fff
jari <jari.bjorne@utu.fi>
2012-07-17 20:36:19 +0300

    Reduce model package size by replacing duplicate files with relative symbolic links.

--- Utils/Release.py
+++ Utils/Release.py
@@ -1,7 +1,8 @@
 import sys,os
-print __file__
+import shutil
+import filecmp
+import subprocess
 mainTEESDir = os.path.abspath(os.path.join(__file__, "../.."))
-print mainTEESDir
 
 def listExecutables(filter=["Core", "FeatureBuilders", "InteractionXML", "GeniaEventsToSharedTask"]):
     tableTitleLines = "| Program | Location | Description |\n"
@@ -59,6 +60,64 @@ def listExecutables(filter=["Core", "FeatureBuilders", "InteractionXML", "GeniaE
                 print "|", program[1], "|", program[0], "|", program[2], "|"
         print
 
+def extractModels(input, output, tasks):
+    assert input != None
+    assert output != None
+    assert input != output
+    if os.path.exists(output):
+        shutil.rmtree(output)
+    if not os.path.exists(output):
+        os.makedirs(output)
+    for subDir in os.listdir(input):
+        subDirAbs = os.path.join(input, subDir)
+        if os.path.isdir(subDirAbs) and subDir.split(".")[0] in tasks:
+            for suffix in ["devel", "test"]:
+                if os.path.exists(os.path.join(subDirAbs, "model-" + suffix)):
+                    src = os.path.join(subDirAbs, "model-" + suffix)
+                    dst = os.path.join(output, subDir.split(".")[0] + "-" + suffix)
+                    print >> sys.stderr, "Copying model", src, "to", dst
+                    shutil.copytree(src, dst)
+                if os.path.exists(os.path.join(subDirAbs, "log.txt")):
+                    print >> sys.stderr, "Copying training log for", subDir
+                    shutil.copy2(os.path.join(subDirAbs, "log.txt"), os.path.join(output, subDir.split(".")[0] + "-train-log.txt"))
+
+def linkDuplicates(input, output):
+    if os.path.exists(output):
+        print >> sys.stderr, "Removing output directory"
+        shutil.rmtree(output)
+    print >> sys.stderr, "Copying input directory"
+    shutil.copytree(input, output)
+    print >> sys.stderr, "Listing files"
+    files = []
+    for triple in os.walk(output):
+        for filename in triple[2]:
+            filePath = os.path.join(triple[0], filename)
+            if os.path.isfile(filePath):
+                files.append(filePath)
+    print >> sys.stderr, "Detecting duplicates"
+    duplicates = {}
+    for i in range(len(files)-1):
+        if os.path.getsize(files[i]) > 1000:
+            print >> sys.stderr, "Processing", files[i]
+            for j in range(i+1, len(files)):
+                if filecmp.cmp(files[i], files[j], shallow=False):
+                    if files[i] not in duplicates:
+                        duplicates[files[i]] = []
+                    duplicates[files[i]].append(files[j])
+        else:
+            print >> sys.stderr, "Skipping small file", files[i]
+    print >> sys.stderr, "Duplicates found:"
+    for key in sorted(duplicates.keys()):
+        print key, sorted(duplicates[key])
+    print >> sys.stderr, "Replacing duplicates with links"
+    for original in sorted(duplicates.keys()):
+        for duplicate in duplicates[original]:
+            os.remove(duplicate)
+            relPath = os.path.relpath(original, os.path.commonprefix((original, duplicate)))
+            lnCommand = "cd " + os.path.dirname(duplicate) + "; ln -s " + relPath + " " + os.path.basename(duplicate) + "; cd -"
+            print >> sys.stderr, "Linking:", lnCommand
+            subprocess.call(lnCommand, shell=True)
+
 if __name__=="__main__":
     # Import Psyco if available
     try:
@@ -73,8 +132,13 @@ if __name__=="__main__":
     optparser.add_option("-i", "--input", default=None, dest="input", help="")
     optparser.add_option("-o", "--output", default=None, dest="output", help="")
     optparser.add_option("-a", "--action", default=None, dest="action", help="")
+    optparser.add_option("-t", "--tasks", default=["GE", "EPI", "ID", "BB", "BI", "BI-FULL", "GE09", "DDI", "DDI-FULL"], dest="tasks", help="")
     (options, args) = optparser.parse_args()
-    assert options.action in ["CONVERT_CORPORA", "BUILD_MODELS", "PACKAGE_MODELS", "BUILD_APIDOC", "LIST_EXECUTABLES"]
+    assert options.action in ["CONVERT_CORPORA", "BUILD_MODELS", "EXTRACT_MODELS", "PACKAGE_MODELS", "BUILD_APIDOC", "LIST_EXECUTABLES"]
     
     if options.action == "LIST_EXECUTABLES":
         listExecutables()
+    elif options.action == "EXTRACT_MODELS":
+        extractModels(options.input, options.output, options.tasks)
+    elif options.action == "PACKAGE_MODELS":
+        linkDuplicates(options.input, options.output)
