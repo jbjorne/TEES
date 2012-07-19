6f115eab98f01d3829951c03b06fb2c84f584ee7
jari <jari.bjorne@utu.fi>
2012-07-19 18:23:00 +0300

    Added the rest of the main modules.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup (name='TEES',
     version='2.0',
-    py_modules=['__init__', 'configure'],
+    py_modules=['__init__', 'configure', 'train', 'classify', 'batch'],
     packages=['Classifiers', 'Detectors', 'Evaluators', 'ExampleBuilders', 'ExampleWriters', 'Tools', 'Utils'],
     description='Turku Event Extraction System',
     maintainer='Jari Bjorne',
