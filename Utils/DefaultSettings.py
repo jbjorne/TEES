cd65e918cf146c56d4294b017a805e78b94cb2b9
jari <jari.bjorne@utu.fi>
2012-07-24 00:56:43 +0300

    Updated the model package URL.

--- Utils/DefaultSettings.py
+++ Utils/DefaultSettings.py
@@ -31,7 +31,7 @@ URL = {}
 
 # Models
 
-URL["MODELS"] = "https://github.com/downloads/jbjorne/TEES/TEES-models-120719-2.tar.bz2"
+URL["MODELS"] = "https://github.com/downloads/jbjorne/TEES/TEES-models-120723.tar.bz2"
 
 # External Tools ##############################################################
 
