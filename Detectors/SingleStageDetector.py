d55a1af23dd53c3a22875b894335b885676c7ae0
jari <jari.bjorne@utu.fi>
2012-10-08 17:50:28 +0300

    The task parameter can be None, and if it is, it isn't saved to the model.

--- Detectors/SingleStageDetector.py
+++ Detectors/SingleStageDetector.py
@@ -100,7 +100,8 @@ class SingleStageDetector(Detector):
         if self.checkStep("EXAMPLES"):
             self.model = self.initModel(self.model, [("exampleStyle", self.tag+"example-style"), ("classifierParameters", self.tag+"classifier-parameters-train")])
             self.saveStr(self.tag+"parse", parse, self.model)
-            self.saveStr(self.tag+"task", task, self.model)
+            if task != None:
+                self.saveStr(self.tag+"task", task, self.model)
             self.buildExamples(self.model, [optData, trainData], [self.workDir+self.tag+"opt-examples.gz", self.workDir+self.tag+"train-examples.gz"], saveIdsToModel=True)
         self.model = self.openModel(model, "a") # Devel model already exists, with ids etc
         self.beginModel("BEGIN-MODEL", self.model, [self.workDir+self.tag+"train-examples.gz"], self.workDir+self.tag+"opt-examples.gz")
@@ -119,7 +120,6 @@ class SingleStageDetector(Detector):
             self.setTempWorkDir()
         model = self.openModel(model, "r")
         if parse == None: parse = self.getStr(self.tag+"parse", model)
-        if task == None: task = self.getStr(self.tag+"task", model)
         workOutputTag = os.path.join(self.workDir, os.path.basename(output) + "-")
         xml = self.classifyToXML(data, model, None, workOutputTag, 
             model.get(self.tag+"classifier-model"), goldData, parse, float(model.getStr("recallAdjustParameter", defaultIfNotExist=1.0)))
@@ -129,6 +129,8 @@ class SingleStageDetector(Detector):
         if stParams["convert"]: #self.useBioNLPSTFormat:
             Utils.STFormat.ConvertXML.toSTFormat(xml, output+"-events.tar.gz", outputTag="a2")
             if stParams["evaluate"]: #self.stEvaluator != None:
+                if task == None: 
+                    task = self.getStr(self.tag+"task", model)
                 self.stEvaluator.evaluate(output+"-events.tar.gz", task)
         self.deleteTempWorkDir()
         self.exitState()
