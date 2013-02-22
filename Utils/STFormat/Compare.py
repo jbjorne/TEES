a1bf626521d8c35c1c9f8853f31703898eba7ce7
jari <jari.bjorne@utu.fi>
2013-02-22 15:12:20 +0200

    Updated for the new STTools.

--- Utils/STFormat/Compare.py
+++ Utils/STFormat/Compare.py
@@ -1,3 +1,4 @@
+import types
 from collections import defaultdict
 from STTools import *
 
@@ -16,31 +17,38 @@ def getCounts(documents):
                 counts["trigger("+trigger.type+")"] += 1
                 counts["trigger"] += 1
         if doc.events != None:
-            for event in doc.events: 
-                counts["event("+event.type+")"] += 1
-                counts["event"] += 1
+            for event in doc.events:
+                if event.trigger != None:
+                    counts["event("+event.type+")"] += 1
+                    counts["event"] += 1
+                else:
+                    counts["relation("+event.type+")"] += 1
+                    counts["relation"] += 1
                 if event.speculation != None:
                     counts["modifier"] += 1
                     counts["modifier(spec)"] += 1
                 if event.negation != None:
                     counts["modifier"] += 1
                     counts["modifier(neg)"] += 1
-        if doc.relations != None:
-            for relation in doc.relations: 
-                counts["relation("+relation.type+")"] += 1
-                counts["relation"] += 1
-        for ann in doc.events + doc.relations:
+        for ann in doc.events:
             for arg in ann.arguments:
                 counts["arg("+arg.type+")"] += 1
                 counts["arg"] += 1
     return counts
 
-def compare(a, b, a2Tag="a2"):
+def getSet(setPath, setName, a2Tags):
+    if type(a2Tags) in types.StringTypes:
+        a2Tags = a2Tags.split(",")
+    print "Loading set " + setName + ":", setPath
+    if a2Tags != None:
+        return loadSet(setPath, a2Tags=a2Tags)
+    else:
+        return loadSet(setPath)
+
+def compare(a, b, a2Tags=None):
     print >> sys.stderr, "Comparing BioNLP Shared Task format document sets"
-    print "Loading set A:", a
-    docsA = loadSet(a, a2Tag=a2Tag)
-    print "Loading set B:", b
-    docsB = loadSet(b, a2Tag=a2Tag)
+    docsA = getSet(a, "A", a2Tags)
+    docsB = getSet(b, "B", a2Tags)
     countsA = getCounts(docsA)
     countsB = getCounts(docsB)
     allKeys = list(set(countsA.keys() + countsB.keys()))
@@ -121,8 +129,8 @@ if __name__=="__main__":
     optparser = OptionParser(description="Compare event distribution")
     optparser.add_option("-a", "--inputA", default=None, dest="inputA", help="", metavar="FILE")
     optparser.add_option("-b", "--inputB", default=None, dest="inputB", help="")
-    optparser.add_option("-t", "--a2Tag", default="a2", dest="a2Tag", help="")
+    optparser.add_option("-t", "--a2Tags", default=None, dest="a2Tags", help="")
     #optparser.add_option("-p", "--parse", default=None, dest="parse", help="Name of parse element.")
     (options, args) = optparser.parse_args()
     
-    compare(options.inputA, options.inputB, a2Tag=options.a2Tag)
+    compare(options.inputA, options.inputB, a2Tags=options.a2Tags)
