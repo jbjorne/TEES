4dd65788f7fd1b90feebfaa733eb2db16bc01367
jari <jari.bjorne@utu.fi>
2013-03-13 18:05:02 +0200

    Iterative parsing so the file will fit in memory.

--- Tools/MetaMap.py
+++ Tools/MetaMap.py
@@ -7,6 +7,7 @@ import subprocess
 import tempfile
 import codecs
 import tarfile
+from collections import defaultdict
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
 import Utils.ElementTreeUtils as ETUtils
@@ -15,28 +16,36 @@ from Utils.ProgressCounter import ProgressCounter
 def process(input, output=None):
     """
     Run and preprocess MetaMap.
-    """
-    print >> sys.stderr, "Loading corpus", input
-    corpusTree = ETUtils.ETFromObj(input)
-    print >> sys.stderr, "Corpus file loaded"
-    corpusRoot = corpusTree.getroot()
+    """    
+    counter = ProgressCounter(None, "MetaMap")
     
-    docCount = 0
-    counter = ProgressCounter(len(sourceElements), "Sentence Splitting")
-    for document in corpusRoot.findall("document"):
-        docCount += 1
-        origId = document.get("pmid")
-        if origId == None:
-            origId = document.get("origId")
-        origId = str(origId)
-        counter.update(1, "Splitting Documents ("+document.get("id")+"/" + origId + "): ")
-        for sentence in document.findall("sentence"):
-            pass
-            
+    counts = defaultdict(int)
+    
+    outWriter = None
+    if output != None:
+        outWriter = ETUtils.ETWriter(output)
+
+    skip = False
+    for event in ETUtils.ETIteratorFromObj(input, ("start", "end")):
+        if event[0] == "start":
+            if event[1].tag == "sentence":
+                sentence = event[1]
+                counter.update(1, "Processing MetaMap ("+sentence.get("id")+"): ")
+            if not skip:
+                outWriter.begin(event[1])
+                counts[event[1].tag + ":kept"] += 1
+            else:
+                counts[event[1].tag + ":removed"] += 1
+        elif event[0] == "end":
+            if not skip:
+                outWriter.end(event[1])
+    
     if output != None:
         print >> sys.stderr, "Writing output to", output
-        ETUtils.write(corpusRoot, output)
-    return corpusTree
+        outWriter.close()
+        ETUtils.encodeNewlines(output)
+
+    return output
     
 if __name__=="__main__":
     import sys
@@ -55,5 +64,5 @@ if __name__=="__main__":
     optparser.add_option("-o", "--output", default=None, dest="output", help="Output file in interaction xml format.")
     (options, args) = optparser.parse_args()
     
-    makeSentences(input=options.input, tokenizationPath=options.tokenizationPath, output=options.output, removeText=False)
+    process(input=options.input, output=options.output)
     
