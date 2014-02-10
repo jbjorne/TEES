4839bb58aff8b9c953cbc0361ae5401e45c7f105
jari <jari.bjorne@utu.fi>
2014-02-10 23:10:03 +0200

    The layout is getting there.

--- Utils/InteractionXML/GraphViz.py
+++ Utils/InteractionXML/GraphViz.py
@@ -2,8 +2,10 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../..")
 import Utils.ElementTreeUtils as ETUtils
 import subprocess
+import SentenceElements
+from Core.SentenceGraph import SentenceGraph
 
-def toGraphViz(input, output, id):
+def toGraphViz(input, output, id, parse="McCC"):
     print >> sys.stderr, "====== Making Graphs ======"
     xml = ETUtils.ETFromObj(input).getroot()
     sentence = None
@@ -16,13 +18,41 @@ def toGraphViz(input, output, id):
         print >> sys.stderr, "No sentence for id", id
         return
     
+    elements = SentenceElements.SentenceElements(sentence, parse)
+    graph = SentenceGraph(elements.sentence, elements.tokens, elements.dependencies)
+    graph.mapInteractions(elements.entities, elements.interactions)
+    
     f = open(output, "wt")
     f.write("digraph " + id.replace(".", "_") + " {\n")
     f.write("node [shape=box];")
-    for entity in sentence.findall("entity"):
+    
+    #f.write("subgraph tokens {\n rank=\"same\";\n")
+    f.write("subgraph tokens {\n")
+    f.write("edge[weight=20, arrowhead=none];\n")
+    f.write("rankdir = LR;\n");
+    f.write("rank=\"same\";\n");
+    tokenIds = []
+    for token in elements.tokens:
+        tokenIds.append(token.get("id").replace(".", "_"))
+        f.write(token.get("id").replace(".", "_") + " [label=\"" + token.get("text") + " [" + token.get("POS") + "]\"];\n")
+    f.write("->".join(tokenIds) + ";\n")
+    f.write("}\n")
+    
+    f.write("subgraph entities {\n")
+    for entity in elements.entities:
         f.write(entity.get("id").replace(".", "_") + " [label=\"" + entity.get("type") + "\"];\n")
-    for interaction in sentence.findall("interaction"):
+    f.write("}\n")
+    
+    for entity in elements.entities:
+        headToken = graph.entityHeadTokenByEntity[entity]
+        if headToken != None:
+            f.write(entity.get("id").replace(".", "_") + " -> " + headToken.get("id").replace(".", "_") + ";\n")
+    
+    f.write("subgraph interactions {\n")
+    for interaction in elements.interactions:
         f.write(interaction.get("e1").replace(".", "_") + " -> " + interaction.get("e2").replace(".", "_") + " [label=\"" + interaction.get("type") + "\"];\n")
+    f.write("}\n")
+    
     f.write("}\n")
     f.close()
     subprocess.call("dot -Tpdf " + output + " > " + output + ".pdf", shell=True)
