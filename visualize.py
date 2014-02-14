f5bc78251a7b0cc33d98d1ed6945e7f8dc95e1de
jari <jari.bjorne@utu.fi>
2014-02-14 18:27:09 +0200

    For using the visualization, currently implemented with GraphViz.

---
+++ visualize.py
@@ -0,0 +1,16 @@
+from Utils.InteractionXML.GraphViz import toGraphViz
+
+if __name__=="__main__":
+    from optparse import OptionParser
+    optparser = OptionParser()
+    optparser.add_option("-i", "--input", default=None, dest="input", help="input interaction XML file")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="output file stem")
+    optparser.add_option("-d", "--id", default=None, dest="id", help="sentence id")
+    optparser.add_option("-p", "--parse", default="McCC", dest="parse", help="parse name")
+    optparser.add_option("-c", "--color", default="set27", dest="color", help="Event color scheme")
+    optparser.add_option("-e", "--colorParse", default="set27", dest="colorParse", help="Parse color scheme")
+    optparser.add_option("-n", "--colorNum", default=7, type="int", dest="colorNum", help="Event color scheme")
+    optparser.add_option("-m", "--colorNumParse", default=7, type="int", dest="colorNumParse", help="Event color scheme")
+    (options, args) = optparser.parse_args()
+    
+    toGraphViz(options.input, options.output, options.id, options.parse, options.color, options.colorNum, options.colorParse, options.colorNumParse)
