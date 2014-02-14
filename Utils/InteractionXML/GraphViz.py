ea18294ba2dbe30f3a1f246e4af03213c87c4b92
jari <jari.bjorne@utu.fi>
2014-02-14 13:50:02 +0200

    Tried to order subgraphs by y.

--- Utils/InteractionXML/GraphViz.py
+++ Utils/InteractionXML/GraphViz.py
@@ -31,16 +31,22 @@ def toGraphViz(input, output, id, parse="McCC"):
     f.write("graph [nodesep=0.1];\n")
     f.write("node [shape=box];")
     
+    f.write("{ \n")
+    f.write(" node [shape = plaintext, fontsize = 20];\n")
+    f.write(" 0 -> 1 -> 2 -> 4;\n")
+    f.write("}\n")
+    
     f.write("subgraph tokens {\n")
     f.write("edge[weight=1000, arrowhead=none];\n")
     f.write("rankdir = LR;\n")
     f.write("rank=\"same\";\n")
     f.write("nodesep=0.01;\n")
+    f.write("0\n")
     tokenIds = []
     for token in elements.tokens:
         tokenIds.append(token.get("id").replace(".", "_"))
         f.write(getId(token) + " [margin=0 label=\"" + token.get("text") + "\\n[" + token.get("POS") + "]\"];\n")
-    f.write("->".join(tokenIds) + ";\n")
+    f.write("->".join(["0"] + tokenIds) + ";\n")
     f.write("}\n")
     
     f.write("subgraph cluster_1m {\n")
@@ -51,15 +57,19 @@ def toGraphViz(input, output, id, parse="McCC"):
     f.write("subgraph dependencies {\n")
     f.write("node [shape=ellipse margin=0];")
     f.write("edge[weight=1 color=green];\n")
+    f.write("1\n")
+    f.write("1 -> " + getId(elements.dependencies[0]) + ";\n")
     for dep in elements.dependencies:
         f.write(getId(dep, "id") + " [margin=0 label=\"" + dep.get("type") + "\"];\n")
-        f.write(getId(dep, "t1") + " -> " + dep.get("id").replace(".", "_") + ";\n")
-        f.write(getId(dep, "id") + " -> " + dep.get("t2").replace(".", "_") + ";\n")
+        f.write(getId(dep, "t1") + " -> " + getId(dep, "id") + ";\n")
+        f.write(getId(dep, "id") + " -> " + getId(dep, "t2") + ";\n")
     f.write("}\n")
 
     f.write("subgraph entities {\n")
     #f.write("rank=\"same\";\n")
     f.write("edge[weight=1];\n")
+    f.write("2\n")
+    f.write("2 -> " + getId(elements.entities[0]) + ";\n")
     for entity in elements.entities:
         if entity.get("event") != "True":
             f.write(getId(entity) + " [label=\"" + entity.get("type") + "\"];\n")
