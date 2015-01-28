43777433a0ccd998ca2cac8beba8bd1935e14bb6
jari <jari.bjorne@utu.fi>
2015-01-28 18:30:07 +0200

    The setLocal function can now use the default local settings file when the TEES_SETTINGS environment variable hasn't been defined.

--- Utils/Settings.py
+++ Utils/Settings.py
@@ -1,17 +1,21 @@
-import sys
+import sys, os
 #print "Settings.vars", vars()
 # Import global defaults
 from DefaultSettings import *
 
-# Import local configuration
-import os
-if "TEES_SETTINGS" in os.environ or os.path.exists(DEFAULT_LOCAL_SETTINGS):
-    import imp
+def getLocalSettingsPath():
     if "TEES_SETTINGS" in os.environ: # TEES_SETTINGS may be set to DISABLED to prevent loading of local settings at import
-        pathname = os.environ["TEES_SETTINGS"]
+        return os.environ["TEES_SETTINGS"]
+    elif os.path.exists(DEFAULT_LOCAL_SETTINGS):
+        return DEFAULT_LOCAL_SETTINGS
     else:
-        pathname = DEFAULT_LOCAL_SETTINGS
-    if pathname != "DISABLED":
+        return None
+
+# Import local configuration
+pathname = getLocalSettingsPath()
+if pathname:
+    if pathname != "DISABLED": # TEES_SETTINGS may be set to DISABLED to prevent loading of local settings at import
+        import imp
         imp.load_source("TEESLocalSettings", pathname)
         # combine the settings dictionaries
         tempURL = URL
@@ -33,12 +37,12 @@ def setLocal(variable, value, setVariable=True):
     if not setVariable: # notify only, do not add the variable
         print >> sys.stderr, "Remember to add local setting", str(variable) + "=\"" + str(value) + "\""
         return
-    assert "TEES_SETTINGS" in os.environ
+    assert getLocalSettingsPath() != None
     print >> sys.stderr, "Adding local setting", str(variable) + "=" + str(value),
     # define the variable in the current module
     exec (variable + " = '" + str(value) + "'") in globals()
     # read the local settings file
-    f = open(os.environ["TEES_SETTINGS"], "rt")
+    f = open(getLocalSettingsPath(), "rt")
     lines = f.readlines()
     f.close()
     
@@ -57,7 +61,7 @@ def setLocal(variable, value, setVariable=True):
         print >> sys.stderr, "(added variable)"
     
     # write the local settings file
-    f = open(os.environ["TEES_SETTINGS"], "wt")
+    f = open(getLocalSettingsPath(), "wt")
     for line in lines:
         f.write(line)
     f.close()
