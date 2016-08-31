8043e010647658781a62bc82b96a1af06b352d39
jari <jari.bjorne@utu.fi>
2016-08-31 18:08:47 +0300

    Use more vectors.

--- ExampleBuilders/FeatureBuilders/WordVectorFeatureBuilder.py
+++ ExampleBuilders/FeatureBuilders/WordVectorFeatureBuilder.py
@@ -7,7 +7,7 @@ import Utils.Settings as Settings
 class WordVectorFeatureBuilder(FeatureBuilder):
     def __init__(self, featureSet, style=None):
         FeatureBuilder.__init__(self, featureSet, style)
-        self.model = WV.load(Settings.W2VFILE, 10000, 500000)
+        self.model = WV.load(Settings.W2VFILE, 100000, 10000000) #10000, 500000)
         
     def buildFeatures(self, token):
         weights = self.model.w_to_normv(token.get("text").lower())
