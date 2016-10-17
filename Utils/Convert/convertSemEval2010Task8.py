6c4b64c1a1f28e84d8957c76080747ab63910c82
jari <jari.bjorne@utu.fi>
2016-10-17 18:55:26 +0300

    Adding the interface.

--- Utils/Convert/convertSemEval2010Task8.py
+++ Utils/Convert/convertSemEval2010Task8.py
@@ -1,21 +1,36 @@
 import sys,os
 import re
 import zipfile
+thisPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(thisPath,"../../")))
 import xml.etree.ElementTree as ET
 import Utils.ElementTreeUtils as ETUtils
 
 class Sentence():
     def __init__(self, origId, text, corpusId):
         self.origId = origId
-        self.id = "SE10T8.d" + origId + ".s" + origId
+        self.id = corpusId + ".d" + origId + ".s" + origId
         self.text = text
         self.entities = []
         self.relation = None
         self.comment = None
     
-    def process(self):
-        self.entities.append(self.getEntity(self.text, "e1"))
-        self.entities.append(self.getEntity(self.text, "e2"))
+    def process(self, directed):
+        # Build the entities
+        self.entities.append(self._getEntity(self.text, "e1"))
+        self.entities.append(self._getEntity(self.text, "e2"))
+        for tag in ("e1", "e2"):
+            self.entities.append(self._getEntity(self.text, "e1"))
+            self.text = self.text.replace("<" + tag + ">", "").replace("</" + tag + ">", "")
+        for entity in self.entities: # check offsets
+            begin, end = [int(x) for x in entity.get("charOffset").split("-")]
+            assert entity.get("text") == self.text[begin, end], (entity.get("text"), self.text, self.text[begin, end], [begin, end])
+        # Build the interactions
+        # Build the sentence
+        sentElem = ET.Element("sentence", {"id":self.id, "charOffset":"0-"+str(len(self.text))})
+        for entity in self.entities:
+            sentElem.append(entity)
+        return sentElem
     
     def _getEntity(self, line, tag):
         before, entityText, after = re.split(r'<' + tag + '>|</' + tag + '>', line)
@@ -23,12 +38,13 @@ class Sentence():
         end = len(before) + len(entityText)
         return ET.Element("entity", {"text":entityText, "charOffset":str(begin)+"-"+str(end), "id":self.id + "." + tag})
         
-def processLines(lines, dataSet, corpusId="SE10T8"):
-    sentences = []
+def processLines(lines, dataSet, directed=True, tree=None, corpusId="SE10T8"):
+    if tree == None:
+        corpus = ET.Element("corpus", {"source":corpusId})
+        tree = ET.ElementTree(corpus)
+    else:
+        corpus = tree.getroot()
     sentence = None
-    corpus = ET.Element("corpus", {"source":corpusId})
-    xml = ET.ElementTree()
-    root = xml.getroot()
     for line in lines:
         line = line.strip().strip("\"")
         if sentence == None:
@@ -42,20 +58,19 @@ def processLines(lines, dataSet, corpusId="SE10T8"):
                 sentence.relation = line
             else:
                 assert sentence != None
-                sentences.append(sentence)
+                corpus.append(sentence.process(directed=directed))
                 sentence = None
+    return tree
 
-def getFiles(inputPath):
-    archive = zipfile.ZipFile(inputPath, 'r')
+def convert(inPath, outDir):
+    archive = zipfile.ZipFile(inPath, 'r')
     #print archive.namelist()
-    trainFile = archive.open("SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT")
-    train = processLines(trainFile.readlines(), "train")
-    trainFile.close()
-    
-    return trainFile, None
-
-def convert(inputPath):
-    train, test = getFiles(inputPath)
+    f = archive.open("SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT")
+    tree = processLines(f.readlines(), "train")
+    f.close()
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+    ETUtils.write(tree.getroot(), os.path.join(outDir, "SemEval201-Task8-all.xml"))
 
 if __name__=="__main__":
     from optparse import OptionParser
@@ -65,4 +80,4 @@ if __name__=="__main__":
     optparser.add_option("--debug", default=False, action="store_true", dest="debug", help="")
     (options, args) = optparser.parse_args()
     
-    convert(options.input)
+    convert(options.input, options.outdir)
