02f65afcf1336aa861bb5becceab843e96295219
jari <jari.bjorne@utu.fi>
2016-10-17 18:43:16 +0300

    More work on the converter.

--- Utils/Convert/convertSemEval2010Task8.py
+++ Utils/Convert/convertSemEval2010Task8.py
@@ -1,31 +1,40 @@
 import sys,os
+import re
 import zipfile
+import xml.etree.ElementTree as ET
+import Utils.ElementTreeUtils as ETUtils
 
 class Sentence():
-    def __init__(self, origId, text):
+    def __init__(self, origId, text, corpusId):
         self.origId = origId
+        self.id = "SE10T8.d" + origId + ".s" + origId
         self.text = text
         self.entities = []
         self.relation = None
         self.comment = None
     
     def process(self):
-        entity = None
-        for i in range(len(self.text)):
-            if self.text[i] == "<":
-                assert entity == None:
-                entity = [None]
-            
+        self.entities.append(self.getEntity(self.text, "e1"))
+        self.entities.append(self.getEntity(self.text, "e2"))
+    
+    def _getEntity(self, line, tag):
+        before, entityText, after = re.split(r'<' + tag + '>|</' + tag + '>', line)
+        begin = len(before)
+        end = len(before) + len(entityText)
+        return ET.Element("entity", {"text":entityText, "charOffset":str(begin)+"-"+str(end), "id":self.id + "." + tag})
         
-def processLines(lines):
+def processLines(lines, dataSet, corpusId="SE10T8"):
     sentences = []
     sentence = None
+    corpus = ET.Element("corpus", {"source":corpusId})
+    xml = ET.ElementTree()
+    root = xml.getroot()
     for line in lines:
-        line = line.strip()
+        line = line.strip().strip("\"")
         if sentence == None:
             assert line[0].isdigit(), line
             origId, line = line.split("\t")
-            sentence = Sentence(origId, line)
+            sentence = Sentence(origId, line, corpusId)
         else:
             if line.startswith("Comment:"):
                 sentence.comment = line.split(":", 1)[-1].strip()
@@ -40,7 +49,7 @@ def getFiles(inputPath):
     archive = zipfile.ZipFile(inputPath, 'r')
     #print archive.namelist()
     trainFile = archive.open("SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT")
-    train = processLines(trainFile.readlines())
+    train = processLines(trainFile.readlines(), "train")
     trainFile.close()
     
     return trainFile, None
