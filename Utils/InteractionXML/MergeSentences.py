da55fd5d2d1895a0f81ce46c166cd7be85525748
jari <jari.bjorne@utu.fi>
2017-03-21 16:30:20 +0200

    Check for offset errors when merging sentences.

--- Utils/InteractionXML/MergeSentences.py
+++ Utils/InteractionXML/MergeSentences.py
@@ -14,6 +14,7 @@ def mergeSentences(input, output):
     counts = defaultdict(int)
     for document in corpusRoot.findall("document"):
         counts["documents"] += 1
+        # Check that the entity has only sentence elements as children
         children = [x for x in document]
         docChildTypes = sorted(set([x.tag for x in children]))
         if len(docChildTypes) == 0:
@@ -23,6 +24,7 @@ def mergeSentences(input, output):
             assert docChildTypes[0] == "sentence"
         else:
             raise Exception("Document '" + str(document.get("id")) + "' has non-sentence children: " + str(docChildTypes))
+        # Process all the child sentence elements
         docId = document.get("id")
         interactions = []
         entities = []
@@ -32,27 +34,47 @@ def mergeSentences(input, output):
             document.remove(sentence)
             combinedText += sentence.get("head", "") + sentence.get("text", "") + sentence.get("tail", "")
             sentOffset = Range.charOffsetToSingleTuple(sentence.get("charOffset"))
+            # Collect and update the entity elements
             for entity in sentence.findall("entity"):
+                # Map sentence-level entity offsets to document level
                 for offsetKey in ("charOffset", "headOffset"):
                     if entity.get(offsetKey) != None:
                         offset = Range.charOffsetToTuples(entity.get(offsetKey))
                         for i in range(len(offset)):
-                            offset[i][0] += sentOffset[0]
-                            offset[i][1] += sentOffset[0]
+                            offset[i] = (offset[i][0] + sentOffset[0], offset[i][1] + sentOffset[0])
+                        # Compare a continous entity's text with the combined sentence text
+                        if offsetKey == "charOffset" and len(offset) == 1:
+                            if not Range.contains((0, len(combinedText)), offset[0]):
+                                raise Exception("Document '" + str(document.get("id")) + "' entity '" + str(entity.get("id")) + "' offset is not contained in combined sentence text: " + str([entity.attrib, [0, len(combinedText)], combinedText]))
+                            combTextSpan = combinedText[offset[0][0]:offset[0][1]]
+                            if entity.get("text") != combTextSpan:
+                                raise Exception("Document '" + str(document.get("id")) + "' entity '" + str(entity.get("id")) + "' text does not match combined sentence text: " + str([entity.get("text"), combTextSpan]))
+                            counts["checked-charOffsets"] += 1
                         entity.set(offsetKey, Range.tuplesToCharOffset(offset))
+                # Compare mapped offsets to origOffset, if available
+                if entity.get("origOffset") != None:
+                    if entity.get("charOffset") != entity.get("origOffset"):
+                        raise Exception("Document '" + str(document.get("id")) + "' entity '" + str(entity.get("id")) + "' new charOffset differs from origOffset: " + str([entity.get("charOffset"), entity.get("origOffset")]))
+                    counts["checked-origOffsets"] += 1
+                    del entity.attrib["origOffset"]
                 assert entity.get("id") not in entityById
-                entityById[entity.get("id")] = entity
+                entityById[entity.get("id")] = entity # For re-mapping the interaction e1 and e2 attributes
                 entity.set("id", docId + ".e" + str(len(entities)))
                 entities.append(entity)
                 counts["moved-entities"] += 1
+            # Collect and update the interaction elements
             for interaction in sentence.findall("interaction"):
+                # Re-map the interaction to the updated entity ids
                 for entKey in ("e1", "e2"):
                     interaction.set(entKey, entityById[interaction.get(entKey)].get("id"))
                 interaction.set("id", docId + ".i" + str(len(interactions)))
                 interactions.append(interaction)
                 counts["moved-interactions"] += 1
+        # Check that the combined sentence text matches the document text, if available
         if document.get("text") != None and document.get("text") != combinedText:
             raise Exception("Document '" + str(document.get("id")) + "' text differs from combined sentence text: " + str([document.get("text"), combinedText]))
+            counts["checked-document-texts"] += 1
+        # Add the entity and interaction elements to the document
         document.extend(entities)
         document.extend(interactions)
     print >> sys.stderr, "Counts:", dict(counts)
