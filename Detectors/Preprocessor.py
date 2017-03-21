4ab8f94f47a00e682deba5f14da340766ee834cf
jari <jari.bjorne@utu.fi>
2017-03-22 00:21:25 +0200

    Added documentation.

--- Detectors/Preprocessor.py
+++ Detectors/Preprocessor.py
@@ -108,7 +108,7 @@ class Preprocessor(ToolChain):
 #         elif self.depParser == "SYNTAXNET":
 #             steps.append( (self.depParser + "-DEP", SyntaxNetParser.parseCls, {"parserName":self.parseName, "debug":False, "modelDir":None}, "dependencies.xml") )
     
-    def process(self, source, output, parameters=None, model=None, sourceDataSetNames=None, fromStep=None, toStep=None, omitSteps=None, logPath="AUTO"):
+    def process(self, source, output, parameters=None, model=None, fromStep=None, toStep=None, omitSteps=None, logPath="AUTO"):
         if logPath == "AUTO":
             logPath = os.path.join(options.output + "-log.txt")
         if logPath not in (None, "None"):
@@ -206,26 +206,29 @@ if __name__=="__main__":
         print >> sys.stderr, "Found Psyco, using"
     except ImportError:
         print >> sys.stderr, "Psyco not installed"
-    from optparse import OptionParser
+    from optparse import OptionParser, OptionGroup
     optparser = OptionParser(description="A tool chain for making interaction XML, sentence splitting, NER and parsing")
-    optparser.add_option("-i", "--input", default=None, dest="input", help="")
-    optparser.add_option("-n", "--inputNames", default=None, dest="inputNames", help="")
-    optparser.add_option("-c", "--corpus", default=None, dest="corpus", help="corpus name")
-    optparser.add_option("-o", "--output", default=None, dest="output", help="output directory")
-    optparser.add_option("-p", "--parameters", default=None, dest="parameters", help="preprocessing parameters")
-    optparser.add_option("-s", "--steps", default=None, dest="steps", help="")
-    optparser.add_option("-f", "--fromStep", default=None, dest="fromStep", help="")
-    optparser.add_option("-t", "--toStep", default=None, dest="toStep", help="")
-    optparser.add_option("--omitSteps", default=None, dest="omitSteps", help="")
-    optparser.add_option("--logPath", default="AUTO", dest="logPath", help="AUTO, None, or a path")
-    optparser.add_option("--debug", default=False, action="store_true", dest="debug", help="")
-    optparser.add_option("--requireEntities", default=False, action="store_true", dest="requireEntities", help="")
+    optparser.add_option("-i", "--input", default=None, dest="input", help="The input argument for the first step")
+    optparser.add_option("-o", "--output", default=None, dest="output", help="The output argument for the last step")
+    optparser.add_option("-p", "--parameters", default=None, dest="parameters", help="Step parameters, a comma-separated list of 'STEP.parameter=value' definitions")
+    optparser.add_option("-s", "--steps", default=None, dest="steps", help="A comma separated list of steps or presets")
+    shortcuts = OptionGroup(optparser, "Preprocessing step parameter shortcuts", "")
+    shortcuts.add_option("-n", "--dataSetNames", default=None, dest="dataSetNames", help="CONVERT step dataset names")
+    shortcuts.add_option("-c", "--corpus", default=None, dest="corpus", help="CONVERT step XML corpus element name")
+    shortcuts.add_option("--requireEntities", default=False, action="store_true", dest="requireEntities", help="Default setting for parsing steps")
     #optparser.add_option("--constParser", default="BLLIP-BIO", help="BLLIP, BLLIP-BIO or STANFORD")
     #optparser.add_option("--depParser", default="STANFORD-CONVERT", help="STANFORD or STANFORD-CONVERT")
-    optparser.add_option("--parseName", default="McCC")
-    optparser.add_option("--parseDir", default=None, help="Only used with IMPORT-PARSE")
-    optparser.add_option("--intermediateFiles", default=False, action="store_true", dest="intermediateFiles", help="Save an intermediate file for each step")
-    optparser.add_option("--listPresets", default=False, action="store_true", dest="listPresets", help="")
+    shortcuts.add_option("--parseName", default="McCC", help="Default setting for parsing steps")
+    shortcuts.add_option("--parseDir", default=None, help="IMPORT-PARSE step parse files directory")
+    optparser.add_option_group(shortcuts)
+    debug = OptionGroup(optparser, "Debug and Process Control Options", "")
+    debug.add_option("-f", "--fromStep", default=None, dest="fromStep", help="Continue from this step")
+    debug.add_option("-t", "--toStep", default=None, dest="toStep", help="Stop at after this step")
+    debug.add_option("--omitSteps", default=None, dest="omitSteps", help="Skip these steps")
+    debug.add_option("--logPath", default="AUTO", dest="logPath", help="AUTO, None, or a path")
+    debug.add_option("--intermediateFiles", default=False, action="store_true", dest="intermediateFiles", help="Save an intermediate file for each step")
+    debug.add_option("--debug", default=False, action="store_true", dest="debug", help="Set debug mode for all steps")
+    optparser.add_option_group(debug)
     (options, args) = optparser.parse_args()
     
     #if options.steps == None and not options.listPresets:
@@ -258,9 +261,10 @@ if __name__=="__main__":
         preprocessor.setArgForAllSteps("debug", options.debug)
         if preprocessor.hasStep("CONVERT"):
             preprocessor.stepArgs("CONVERT")["corpusName"] = options.corpus
+            preprocessor.stepArgs("CONVERT")["dataSetNames"] = options.dataSetNames
         if options.parseDir:
             preprocessor.stepArgs("IMPORT-PARSE")["parseDir"] = options.parseDir
         if options.intermediateFiles:
             preprocessor.setIntermediateFiles(True)
         #preprocessor.stepArgs("PARSE")["requireEntities"] = options.requireEntities
-        preprocessor.process(options.input, options.output, options.parameters, None, options.inputNames, fromStep=options.fromStep, toStep=options.toStep, omitSteps=options.omitSteps, logPath=options.logPath)
+        preprocessor.process(options.input, options.output, options.parameters, model=None, fromStep=options.fromStep, toStep=options.toStep, omitSteps=options.omitSteps, logPath=options.logPath)
