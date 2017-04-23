e342b0ceeada9e0308908c1c4e53e3aff49b1c21
jari <jari.bjorne@utu.fi>
2017-04-23 04:41:22 +0300

    Another layer test.

--- Detectors/KerasDetector.py
+++ Detectors/KerasDetector.py
@@ -134,7 +134,8 @@ class KerasDetector(Detector):
         #x = Conv2D(dimTargetFeatures, (1, 1), padding='same')(x)
         
         x = Conv2D(16, (3, 3), padding='same')(inputShape)
-        x = Conv2D(dimTargetFeatures, (1, 1), activation='softmax', padding='same')(x)
+        x = Dense(dimTargetFeatures)(x)
+        #x = Conv2D(dimTargetFeatures, (1, 1), activation='softmax', padding='same')(x)
         
         self.kerasModel = Model(inputShape, x)
         
