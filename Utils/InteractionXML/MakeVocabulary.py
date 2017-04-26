07700d2b0b4aa1ae147bdf4f0511cb5f779e1c48
jari <jari.bjorne@utu.fi>
2017-04-26 18:54:14 +0300

    Getting ready to test vocabulary generation.

--- Utils/InteractionXML/MakeVocabulary.py
+++ Utils/InteractionXML/MakeVocabulary.py
@@ -1,4 +1,5 @@
 import sys, os
+from Utils.ProgressCounter import ProgressCounter
 extraPath = os.path.dirname(os.path.abspath(__file__))+"/../.."
 sys.path.append(extraPath)
 try:
@@ -8,6 +9,7 @@ except ImportError:
 import Utils.ElementTreeUtils as ETUtils
 import Utils.InteractionXML.InteractionXMLUtils as IXMLUtils
 from Utils.Libraries.wvlib_light.lwvlib import WV
+import gzip, json
     
 def removeAttributes(parent, elementName, attributes, countsByType):
     for element in parent.getchildren():
@@ -26,19 +28,30 @@ def processCorpus(input, output, wordVectorPath, tokenizerName="McCC"):
     
     print >> sys.stderr, "Loading word vectors from", wordVectorPath
     wv = WV.load(wordVectorPath, 100000, 10000000)
-    for document in corpusRoot.findall("document"):
-        for sentence in corpusRoot.findall("sentence"):
+    
+    documents = corpusRoot.findall("document")
+    counter = ProgressCounter(len(documents), "Documents")
+    vocabulary = {}
+    for document in documents:
+        counter.update()
+        for sentence in document.findall("sentence"):
             tokenization = IXMLUtils.getTokenizationElement(sentence, tokenizerName)
             for token in tokenization.findall(token):
-                vector = wv.w_to_normv(token.get("text").lower())
+                text = token.get("text")
+                if text not in vocabulary:
+                    vector = wv.w_to_normv(token.get("text").lower())
+                    if vector != None:
+                        vector = vector.tolist()
+                    vocabulary[text] = vector              
     
     #for k in sorted(countsByType.keys()):
     #    print >> sys.stderr, "  " + k + ":", countsByType[k]
     
     if output != None:
         print >> sys.stderr, "Writing output to", output
-        ETUtils.write(corpusRoot, output)
-    return corpusTree
+        with gzip.open(output) as f:
+            json.dump(vocabulary, f)
+    return vocabulary
 
 if __name__=="__main__":
     import sys
@@ -56,7 +69,7 @@ if __name__=="__main__":
     optparser = OptionParser(usage="%prog [options]\nPath generator.")
     optparser.add_option("-i", "--input", default=None, dest="input", help="Corpus in interaction xml format", metavar="FILE")
     optparser.add_option("-o", "--output", default=None, dest="output", help="Output file in interaction xml format.")
-    optparser.add_option("-r", "--rules", default=None, dest="rules", help="dictionary of python dictionaries with attribute:value pairs.")    
+    optparser.add_option("-w", "--wordvectors", default=None, dest="wordvectors", help="dictionary of python dictionaries with attribute:value pairs.")    
     (options, args) = optparser.parse_args()
     
     if options.input == None:
@@ -69,6 +82,4 @@ if __name__=="__main__":
         sys.exit(1)
 
     # Rules e.g. "{\"pair\":{},\"interaction\":{},\"entity\":{\"given\":\"False\"}}"
-    rules = eval(options.rules)
-    print >> sys.stderr, "Rules:", rules
-    processCorpus(options.input, options.output, rules)
+    processCorpus(options.input, options.output, options.wordvectors)
