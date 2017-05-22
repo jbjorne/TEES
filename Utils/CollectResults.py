f7f98c139393e907d303255f7c6a70ca59c88a85
jari <jari.bjorne@utu.fi>
2017-05-22 15:26:54 +0300

    For collecting results from model directories.

--- Utils/CollectResults.py
+++ Utils/CollectResults.py
@@ -1,29 +1,64 @@
 import sys, os
-import shutil
 
-TEAM_NAME = "TEES"
-MODEL_DIR = "/home/jari/Dropbox/data/BioNLP16/models"
-OUTPUT_DIR = "/home/jari/Dropbox/data/BioNLP16/submission"
+def getLines(lines, boundaries):
+    for begin, end in boundaries:
+        subset = None
+        endFound = False
+        for line in lines:
+            if subset == None and begin in line:
+                subset = []
+            elif end in line:
+                endFound = True
+                assert subset != None
+                break
+            elif subset != None:
+                subset.append(line)
+        assert endFound
+        lines = subset
+    return subset
 
-results = {
-"BB_EVENT_16-160406":"BB3-event",
-"BB_EVENT_NER_16-BBSPAN-160406":"BB3-event+ner",
-"SDB16-160406":"SeeDev-binary"
-}
+def getGENIAResults(lines, outPath=None):
+    lines = getLines(lines, [("------------ Empty devel classification ------------", "=== EXIT STEP EMPTY"), 
+                             ("Finished!", "=== EXIT STEP ST-CONVERT")])
+    fullResults = [x.split("\t", 1)[-1] for x in lines]
+    mainResult = getLines(fullResults, [("##### approximate span and recursive mode #####", "##### event decomposition in the approximate span mode #####")])
+    for line in mainResult:
+        if "==[ALL-TOTAL]==" in line:
+            mainResult = line
+            break
+    if outPath != None:
+        with open(outPath, "wt") as f:
+            f.write("------------ Primary Result ------------\n")
+            f.write(mainResult)
+            f.write("\n")
+            f.write("------------ All Results ------------\n")
+            for line in fullResults:
+                f.write(line)
+    return fullResults, mainResult                   
 
-if os.path.exists(OUTPUT_DIR):
-    shutil.rmtree(OUTPUT_DIR)
-assert not os.path.exists(OUTPUT_DIR)
-os.makedirs(OUTPUT_DIR)
+def collectResults(inDir, inPattern, outDir):
+    print >> sys.stderr, "Collecting results from:", inDir
+    models = []
+    for filename in os.listdir(inDir):
+        fullPath = os.path.join(inDir, filename)
+        if os.path.isdir(fullPath) and (inPattern is None or inPattern.match(filename)):
+            models.append(fullPath)
+    print >> sys.stderr, "Collecting parses for models:", models
+    for model in models:
+        modelName = os.path.basename(model)
+        print >> sys.stderr, "Reading model:", modelName
+        logPath = os.path.join(model, "log.txt")
+        assert os.path.exists(logPath), logPath
+        with open(logPath, "rt") as f:
+            fullResults, mainResult = getGENIAResults(f.readlines(), outDir)
+            print >> sys.stderr, mainResult.strip()
 
-SOURCE_FILES = {"DEVEL":"classification-empty/devel-empty-events.zip", "TEST":"classification-test/test-events.zip"}
-SUBMIT_FILES = {"DEVEL":"develset", "TEST":"testset"}
+if __name__== "__main__":
+    from optparse import OptionParser
+    optparser = OptionParser(description="Collect TEES results from log files and models")
+    optparser.add_option("-i", "--input", default=None, help="Input directory")
+    optparser.add_option("-o", "--output", default=None, help="Output directory")
+    optparser.add_option("-p", "--pattern", default=None, help="Input directory subdirectories (optional)")
+    (options, args) = optparser.parse_args()
 
-for model in sorted(results.keys()):
-    print "===============", "Processing model", model, "==============="
-    for dataSet in ("DEVEL", "TEST"):
-        src = os.path.join(MODEL_DIR, model, SOURCE_FILES[dataSet])
-        dst = os.path.join(OUTPUT_DIR, TEAM_NAME + "_" + results[model] + "_" + SUBMIT_FILES[dataSet] + ".zip")
-        print "Copying", (src, dst)
-        shutil.copy2(src, dst)
-
+    collectResults(options.input, options.pattern, options.output)
