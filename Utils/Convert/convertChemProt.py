6116341e533b3292268015e8ab47c9c11e15ef2b
jari <jari.bjorne@utu.fi>
2017-08-21 22:43:58 +0300

    Collecting the ChemProt files.

--- Utils/Convert/convertChemProt.py
+++ Utils/Convert/convertChemProt.py
@@ -1,10 +1,33 @@
 import os
+import csv
+import codecs
+try:
+    import cElementTree as ET
+except ImportError:
+    import xml.etree.cElementTree as ET
 
 def convertChemProt(inDir):
     filenames = os.listdir(inDir)
     filetypes = ["_abstracts.tsv", "_entities.tsv", "_relations.tsv"]
+    documents = []
     for filename in filenames:
-        if not filename.endswith(".tsv"):
+        if not (filename.endswith(".tsv") and any([filename.endswith(x) for x in filetypes])):
             continue
-        if not any([filename.endswith(x) for x in filetypes]):
-            continue
+        docId, dataType = filename.rsplit("_", 1)
+        dataType = dataType.split(".")[0]
+        if docId not in documents:
+            documents[docId] = {}
+        assert dataType not in documents[docId]
+        documents[docId][dataType] = os.path.join(inDir, filename)
+    corpusName = "CP17"
+    corpus = ET.Element("root", {"source":corpusName})
+    docCount = 0
+    for docId in sorted(documents.keys()):
+        docFiles = documents[docId]
+        document = ET.SubElement(corpus, "document", {"id":corpusName + ".d" + str(docCount)})
+        docCount += 1
+        with codecs.open(docFiles["abstracts"], "rt", "utf-8") as f:
+            for row in csv.DictReader(f, delimiter="\t", fieldnames=["id", "title", "abstract"]):
+                
+        
+        
