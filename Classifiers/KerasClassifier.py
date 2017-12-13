4b77875493c081b13ebebfc22a5aa3300391152a
jari <jari.bjorne@utu.fi>
2017-12-13 17:25:10 +0200

    For testing.

---
+++ Classifiers/KerasClassifier.py
@@ -0,0 +1,7 @@
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/..")
+from Classifier import Classifier
+
+class KerasClassifier(Classifier):
+    def __init__(self):
+        pass
