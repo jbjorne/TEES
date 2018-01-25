9f79f45c493a515d5f2be9ca2bdb3d169dc0648f
jari <jari.bjorne@utu.fi>
2018-01-25 17:43:04 +0200

    More work on the DistanceAnalyzer.

--- Utils/DistanceAnalyzer.py
+++ Utils/DistanceAnalyzer.py
@@ -2,15 +2,26 @@ import Utils.InteractionXML.InteractionXMLUtils as IXMLUtils
 import Utils.Range as Range
 
 class DistanceAnalyzer():
-    def __init__():
+    def __init__(self):
         pass
     
-    def addSentence(self, sentenceGraph, parserName):
-        parse = IXMLUtils.getParseElement(sentence, parserName)
-        tokenization = IXMLUtils.getParseElement(sentence, parse.get("tokenizer"))
-        tokens = sorted([(Range.charOffsetToSingleTuple(x.get("charOffset")), x) for x in tokenization.findall("token")])
+    def addSentence(self, sentenceGraph):
+        tokens = sorted([(Range.charOffsetToSingleTuple(x.get("charOffset")), x) for x in sentenceGraph.tokens])
         indexByTokenId = {tokens[i][0].get("id"):i for i in range(len(tokens))}
         assert len(indexByTokenId) == len(tokens) # check that there were no duplicate ids
-        for interaction in sentence.findall("interaction"):
-            indexByTokenId[interaction.get("e1"), interaction.get("e2")]
+        entityById = {x.get("id"):x for x in sentenceGraph.entities}
+        events = {}
+        for interaction in sentenceGraph.interactions:
+            e1Id = entityById[interaction.get("e1")]
+            e2 = entityById[interaction.get("e2")]
+            t1 = sentenceGraph.entityHeadTokenByEntity[e1]
+            t2 = sentenceGraph.entityHeadTokenByEntity[e2]
+            index1 = indexByTokenId[interaction.get("e1")]
+            index2 = indexByTokenId[interaction.get("e2")]
+            intSpan = abs(index1 - index2)
+            if interaction.get("event") == "True":
+                if e1Id not in events:
+                    events[e1Id] = {"min":9999, "max":-9999}
+                events[e1Id]["min"] = min(events[e1Id]["min"], index1, index2)
+                events[e1Id]["max"] = max(events[e1Id]["max"], index1, index2)
         
